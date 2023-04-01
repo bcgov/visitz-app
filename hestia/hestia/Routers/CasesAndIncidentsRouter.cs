77fbeac617134f422b44156a0f18ba2c0be29af0
Mohamed Afsar Shakeer <78597890+AfsarFresh@users.noreply.github.com>
2023-03-31 17:48:32 -0700

    feat: Create case notes and case details screens

---
+++ hestia/hestia/Routers/CasesAndIncidentsRouter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using hestia.Views;
+using hestia.Models.BOs;
+
+namespace hestia.Routers
+{
+    /// <summary>
+    /// Use Router to move to different screens. Router handles navigation between screens.
+    /// </summary>
+	public class CasesAndIncidentsRouter
+    {
+        public void RouteUsing(ListCaseIncident2 bo)
+        {
+            routeToCaseNotes(bo);
+        }
+
+        private void routeToCaseNotes(ListCaseIncident2 bo)
+        {
+            Routing.RegisterRoute(nameof(CaseNotesPage), typeof(CaseNotesPage));
+            MainThread.BeginInvokeOnMainThread(() =>
+            {
+                var navigationParameter = new Dictionary<string, object> { { "caseIncident", bo } };
+                ((AppShell)Shell.Current).GoToAsyncRequest(nameof(CaseNotesPage), parameters: navigationParameter);
+            });
+        }
+    }
+}
+
---
+++ hestia/hestia/ViewModels/CaseIncidentDetailsViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace hestia.ViewModels
+{
+    /// <summary>
+    /// The business logic for the cases and incidents details rendering goes here.
+    /// </summary>
+	public partial class CaseIncidentDetailsViewModel : BaseViewModel, IQueryAttributable
+    {
+        [ObservableProperty]
+        public Models.BOs.ListCaseIncident2 caseIncident;
+
+        public void ApplyQueryAttributes(IDictionary<string, object> query)
+        {
+            CaseIncident = query["caseIncident"] as Models.BOs.ListCaseIncident2;
+        }
+    }
+}
+
