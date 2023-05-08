ac34a2260a80cb326e72c858c3df3ba71ba7e30d
Todd Scharien <todd.scharien@gov.bc.ca>
2023-05-08 09:03:32 -0700

    Refactor font configuration

---
+++ hestia/hestia/HestiaConfig/HestiaFonts.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hestia.HestiaConfig
+{
+    public class HestiaFonts
+    {
+        private static readonly string BcSansBoldFile = "BCSans-Bold.ttf";
+        private static readonly string BcSansBoldAlias = "BCSansBold";
+
+        private static readonly string BcSansBoldItalicFile = "BCSans-BoldItalic.ttf";
+        private static readonly string BcSansBoldItalicAlias = "BCSansBoldItalic";
+
+        private static readonly string BcSansItalicFile = "BCSans-Italic.ttf";
+        private static readonly string BcSansItalicAlias = "BCSansItalic";
+
+        private static readonly string BcSansRegularFile = "BCSans-Regular.ttf";
+        private static readonly string BcSansRegularAlias = "BCSansRegular";
+
+        public static MauiAppBuilder ConfigureFonts(MauiAppBuilder builder)
+        {
+            return builder.ConfigureFonts(fonts =>
+            {
+                fonts.AddFont(BcSansBoldFile, BcSansBoldAlias);
+                fonts.AddFont(BcSansBoldItalicFile, BcSansBoldItalicAlias);
+                fonts.AddFont(BcSansItalicFile, BcSansItalicAlias);
+                fonts.AddFont(BcSansRegularFile, BcSansRegularAlias);
+            });
+        }
+    }
+}
