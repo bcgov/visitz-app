5d6e7375d535ad49519173ea398a8543a2bf986d
Todd Scharien <todd.scharien@gov.bc.ca>
2023-05-08 16:28:53 -0700

    Begin API implementation

--- hestia/hestiapi/HestiApi.cs
+++ hestia/hestiapi/HestiApi.cs
@@ -1,6 +1,23 @@
-﻿namespace hestiapi
+﻿using hestiapi.Models;
+using hestiapi.Requests;
+
+namespace hestiapi
 {
     public class HestiApi
     {
+        private HttpClient HttpClient { get; }
+        private string BaseHestiApiUrl { get; }
+
+        public HestiApi(HttpClient httpClient, string baseHestiApiUrl)
+        {
+            HttpClient = httpClient;
+            BaseHestiApiUrl = baseHestiApiUrl;
+        }
+
+        public async List<CaseloadBaseItem> GetCaseload(params string[] workerIds)
+        {
+            var caseload = new GetCaseloadEndpoint(BaseHestiApiUrl, workerIds);
+            var response = await HttpClient.SendAsync(caseload.MakeRequest());
+        }
     }
 }
---
+++ hestia/hestiapi/Requests/GetCaseloadEndpoint.cs
@@ -0,0 +1,59 @@
+﻿using System.Text.Json.Nodes;
+
+namespace hestiapi.Requests
+{
+    internal class GetCaseloadEndpoint : HestiaBaseEndpoint
+    {
+        private static readonly string CaseloadPath = "/v1/620b";
+
+        private static readonly string WorkerIdsListKey = "workerIds";
+        private static readonly string WorkerIdKey = "workerId";
+
+        private static readonly string GetListCaseIncidentKey = "getListCaseIncident";
+
+        private readonly string[] _workerIds;
+
+        private string RequestPayload
+        {
+            get
+            {
+                return new JsonObject
+                {
+                    [GetListCaseIncidentKey] = new JsonObject
+                    {
+                        [PayloadKey] = new JsonObject
+                        {
+                            [WorkerIdsListKey] = new JsonArray
+                            {
+                                _workerIds.Select(id => new JsonObject
+                                {
+                                    [WorkerIdKey] = id
+                                })
+                            }
+                        }
+                    }
+                }.ToString();
+            }
+        }
+
+        public GetCaseloadEndpoint(string baseUrl, params string[] workerIds) : base(baseUrl, CaseloadPath)
+        {
+            _workerIds = workerIds;
+        }
+
+        public override HttpRequestMessage MakeRequest()
+        {
+            return new HttpRequestMessage()
+            {
+                Content = new FormUrlEncodedContent(FormDataCollection(DocRequestKey, RequestPayload)),
+                Method = HttpMethod.Post,
+                RequestUri = RequestUri
+            };
+        }
+
+        public override HestiaBaseEndpoint HandleResponse()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
