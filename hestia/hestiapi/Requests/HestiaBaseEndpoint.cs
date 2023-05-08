30644fc7eb5056e8bcdc61e85fb7af75f0b0824d
Todd Scharien <todd.scharien@gov.bc.ca>
2023-05-08 16:28:53 -0700

    Begin API implementation

---
+++ hestia/hestiapi/Requests/HestiaBaseEndpoint.cs
@@ -0,0 +1,37 @@
+﻿namespace hestiapi.Requests
+{
+    internal abstract class HestiaBaseEndpoint
+    {
+        protected static readonly string DocRequestKey = "docRequest";
+        protected static readonly string PayloadKey = "payLoad";
+
+        protected static KeyValuePair<string, string> FormDataPair(string key, string value)
+        {
+            return new KeyValuePair<string, string>(key, value);
+        }
+
+        protected static IEnumerable<KeyValuePair<string, string>> FormDataCollection(string key, string value)
+        {
+            return new List<KeyValuePair<string, string>>()
+            {
+                FormDataPair(key, value)
+            };
+        }
+
+        public string BaseUrl { get; }
+        public string RequestPath { get; }
+
+        public string RequestUrl => BaseUrl.TrimEnd('/') + "/" + RequestPath.TrimStart('/');
+        public Uri RequestUri => new(RequestUrl);
+
+        public HestiaBaseEndpoint(string baseUrl, string requestPath)
+        {
+            BaseUrl = baseUrl;
+            RequestPath = requestPath;
+        }
+
+        public abstract HttpRequestMessage MakeRequest();
+
+        public abstract HestiaBaseEndpoint HandleResponse();
+    }
+}
