ba325b93bb7195a054fb764cf71e30cff4daccf9
Todd Scharien <todd.scharien@gov.bc.ca>
2023-05-09 16:48:38 -0700

    Rework view model to use API wrapper

--- hestia/hestia/ViewModels/CasesAndIncidentsViewModel.cs
+++ hestia/hestia/ViewModels/CasesAndIncidentsViewModel.cs
@@ -2,6 +2,8 @@
 using System.Collections.ObjectModel;
 using CommunityToolkit.Mvvm.Input;
 using CommunityToolkit.Mvvm.ComponentModel;
+using hestiapi;
+using hestia.Models.BOs;
 
 namespace hestia.ViewModels
 {
@@ -22,27 +24,17 @@ namespace hestia.ViewModels
 
         public async void FetchCasesAndIncidents()
         {
-            CaseIncidentListPayload payload = new();
-            payload.getListCaseIncident.payLoad.workerIds.Add(new(workerId: "CGWRK68"));
-            string json = JsonSerializer.Serialize<CaseIncidentListPayload>(payload, options: null);
-            var nvc = new List<KeyValuePair<string, string>>();
-            nvc.Add(new KeyValuePair<string, string>("docRequest", json));
-            var nvcContent = new FormUrlEncodedContent(nvc);
-            nvcContent.Headers.Clear();
-            nvcContent.Headers.Add("Content-Type", "application/x-www-form-urlencoded");
             try
             {
-                httpClient.BaseAddress = new Uri("https://hestia-dev.api.gov.bc.ca/v1/620b");
-                HttpResponseMessage response = await httpClient.PostAsync("", nvcContent);
-                string responseContent = await response.Content.ReadAsStringAsync();
-                CaseIncidentListDTO dto = JsonSerializer.Deserialize<CaseIncidentListDTO>(responseContent, options: null);
-                string caseIncidentNumber = dto.listCaseIncident.payLoad.listCaseIncidents.FirstOrDefault<Models.DTOs.ListCaseIncident2>().caseIncidentNumber;
+                // TODO: Base URL should be read from an ApiSettings implementation
+                var api = new HestiApi(httpClient, "https://hestia-dev.api.gov.bc.ca");
+
+                // TODO: Worker ID should be collected from current JWT Access Token field "idir_username"
+                var caseload = await api.GetCaseloadAsync("CGWRK68");
+
                 BoCasesAndInsidents.Clear();
-                var caseIncidentListBO = Models.BOs.CaseIncidentListBO.ToBO(dto);
-                caseIncidentListBO?.listCaseIncident?.payLoad?.listCaseIncidents?.ForEach(item =>
-                {
-                    BoCasesAndInsidents.Add(item);
-                });
+
+                caseload.Select(item => BoCasesAndInsidents.Add(item));
             }
             catch (Exception ex)
             {
