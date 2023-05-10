812121752c31e39528b827f9e8b513aa1eaed822
Todd Scharien <todd.scharien@gov.bc.ca>
2023-05-10 14:13:13 -0700

    Add error handling classes

---
+++ hestia/hestiapi/Extensions/JsonElementFirstsExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.Json;
+
+namespace hestiapi.Extensions
+{
+    internal static class JsonElementFirstsExtensions
+    {
+        public static JsonElement FirstProperty(this JsonElement element)
+        {
+            return element.GetProperty(element.EnumerateObject().First().Name);
+        }
+
+        public static JsonElement FirstArrayElement(this JsonElement element)
+        {
+            return element.EnumerateArray().First();
+        }
+    }
+}
