320d0a208fb6a789c4130213c531524511483928
Todd Scharien <todd.scharien@gov.bc.ca>
2023-05-09 18:32:28 -0700

    Implement get notes API endpoint

---
+++ hestia/hestiapi/Models/NoteEntity.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json.Serialization;
+
+namespace hestiapi.Models
+{
+    public class NoteEntity
+    {
+        private const string NotesKey = "notes";
+
+        public string NotePeriod { get; set; } // Intentionally a string, not DateTime
+
+        public DateTime CreatedDate { get; set; }
+
+        [JsonPropertyName(NotesKey)]
+        public string Content { get; set; }
+    }
+}
---
+++ hestia/hestiapi/Models/NotesListEntity.cs
@@ -0,0 +1,9 @@
+﻿namespace hestiapi.Models
+{
+    public class NotesListEntity
+    {
+        public string EntityNumber { get; set; }
+        public string EntityType { get; set; }
+        public List<NoteEntity> Notes { get; set; }
+    }
+}
