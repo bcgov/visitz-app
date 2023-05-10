9117feb768221c7981b10151ef2cb62b52547687
Todd Scharien <todd.scharien@gov.bc.ca>
2023-05-10 16:25:52 -0700

    Move OidcSettings and use new wrapper

--- hestia/hestia/HestiaConfig/HestiaAuth.cs
+++ hestia/hestia/HestiaConfig/HestiaAuth.cs
@@ -1,28 +1,15 @@
-﻿using hestia.Models;
-using hestia.Services.Authentication;
+﻿using hestia.Services.Authentication;
+using hestia.Settings;
 using Microsoft.Extensions.Configuration;
-using System.Reflection;
 
 namespace hestia.HestiaConfig
 {
     public static class HestiaAuth
     {
-        private static readonly string Namespace = "hestia";
-        private static readonly string AppSettingsFile = "appSettings.json";
-
-        private static readonly string OidcSettings = "OidcSettings";
-
         public static MauiAppBuilder ConfigureHestiaAuth(this MauiAppBuilder builder)
         {
-            var assembly = Assembly.GetExecutingAssembly();
-            using var stream = assembly.GetManifestResourceStream(Namespace + "." + AppSettingsFile);
-
-            var config = new ConfigurationBuilder()
-                         .AddJsonStream(stream)
-                         .Build();
-            builder.Configuration.AddConfiguration(config);
-
-            var settings = config.GetRequiredSection(OidcSettings).Get<OidcSettings>();
+            // TODO: Get AppSettings working correctly with DI
+            var settings = new AppSettings().Oidc;
 
             var options = new AuthenticationClient.Options()
             {
