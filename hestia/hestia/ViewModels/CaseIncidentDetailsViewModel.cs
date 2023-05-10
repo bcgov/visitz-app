ffe339fa79d845c79641506639f577cbeaff5cc5
Todd Scharien <todd.scharien@gov.bc.ca>
2023-05-10 08:40:52 -0700

    Simplify type qualifiers

--- hestia/hestia/ViewModels/CaseIncidentDetailsViewModel.cs
+++ hestia/hestia/ViewModels/CaseIncidentDetailsViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using hestia.Models.BOs;
 
 namespace hestia.ViewModels
 {
@@ -10,11 +11,11 @@ namespace hestia.ViewModels
 	public partial class CaseIncidentDetailsViewModel : BaseViewModel, IQueryAttributable
     {
         [ObservableProperty]
-        public Models.BOs.ListCaseIncident2 caseIncident;
+        public ListCaseIncident2 caseIncident;
 
         public void ApplyQueryAttributes(IDictionary<string, object> query)
         {
-            CaseIncident = query["caseIncident"] as Models.BOs.ListCaseIncident2;
+            CaseIncident = query["caseIncident"] as ListCaseIncident2;
         }
     }
 }
