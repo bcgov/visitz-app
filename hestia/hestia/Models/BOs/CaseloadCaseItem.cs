0a3605571fb51f44db161885d6cf19a93da1c197
Todd Scharien <todd.scharien@gov.bc.ca>
2023-05-09 17:35:07 -0700

    Add objects to handle caseload processing

---
+++ hestia/hestia/Models/BOs/CaseloadCaseItem.cs
@@ -0,0 +1,20 @@
+﻿using hestiapi.Models;
+
+namespace hestia.Models.BOs
+{
+    public class CaseloadCaseItem : CaseloadBaseItem
+    {
+        public string CreatedDate { get; set; }
+        public string KeyPlayerCellPhone { get; set; }
+        public string KeyPlayerEmail { get; set; }
+        public string KeyPlayerHomePhone { get; set; }
+
+        public CaseloadCaseItem(CaseloadCaseEntity caseloadCase) : base(caseloadCase)
+        {
+            CreatedDate = caseloadCase.CreatedDate;
+            KeyPlayerCellPhone = caseloadCase.KeyPlayerCellPhone;
+            KeyPlayerEmail = caseloadCase.KeyPlayerEmail;
+            KeyPlayerHomePhone = caseloadCase.KeyPlayerHomePhone;
+        }
+    }
+}
---
+++ hestia/hestia/Models/BOs/CaseloadIncidentItem.cs
@@ -0,0 +1,14 @@
+﻿using hestiapi.Models;
+
+namespace hestia.Models.BOs
+{
+    public class CaseloadIncidentItem : CaseloadBaseItem
+    {
+        public string DateReported { get; set; }
+
+        public CaseloadIncidentItem(CaseloadIncidentEntity caseloadEntity) : base(caseloadEntity)
+        {
+            DateReported = caseloadEntity.DateReported;
+        }
+    }
+}
