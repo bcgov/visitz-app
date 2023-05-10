ed4be96b8d04ce48413d1bd5d35bc244d162cbae
Todd Scharien <todd.scharien@gov.bc.ca>
2023-05-10 16:24:02 -0700

    Add AppSettings wrapper and ApiSettings section

---
+++ hestia/hestia/Settings/AppSettings.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.Reflection;
+
+namespace hestia.Settings
+{
+    public class AppSettings
+    {
+        private static readonly string Namespace = "hestia";
+        private static readonly string AppSettingsFile = "appSettings.json";
+
+        private static readonly string ApiSettingsKey = "ApiSettings";
+        private static readonly string OidcSettingsKey = "OidcSettings";
+
+        public static string AppSettingsPath => Namespace + "." + AppSettingsFile;
+
+        public IConfiguration Configuration { get; }
+
+        public ApiSettings Api => Configuration.GetRequiredSection(ApiSettingsKey).Get<ApiSettings>();
+
+        public OidcSettings Oidc => Configuration.GetRequiredSection(OidcSettingsKey).Get<OidcSettings>();
+
+        public AppSettings()
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+
+            using var stream = assembly.GetManifestResourceStream(AppSettingsPath);
+
+            Configuration = new ConfigurationBuilder()
+                .AddJsonStream(stream)
+                .Build();
+        }
+    }
+}
