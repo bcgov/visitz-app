ca645afaac1f01d5ada08486ec3c067ceeee4ded
Todd Scharien <todd.scharien@gov.bc.ca>
2023-05-12 10:50:37 -0700

    Pull WebMethods-specific code into own impl

--- visitz/VisitzApi/ErrorHandling/VisitzApiException.cs
+++ visitz/VisitzApi/ErrorHandling/VisitzApiException.cs
@@ -6,9 +6,6 @@ namespace VisitzApi.ErrorHandling
 {
     public class VisitzApiException : Exception
     {
-        private static readonly string ErrorKey = "error";
-        private static readonly string ErrorDetailKey = "errorDetail";
-
         public HttpStatusCode HttpStatusCode { get; private set; }
 
         public bool IsError => IsErroneousStatus(HttpStatusCode) || Message.Length > 0;
@@ -23,39 +20,13 @@ namespace VisitzApi.ErrorHandling
             return (int)statusCode >= 400;
         }
 
-        private static bool TryFindFirstJsonError(HttpResponseMessage response, out string errorMessage)
-        {
-            var json = JsonDocument.Parse(response.Content.ReadAsStringAsync().Result);
-            return TryFindError(json.RootElement.FirstProperty(), out errorMessage);
-        }
-
-        private static bool TryFindError(JsonElement element, out string errorMessage)
-        {
-            errorMessage = string.Empty;
-
-            return element.TryGetProperty(JsonKey.Payload, out var payload)
-                && payload.TryGetProperty(ErrorKey, out var errorArray)
-                && TryGetErrorDetail(errorArray, out errorMessage);
-        }
-
-        private static bool TryGetErrorDetail(JsonElement array, out string errorMessage)
-        {
-            errorMessage = string.Empty;
-            var errorElement = array.FirstArrayElement().FirstProperty();
-
-            if (errorElement.TryGetProperty(ErrorDetailKey, out var error))
-                errorMessage = error.GetString();
-
-            return errorMessage.Length > 0;
-        }
-
         internal static void ThrowIfInvalid(HttpResponseMessage response)
         {
             if (IsErroneousStatus(response.StatusCode))
                 throw new VisitzApiException(response.StatusCode,
                         response.Content.ReadAsStringAsync().Result);
 
-            else if (TryFindFirstJsonError(response, out string errorMessage))
+            else if (WebMethodsJsonError.TryFindFirstError(response, out string errorMessage))
                 throw new VisitzApiException(response.StatusCode, errorMessage);
         }
     }
