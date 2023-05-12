06c55f5d8639d4086f1f43a968d7cda4f1a2b23f
Todd Scharien <todd.scharien@gov.bc.ca>
2023-05-12 12:33:40 -0700

    Add struct for Icm entity type

---
+++ visitz/Visitz/Models/BOs/IcmEntity.cs
@@ -0,0 +1,11 @@
+﻿namespace Visitz.Models.BOs
+{
+    public readonly struct IcmEntity
+    {
+        public static readonly string Case = "Case";
+
+        public static readonly string Incident = "Incident";
+
+        public static readonly string ServiceRequest = "Service Request";
+    }
+}
