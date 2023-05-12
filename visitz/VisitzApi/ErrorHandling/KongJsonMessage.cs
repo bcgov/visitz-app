48f19d05ad5ec111f2c0b8a82e7c9472f06c3359
Todd Scharien <todd.scharien@gov.bc.ca>
2023-05-12 11:24:08 -0700

    Add Kong message handler

---
+++ visitz/VisitzApi/ErrorHandling/KongJsonMessage.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.Json;
+
+namespace VisitzApi.ErrorHandling
+{
+    internal class KongJsonMessage
+    {
+        private static readonly string MessageKey = "message";
+
+        internal static bool TryFindMessage(HttpResponseMessage response, out string message)
+        {
+            var json = JsonDocument.Parse(response.Content.ReadAsStringAsync().Result).RootElement;
+
+            if (json.TryGetProperty(MessageKey, out var element))
+            {
+                message = element.GetString();
+                return true;
+            }
+
+            message = "";
+            return false;
+        }
+    }
+}
