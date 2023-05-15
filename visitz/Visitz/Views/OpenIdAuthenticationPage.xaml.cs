8a8966be0cb5d47eb0889786cbd8e24dc552f7e8
Todd Scharien <todd.scharien@gov.bc.ca>
2023-05-15 12:24:32 -0700

    Rework OpenIdAuthenticationPage to use Visitz lifecycle

--- visitz/Visitz/Views/OpenIdAuthenticationPage.xaml.cs
+++ visitz/Visitz/Views/OpenIdAuthenticationPage.xaml.cs
@@ -1,24 +1,12 @@
 ﻿using Visitz.ViewModels;
-using Visitz.Routers;
 
 namespace Visitz.Views;
 
 public partial class OpenIdAuthenticationPage : VisitzPage
 {
-    private OpenIdAuthenticationViewModel viewModel;
-    private OpenIdAuthenticationRouter router;
-
-    public OpenIdAuthenticationPage(OpenIdAuthenticationViewModel viewModel, OpenIdAuthenticationRouter router) : base(viewModel)
+    public OpenIdAuthenticationPage(OpenIdAuthenticationViewModel viewModel) : base(viewModel)
     {
         InitializeComponent();
         BindingContext = viewModel;
-        this.viewModel = viewModel;
-        this.router = router;
-    }
-
-    protected override async void OnLoad()
-    {
-        OpenIdAuthenticationViewModel.Result result = await viewModel.Authenticate();
-        router.routeUsing(result);
     }
 }
