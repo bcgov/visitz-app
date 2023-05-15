11906f60b57ebba35a11d04b0dcaa67b21977e55
Todd Scharien <todd.scharien@gov.bc.ca>
2023-05-15 13:01:00 -0700

    Remove unnecessary interface

--- visitz/Visitz/ViewModels/VisitzViewModel.cs
+++ visitz/Visitz/ViewModels/VisitzViewModel.cs
@@ -5,7 +5,7 @@ namespace Visitz.ViewModels
     /// <summary>
     /// The base class for all the view models. Common functionality can be defined here.
     /// </summary>
-	public partial class VisitzViewModel : ObservableObject, IVisitzLifecycle
+	public partial class VisitzViewModel : ObservableObject
     {
         [ObservableProperty]
         [NotifyPropertyChangedFor(nameof(IsNotBusy))]
