4e494d0e813d6ed26dcf9a97e75795cb19b23f87
Todd Scharien <todd.scharien@gov.bc.ca>
2023-05-15 12:08:37 -0700

    Rework LandingPage to use Visitz lifecycle

--- visitz/Visitz/Views/LandingPage.xaml.cs
+++ visitz/Visitz/Views/LandingPage.xaml.cs
@@ -1,42 +1,12 @@
 ﻿using Visitz.ViewModels;
-using Visitz.Routers;
 
 namespace Visitz.Views;
 
 public partial class LandingPage : VisitzPage
 {
-    LandingRouter router;
-    LandingViewModel viewModel;
-
-    public LandingPage(LandingViewModel viewModel, LandingRouter router) : base(viewModel)
+    public LandingPage(LandingViewModel viewModel) : base(viewModel)
     {
         InitializeComponent();
         BindingContext = viewModel;
-        this.viewModel = viewModel;
-        this.router = router;
-        ListenToViewModelProperties();
-    }
-
-    protected override void OnLoad()
-    {
-        LandingRouter.Route solvedRoute = viewModel.SolveRoute();
-        router.RouteTo(solvedRoute);
-    }
-
-    void ListenToViewModelProperties()
-    {
-        viewModel.PropertyChanged += (sender, args) =>
-        {
-            if (args.PropertyName.Equals(nameof(viewModel.BackFromPage)))
-            {
-                TriggerRouteUpdate();
-            }
-        };
-    }
-
-    void TriggerRouteUpdate()
-    {
-        LandingRouter.Route solvedRoute = viewModel.SolveRoute();
-        router.RouteTo(solvedRoute);
     }
 }
