903f5ec52be3285d2611266fbbb76a5bf737358b
Todd Scharien <todd.scharien@gov.bc.ca>
2023-05-15 13:01:22 -0700

    Remove (currently) unused event handlers

--- visitz/Visitz/ViewModels/VisitzViewModel.cs
+++ visitz/Visitz/ViewModels/VisitzViewModel.cs
@@ -21,10 +21,6 @@ namespace Visitz.ViewModels
             if (window == null)
                 return;
 
-            window.Created += Window_Created;
-            window.Activated += Window_Activated;
-            window.Deactivated += Window_Deactivated;
-            window.Stopped += Window_Stopped;
             window.Resumed += Window_Resumed;
         }
 
@@ -33,25 +29,13 @@ namespace Visitz.ViewModels
             if (window == null)
                 return;
 
-            window.Created -= Window_Created;
-            window.Activated -= Window_Activated;
-            window.Deactivated -= Window_Deactivated;
-            window.Stopped -= Window_Stopped;
             window.Resumed -= Window_Resumed;
         }
-
-        public void Window_Activated(object sender, EventArgs e) { }
-
-        public void Window_Created(object sender, EventArgs e) { }
-
-        public void Window_Deactivated(object sender, EventArgs e) { }
 
         public void Window_Resumed(object sender, EventArgs e)
         {
             PageStarted();
         }
-
-        public void Window_Stopped(object sender, EventArgs e) { }
 
         public virtual void PageCreated() { }
 
