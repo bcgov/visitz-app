8752b60d2866fabec7d147f6338695335e230a62
Todd Scharien <todd.scharien@gov.bc.ca>
2023-05-15 11:59:08 -0700

    Rework CaseloadPage to use Visitz lifecycle

--- visitz/Visitz/Views/CaseloadPage.xaml.cs
+++ visitz/Visitz/Views/CaseloadPage.xaml.cs
@@ -1,42 +1,12 @@
 ﻿using Visitz.ViewModels;
-using Visitz.Routers;
-using Visitz.Models.BOs;
 
 namespace Visitz.Views;
 
 public partial class CaseloadPage : VisitzPage
 {
-    private CaseloadViewModel viewModel;
-    private CaseloadRouter router;
-
-    public CaseloadPage(CaseloadViewModel viewModel, CaseloadRouter router) : base(viewModel)
+    public CaseloadPage(CaseloadViewModel viewModel) : base(viewModel)
     {
         InitializeComponent();
         BindingContext = viewModel;
-        this.viewModel = viewModel;
-        this.router = router;
-    }
-
-    protected override void OnLoad()
-    {
-        viewModel.FetchCasesAndIncidents();
-        ListenToViewModelProperties();
-    }
-
-    private void ListenToViewModelProperties()
-    {
-        viewModel.PropertyChanged += (sender, args) =>
-        {
-            if (args.PropertyName.Equals(nameof(viewModel.SelectedCaseIncident)) &&
-                viewModel.SelectedCaseIncident is not null)
-            {
-                TriggerRouteUpdate(viewModel.SelectedCaseIncident);
-            }
-        };
-    }
-
-    private void TriggerRouteUpdate(CaseloadItem caseIncident)
-    {
-        router.RouteUsing(caseIncident);
     }
 }
