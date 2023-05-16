4b3391f6f50ab927d92824155533eef98a74f83f
Todd Scharien <todd.scharien@gov.bc.ca>
2023-05-16 13:00:30 -0700

    Prompt with AppLockPage every time app starts

--- visitz/Visitz/Views/AppLockPage.xaml.cs
+++ visitz/Visitz/Views/AppLockPage.xaml.cs
@@ -4,9 +4,35 @@ namespace Visitz.Views;
 
 public partial class AppLockPage : VisitzPage
 {
+    /// <summary>
+    /// Back button behavior disabled on purpose. We don't want to let users avoid this authentication check.
+    /// </summary>
+    private static readonly bool BackButtonEnabled = false;
+
+    public static bool IsOpen => Shell.Current?.CurrentPage?.GetType() == typeof(AppLockPage);
+
     public AppLockPage(AppLockViewModel viewModel) : base(viewModel)
     {
         InitializeComponent();
         BindingContext = viewModel;
+    }
+
+    public static async Task TryPrompt()
+    {
+        if (IsOpen)
+            return;
+
+        var lockPage = Application.Current
+            .Handler
+            .MauiContext
+            .Services
+            .GetRequiredService<AppLockPage>();
+
+        await Shell.Current.Navigation.PushModalAsync(lockPage);
+    }
+
+    protected override bool OnBackButtonPressed()
+    {
+        return BackButtonEnabled;
     }
 }
