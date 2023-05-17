d1f8983ba8eda4ecd327fe2288d11c0aa151224c
Todd Scharien <todd.scharien@gov.bc.ca>
2023-05-17 10:59:46 -0700

    Move LoginPage impl into VisitzSession utility
    Remove LoginPage and its UI. This functionality can be better used
    without making the user navigate through more screens.

---
+++ visitz/Visitz/Services/Networking/VisitzSession.cs
@@ -0,0 +1,46 @@
+﻿using IdentityModel.OidcClient;
+using Visitz.Services.Authentication;
+
+namespace Visitz.Services.Networking
+{
+    public class VisitzSession
+    {
+        // TODO: Temporary naive access token handling until it's properly implemented
+        public static bool IsValid => TokenHolder.AccessToken?.Length > 0;
+
+        public static async Task<Result> PromptForLogin()
+        {
+            var authClient = Application.Current
+                .Handler
+                .MauiContext
+                .Services
+                .GetRequiredService<AuthenticationClient>();
+
+            return HandleReponse(await authClient.LoginAsync());
+        }
+
+        private static Result HandleReponse(LoginResult loginResult)
+        {
+            if (!loginResult.IsError)
+                // TODO: Incoming JWT token should be processed and stored in SecureStorage within this
+                // class instead of using TokenHolder.
+                TokenHolder.AccessToken = loginResult.AccessToken;
+
+            return new Result(loginResult.IsError, loginResult.Error, loginResult.ErrorDescription);
+        }
+
+        public struct Result
+        {
+            public bool IsError;
+            public string Error;
+            public string ErrorDescription;
+
+            public Result(bool isError, string error, string errorDescription)
+            {
+                IsError = isError;
+                Error = error;
+                ErrorDescription = errorDescription;
+            }
+        }
+    }
+}
