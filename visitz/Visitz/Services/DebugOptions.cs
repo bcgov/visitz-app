fdd63ee16cb2b24455cb69791b8526e57fa8ad03
Todd Scharien <todd.scharien@gov.bc.ca>
2023-05-31 16:21:24 -0700

    Add preferences convenience class for debug options

---
+++ visitz/Visitz/Services/DebugOptions.cs
@@ -0,0 +1,27 @@
+﻿namespace Visitz.Services
+{
+    public class DebugOptions
+    {
+        private static readonly string IdirOverrideKey = "IdirOverride";
+        private static readonly string AlwaysExpireAccessTokenKey = "AlwaysExpireAccessTokenKey";
+        private static readonly string AlwaysExpireRefreshTokenKey = "AlwaysExpireRefreshTokenKey";
+
+        public static string IdirOverride
+        {
+            get => Preferences.Default.Get(IdirOverrideKey, "");
+            set => Preferences.Default.Set(IdirOverrideKey, value.Trim());
+        }
+
+        public static bool AlwaysExpireAccessToken
+        {
+            get => Preferences.Default.Get(AlwaysExpireAccessTokenKey, false);
+            set => Preferences.Default.Set(AlwaysExpireAccessTokenKey, value);
+        }
+
+        public static bool AlwaysExpireRefreshToken
+        {
+            get => Preferences.Default.Get(AlwaysExpireRefreshTokenKey, false);
+            set => Preferences.Default.Set(AlwaysExpireRefreshTokenKey, value);
+        }
+    }
+}
