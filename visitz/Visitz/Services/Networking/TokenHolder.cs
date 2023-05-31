7d511c302c69c6bfd83ad9180120f69e0c44d7ac
Todd Scharien <todd.scharien@gov.bc.ca>
2023-05-31 12:24:24 -0700

    Store Access and Refresh tokens in SecureStorage

--- visitz/Visitz/Services/Networking/TokenHolder.cs
+++ visitz/Visitz/Services/Networking/TokenHolder.cs
@@ -1,8 +1,71 @@
-﻿namespace Visitz.Services.Networking
+﻿using IdentityModel.OidcClient;
+using System.IdentityModel.Tokens.Jwt;
+
+namespace Visitz.Services.Networking
 {
     public class TokenHolder
     {
-        public static string AccessToken { get; set; }
+        private static readonly string NamespaceKey = "visitz_oauth_";
+        private static readonly string AccessTokenKey = NamespaceKey + "access_token";
+        private static readonly string RefreshTokenKey = NamespaceKey + "refresh_token";
+
+        public static JwtSecurityToken AccessToken { get; private set; }
+
+        public static JwtSecurityToken RefreshToken { get; private set; }
+
+        private async static Task SetAsync(string key, string value)
+        {
+            await SecureStorage.Default.SetAsync(key, value);
+        }
+
+        private async static Task<string> GetAsync(string key)
+        {
+            return await SecureStorage.Default.GetAsync(key);
+        }
+
+        public static async Task SaveAsync(LoginResult loginResult)
+        {
+            await SetAsync(AccessTokenKey, loginResult.AccessToken);
+            await SetAsync(RefreshTokenKey, loginResult.RefreshToken);
+        }
+
+        public static async Task<string> GetAccessTokenStringAsync()
+        {
+            return await GetAsync(AccessTokenKey);
+        }
+
+        public static async Task<JwtSecurityToken> GetAccessTokenAsync()
+        {
+            AccessToken ??= new JwtSecurityToken(await GetAccessTokenStringAsync());
+            return AccessToken;
+        }
+
+        public static async Task<JwtSecurityToken> GetRefreshTokenAsync()
+        {
+            RefreshToken ??= new JwtSecurityToken(await GetAsync(RefreshTokenKey));
+            return RefreshToken;
+        }
+
+        public static void Delete()
+        {
+            SecureStorage.Default.Remove(AccessTokenKey);
+            SecureStorage.Default.Remove(RefreshTokenKey);
+        }
+
+        private static bool IsTokenValid(JwtSecurityToken token)
+        {
+            return token.ValidTo < DateTime.UtcNow;
+        }
+
+        public static async Task<bool> IsAccessTokenValid()
+        {
+            return IsTokenValid(await GetAccessTokenAsync());
+        }
+
+        public static async Task<bool> IsRefreshTokenValid()
+        {
+            return IsTokenValid(await GetRefreshTokenAsync());
+        }
     }
 }
 
