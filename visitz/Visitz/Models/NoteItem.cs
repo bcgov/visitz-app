5cb00b09ce7c5d3576077f660d4920a17f7022f1
Todd Scharien <todd.scharien@gov.bc.ca>
2023-06-05 13:46:57 -0700

    Implement NoteItem object for Realm
    Fody/Realm requires the object to have a default constructor, so the
    copy/transformation constructor was changed into a static function.

--- visitz/Visitz/Models/NoteItem.cs
+++ visitz/Visitz/Models/NoteItem.cs
@@ -1,21 +1,38 @@
-﻿using VisitzApi.Models;
+﻿using Realms;
+using VisitzApi.Models;
 
 namespace Visitz.Models
 {
     /// <summary>
     /// The business object that would be used by the app source.
     /// </summary>
-    public class NoteItem
+    public partial class NoteItem : IRealmObject
     {
+        /// <summary>
+        /// Used app-only to associate Notes with CaseloadItems. As of 2023-06-05 the ICM API does
+        /// not return PK/FK information about notes.
+        /// </summary>
+        [Indexed]
+        public string IcmId { get; set; }
+
         public string NotePeriod { get; set; }
         public string CreatedDate { get; set; }
         public string Content { get; set; }
 
-        public NoteItem(NoteEntity note)
+        public static NoteItem FromApiEntity(string icmId, NoteEntity note)
         {
-            NotePeriod = note.NotePeriod;
-            CreatedDate = note.CreatedDate; // TODO use actual DateTime type
-            Content = note.Content;
+            return new NoteItem()
+            {
+                IcmId = icmId,
+                NotePeriod = note.NotePeriod,
+                CreatedDate = note.CreatedDate, // TODO use actual DateTime type
+                Content = note.Content,
+            };
+        }
+
+        public static IEnumerable<NoteItem> FromApiEntities(string icmId, IEnumerable<NoteEntity> noteEntities)
+        {
+            return noteEntities.Select(note => FromApiEntity(icmId, note));
         }
     }
 }
