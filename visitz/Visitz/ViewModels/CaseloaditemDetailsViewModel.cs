1719d9378a7746d4ac7b5c7db5ee84fc3fd7b885
Todd Scharien <todd.scharien@gov.bc.ca>
2023-06-07 09:06:51 -0700

    Use ViewModel.Parameters

--- visitz/Visitz/ViewModels/CaseloaditemDetailsViewModel.cs
+++ visitz/Visitz/ViewModels/CaseloaditemDetailsViewModel.cs
@@ -7,7 +7,7 @@ namespace Visitz.ViewModels
     /// <summary>
     /// The business logic for the cases and incidents details rendering goes here.
     /// </summary>
-	public partial class CaseloadItemDetailsViewModel : VisitzViewModel, IQueryAttributable
+	public partial class CaseloadItemDetailsViewModel : VisitzViewModel
     {
         public static readonly string CaseIncidentIdKey = "caseIncidentId";
 
@@ -22,13 +22,10 @@ namespace Visitz.ViewModels
         [ObservableProperty]
         public string idSubtitle;
 
-        public void ApplyQueryAttributes(IDictionary<string, object> query)
-        {
-            caseIncidentId = query[CaseIncidentIdKey] as string;
-        }
-
         public override async void PageCreated()
         {
+            caseIncidentId = Parameters[CaseIncidentIdKey] as string;
+
             using var realm = await VisitzRealm.GetAsync();
 
             CaseloadItem = realm.Find<CaseloadItem>(caseIncidentId);
