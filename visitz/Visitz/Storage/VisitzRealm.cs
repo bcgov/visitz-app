8aa15f301d8d5e751627cf63dbcba58ac54732bb
Todd Scharien <todd.scharien@gov.bc.ca>
2023-06-08 11:05:52 -0700

    Add error handling for Realm DB errors

--- visitz/Visitz/Storage/VisitzRealm.cs
+++ visitz/Visitz/Storage/VisitzRealm.cs
@@ -1,4 +1,5 @@
 ﻿using Realms;
+using Realms.Exceptions;
 
 namespace Visitz.Storage
 {
@@ -6,7 +7,7 @@ namespace Visitz.Storage
     {
         private static readonly string DbName = "visitz.realm";
 
-        public static async Task<Realm> GetAsync()
+        private static async Task<Realm> GetInstanceAsync()
         {
             return await Realm.GetInstanceAsync(new RealmConfiguration(DbName)
             {
@@ -15,6 +16,41 @@ namespace Visitz.Storage
                 ShouldDeleteIfMigrationNeeded = true
 #endif
             });
+        }
+
+        private static async Task<Realm> ErrorNewInstanceAsync(Exception ex)
+        {
+#if DEBUG
+            Console.WriteLine(ex.StackTrace);
+#endif
+
+            await Application.Current.MainPage.DisplayAlert(
+                Resources.Localization.LocalizedStrings.RealmDatabaseErrorTitle,
+                Resources.Localization.LocalizedStrings.RealmDatabaseErrorMessage,
+                Resources.Localization.LocalizedStrings.Ok);
+
+            Realm.DeleteRealm(new RealmConfiguration(DbName));
+            return await GetInstanceAsync();
+        }
+
+        public static async Task<Realm> GetAsync()
+        {
+            try
+            {
+                return await GetInstanceAsync();
+            }
+            catch (RealmMismatchedConfigException ex)
+            {
+                return await ErrorNewInstanceAsync(ex);
+            }
+            catch (RealmDecryptionFailedException ex)
+            {
+                return await ErrorNewInstanceAsync(ex);
+            }
+            catch (RealmInvalidDatabaseException ex)
+            {
+                return await ErrorNewInstanceAsync(ex);
+            }
         }
     }
 }
