992018554ac57efb3b154726eed6ae4940b73998
Todd Scharien <todd.scharien@gov.bc.ca>
2023-06-08 10:24:30 -0700

    Implement encryption key handling for Realm

---
+++ visitz/Visitz/Storage/VisitzKey.cs
@@ -0,0 +1,54 @@
+﻿using System.Security.Cryptography;
+
+namespace Visitz.Storage
+{
+    public static class VisitzKey
+    {
+        private static readonly int KeySize = 64;
+
+        private static readonly string EncryptionKeyName = "visitz.encryption.key";
+
+        private static byte[] NewKey()
+        {
+            var encryptionKey = new byte[KeySize];
+            using var rng = RandomNumberGenerator.Create();
+
+            rng.GetBytes(encryptionKey);
+            return encryptionKey;
+        }
+
+        private static async Task SetKeyInStorage(byte[] encryptionKey)
+        {
+            var encodedKey = Convert.ToBase64String(encryptionKey);
+
+            await SecureStorage.Default.SetAsync(EncryptionKeyName, encodedKey);
+        }
+
+        private static async Task<byte[]> GetKeyFromStorage()
+        {
+            var encodedKey = await SecureStorage.Default.GetAsync(EncryptionKeyName);
+
+            return encodedKey != null 
+                ? Convert.FromBase64String(encodedKey)
+                : null;
+        }
+
+        public static async Task<byte[]> GetKey()
+        {
+            byte[] encryptionKey = await GetKeyFromStorage();
+
+            if (encryptionKey == null)
+            {
+                await SetKeyInStorage(NewKey());
+                encryptionKey = await GetKeyFromStorage();
+            }
+
+            return encryptionKey;
+        }
+
+        public static void RemoveKey()
+        {
+            SecureStorage.Default.Remove(EncryptionKeyName);
+        }
+    }
+}
