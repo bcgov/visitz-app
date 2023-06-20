9665bdf089e1b845bbef33f2e684adb63348e1d3
Todd Scharien <todd.scharien@gov.bc.ca>
2023-06-20 12:21:45 -0700

    Create messages for services

---
+++ visitz/Visitz/Services/Messages/ServiceInfoMessage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Visitz.Services
+{
+    public class ServiceInfoMessage
+    {
+        public string ServiceId { get; set; }
+
+        public Type ServiceType { get; set; }
+    }
+}
---
+++ visitz/Visitz/Services/Messages/StartServiceMessage.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Visitz.Services.Messages
+{
+    public class StartServiceMessage : ServiceInfoMessage
+    {
+        public object Payload { get; set; }
+    }
+}
