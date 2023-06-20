e739aa18bee84e9421474434935b077c661cf6dc
Todd Scharien <todd.scharien@gov.bc.ca>
2023-06-20 14:19:29 -0700

    Move dictionary guarding code to TryRunServiceAsync()

--- visitz/Visitz/Services/ServiceHandler.cs
+++ visitz/Visitz/Services/ServiceHandler.cs
@@ -19,29 +19,35 @@ namespace Visitz.Services
 
         public async Task TryRunServiceAsync(StartServiceMessage startMessage)
         {
-            await RunServiceAsync(startMessage);
-        }
-
-        public async Task RunServiceAsync(StartServiceMessage startMessage)
-        {
             // Only allow 1 service per ID at a time. For now, ServiceHandler shouldn't need to worry about
             // queueing services or distributing workloads.
             if (!Services.ContainsKey(startMessage.ServiceId))
             {
                 try
                 {
-                    var service = (VisitzService)ServiceProvider.Current.GetRequiredService(startMessage.ServiceType);
-                    service.Payload = startMessage.Payload;
-                    Services[startMessage.ServiceId] = service;
-                
-                    await service.OnStartAsync();
-                    await service.OnRunAsync();
-                    await service.OnFinishAsync();
+                    await RunServiceAsync(startMessage);
                 }
                 finally
                 {
                     Services.Remove(startMessage.ServiceId);
                 }
+            }
+        }
+
+        public async Task RunServiceAsync(StartServiceMessage startMessage)
+        {
+            var service = (VisitzService)ServiceProvider.Current.GetRequiredService(startMessage.ServiceType);
+            service.Payload = startMessage.Payload;
+            Services[startMessage.ServiceId] = service;
+
+            try
+            {
+                await service.OnStartAsync();
+                await service.OnRunAsync();
+            }
+            finally
+            {
+                await service.OnFinishAsync();
             }
         }
 
