ed8699adee74cf6607874ce0919b6e859586adef
Todd Scharien <todd.scharien@gov.bc.ca>
2023-06-20 14:34:59 -0700

    Replace CaseloadViewModel impl with service invocation

--- visitz/Visitz/ViewModels/CaseloadViewModel.cs
+++ visitz/Visitz/ViewModels/CaseloadViewModel.cs
@@ -1,78 +1,38 @@
 ﻿using CommunityToolkit.Mvvm.Input;
 using CommunityToolkit.Mvvm.ComponentModel;
-using VisitzApi;
 using Visitz.Models;
-using VisitzApi.ErrorHandling;
 using Visitz.Pages;
 using Visitz.Storage;
 using Visitz.Authentication.Keycloak;
+using CommunityToolkit.Mvvm.Messaging;
+using Visitz.Services;
 
 namespace Visitz.ViewModels
 {
     /// <summary>
     /// The business logic for the cases and incidents list rendering goes here.
     /// </summary>
-    public partial class CaseloadViewModel : VisitzViewModel
+    public partial class CaseloadViewModel : VisitzViewModel, IRecipient<ServiceStateMessage>
     {
         [ObservableProperty]
         public IEnumerable<CaseloadItem> caseload;
-
-        private Vpi Vpi { get; set; }
 
         [ObservableProperty]
         public bool isRefreshing;
 
         public override async void PageCreated()
         {
-            Vpi = VisitzApp.VisitzServices.GetRequiredService<Vpi>();
+            WeakReferenceMessenger.Default.Register(this, GetCaseloadService.MakeId());
 
             var realm = await IcmDataRealm.GetAsync();
             Caseload = realm.All<CaseloadItem>();
         }
 
-        public async Task TryFetchCasesAndIncidents()
-        {
-            if (await VisitzSession.GetValidSessionAsync())
-                await FetchCasesAndIncidents();
-        }
-
-        public async Task FetchCasesAndIncidents()
-        {
-            try
-            {
-                var info = await VisitzSessionInfo.GetAsync();
-                var caseloadFromApi = await Vpi.GetCaseloadAsync(info.Idir);
-                var caseloadContent = CaseloadItem.FromApiEntities(caseloadFromApi);
-
-                using var realm = await IcmDataRealm.GetAsync();
-                await realm.WriteAsync(() =>
-                {
-                    realm.Add(caseloadContent, update: true);
-                });
-            }
-            catch (VisitzApiException ex)
-            {
-                // TODO: Implement proper error logging/handling (show to user? store errors somewhere?)
-                Console.WriteLine(ex.Message);
-            }
-            catch (Exception ex)
-            {
-                // TODO: Implement proper error logging/handling (show to user? store errors somewhere?)
-                Console.WriteLine(ex.StackTrace);
-            }
-        }
-
         [RelayCommand]
         public async Task RefreshCaseload()
         {
-            try
-            {
-                await TryFetchCasesAndIncidents();
-            }
-            finally
-            {
-                IsRefreshing = false;
-            }
+            var info = await VisitzSessionInfo.GetAsync();
+            WeakReferenceMessenger.Default.Send(GetCaseloadService.MakeStartMessage(info.Idir));
         }
 
         [RelayCommand]
@@ -85,6 +45,12 @@ namespace Visitz.ViewModels
         {
             if (DebugOptions.Enabled)
                 await NavigateTo<DebugOptionsPage>();
+        }
+
+        public void Receive(ServiceStateMessage message)
+        {
+            IsRefreshing = message.Status == VisitzService.State.StartPending 
+                        || message.Status == VisitzService.State.Running;
         }
     }
 }
