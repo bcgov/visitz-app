b3883e7e40925ebe5b786f1f74f352f0240b864e
Todd Scharien <todd.scharien@gov.bc.ca>
2023-06-20 12:13:31 -0700

    Create base "VisitzApiService" class

---
+++ visitz/Visitz/Services/VisitzApiService.cs
@@ -0,0 +1,32 @@
+﻿using System.Net;
+using Visitz.Authentication.Keycloak;
+using VisitzApi.ErrorHandling;
+
+namespace Visitz.Services
+{
+    public abstract class VisitzApiService : VisitzService
+    {
+        public override async Task OnRunAsync()
+        {
+            try
+            {
+                await base.OnRunAsync();
+            }
+            catch (VisitzApiException ex)
+            {
+                if (ex.HttpStatusCode == HttpStatusCode.Unauthorized
+                    || ex.HttpStatusCode == HttpStatusCode.Forbidden)
+                {
+                    VisitzSession.InvalidateSession();
+
+                    // TODO: Properly notify user their session is invalid and
+                    // prompt them to login again
+#if DEBUG
+                    Console.WriteLine(nameof(VisitzApiException) + $" {ex.HttpStatusCode} error");
+                    Console.WriteLine(ex.StackTrace);
+#endif
+                }
+            }
+        }
+    }
+}
