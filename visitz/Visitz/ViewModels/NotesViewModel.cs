34101adcd63c5cdc2d49f0b61250f51db0792973
Todd Scharien <todd.scharien@gov.bc.ca>
2023-06-20 16:15:45 -0700

    Replace NotesViewModel impl with service invocation

--- visitz/Visitz/ViewModels/NotesViewModel.cs
+++ visitz/Visitz/ViewModels/NotesViewModel.cs
@@ -1,22 +1,19 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
-using Visitz.Authentication.Keycloak;
 using Visitz.Models;
 using Visitz.Storage;
 using Visitz.Pages;
-using VisitzApi;
-using VisitzApi.ErrorHandling;
+using CommunityToolkit.Mvvm.Messaging;
+using Visitz.Services;
 
 namespace Visitz.ViewModels
 {
     /// <summary>
     /// The business logic for the cases notes rendering goes here.
     /// </summary>
-	public partial class NotesViewModel : VisitzViewModel
+	public partial class NotesViewModel : VisitzViewModel, IRecipient<ServiceStateMessage>
     {
         public static readonly string CaseIncidentIdKey = "caseIncidentId";
-
-        private IQueryable<NoteItem> NotesQuery;
 
         public string caseIncidentId;
 
@@ -26,68 +23,39 @@ namespace Visitz.ViewModels
         [ObservableProperty]
         public IEnumerable<NoteItem> notes;
 
-        private Vpi Vpi { get; }
-
-        public NotesViewModel(Vpi visitzApi)
-        {
-            Vpi = visitzApi;
-        }
-
         public override async void PageCreated()
         {
             caseIncidentId = Parameters[CaseIncidentIdKey] as string;
+
+            WeakReferenceMessenger.Default.Register(this, GetNotesService.MakeId(caseIncidentId));
 
             var realm = await IcmDataRealm.GetAsync();
 
             CaseIncident = realm.Find<CaseloadItem>(caseIncidentId);
 
-            Notes = NotesQuery = realm
+            Notes = realm
                 .All<NoteItem>()
                 .Where(note => note.IcmId == caseIncidentId);
 
-            await TryFetchNotes();
+            // TODO: Replace FetchNotes() with IsRefreshing implementation, like CaseloadViewModel
+            FetchNotes();
         }
 
-        private async Task TryFetchNotes()
+        private void FetchNotes()
         {
-            if (await VisitzSession.GetValidSessionAsync())
-                await FetchNotes();
-        }
-
-        private async Task FetchNotes()
-        {
-            try
-            {
-                var notesFromApi = await Vpi.GetNotesAsync(CaseIncident.CaseIncidentNumber, CaseIncident.EntityType);
-                var notes = NoteItem.FromApiEntities(CaseIncident.CaseIncidentNumber, notesFromApi);
-
-                using var realm = await IcmDataRealm.GetAsync();
-                await realm.WriteAsync(() =>
-                {
-                    // NOTE 2023-06-05: The ICM API currently does not return PK info about notes.
-                    // We can only associate them by the entity ID and the actual content of the
-                    // note. So instead of using upserts, we're removing-then-adding new notes
-                    // that come in.
-                    realm.RemoveRange(NotesQuery);
-                    realm.Add(notes);
-                });
-            }
-            catch (VisitzApiException ex)
-            {
-                // TODO: Implement proper error logging/handling (show to user? store errors somewhere?)
-                Console.WriteLine(ex.Message);
-            }
-            catch (Exception ex)
-            {
-                // TODO: Implement proper error logging/handling (show to user? store errors somewhere?)
-                Console.WriteLine(ex.StackTrace);
-            }
+            var msg = GetNotesService.MakeStartMessage(caseIncidentId, CaseIncident.EntityType);
+            WeakReferenceMessenger.Default.Send(msg);
         }
 
         [RelayCommand]
         public async Task CaseDetailsTapped()
         {
             await CaseloadItemDetailsPage.Open(VisitzPage, CaseIncident.CaseIncidentNumber);
+        }
+
+        public void Receive(ServiceStateMessage message)
+        {
+            // TODO: IsRefreshing implementation, like CaseloadViewModel
         }
     }
 }
