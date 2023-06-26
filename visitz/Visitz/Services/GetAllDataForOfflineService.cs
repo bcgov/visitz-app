efb5d6c377eb65f625d8994e850f7f4a4227c888
Todd Scharien <todd.scharien@gov.bc.ca>
2023-06-26 12:45:07 -0700

    Run in separate thread to avoid Realm issues

--- visitz/Visitz/Services/GetAllDataForOfflineService.cs
+++ visitz/Visitz/Services/GetAllDataForOfflineService.cs
@@ -37,8 +37,11 @@ namespace Visitz.Services
 
         protected override async Task RunAsync()
         {
-            await GetCaseload();
-            await GetAllNotes();
+            await Task.Run(async () =>
+            {
+                await GetCaseload();
+                await GetAllNotes();
+            });
         }
 
         private async Task GetCaseload()
