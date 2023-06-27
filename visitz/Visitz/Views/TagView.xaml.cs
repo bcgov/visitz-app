f2c60ef4b57d686f6cc782bed47e6e74d13f1c92
Mohamed Afsar <afsar.shakeer@ca.ey.com>
2023-06-27 14:26:19 -0700

    feat: Create and integrate TagView

--- visitz/Visitz/Views/TagView.xaml.cs
+++ visitz/Visitz/Views/TagView.xaml.cs
@@ -4,6 +4,39 @@ namespace Visitz.Views;
 
 public partial class TagView : ContentView
 {
+    public static readonly new BindableProperty BackgroundColorProperty =
+        BindableProperty.Create(nameof(BackgroundColor), typeof(Color), typeof(TagView));
+    public static readonly BindableProperty TextColorProperty =
+        BindableProperty.Create(nameof(TextColor), typeof(Color), typeof(TagView));
+    public static readonly BindableProperty IconNameProperty =
+        BindableProperty.Create(nameof(IconName), typeof(string), typeof(TagView));
+    public static readonly BindableProperty TextProperty =
+        BindableProperty.Create(nameof(Text), typeof(string), typeof(TagView));
+
+    public new Color BackgroundColor
+    {
+        get => (Color)GetValue(BackgroundColorProperty);
+        set => SetValue(BackgroundColorProperty, value);
+    }
+
+    public Color TextColor
+    {
+        get => (Color)GetValue(TextColorProperty);
+        set => SetValue(TextColorProperty, value);
+    }
+
+    public string IconName
+    {
+        get => (string)GetValue(IconNameProperty);
+        set => SetValue(IconNameProperty, value);
+    }
+
+    public string Text
+    {
+        get => (string)GetValue(TextProperty);
+        set => SetValue(TextProperty, value);
+    }
+
     public TagView()
     {
         InitializeComponent();
