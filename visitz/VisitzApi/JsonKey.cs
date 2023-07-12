18ed8ef0e399bf8edabf601a27d53eee84a2f5f8
Todd Scharien <todd.scharien@gov.bc.ca>
2023-07-12 15:16:03 -0700

    Add "status" JsonKey

--- visitz/VisitzApi/JsonKey.cs
+++ visitz/VisitzApi/JsonKey.cs
@@ -4,6 +4,6 @@
     {
         public static readonly string DocRequest = "docRequest";
         public static readonly string Payload = "payLoad";
-
+        public static readonly string Status = "status";
     }
 }
