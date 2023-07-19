193770a62f1965ba83b3073c818985d7392723e6
Todd Scharien <todd.scharien@gov.bc.ca>
2023-07-18 18:31:40 -0700

    Fix and refactor WebBrowserAuthenticator
    - Defaults to use RedirectUri if "EndUrl" is empty
    - Added comment about enabling incognito logins

--- visitz/Visitz/Authentication/Keycloak/WebBrowserAuthenticator.cs
+++ visitz/Visitz/Authentication/Keycloak/WebBrowserAuthenticator.cs
@@ -1,6 +1,7 @@
 ﻿using IdentityModel.Client;
 using IdentityModel.OidcClient.Browser;
 using Visitz.Resources.Localization;
+using Visitz.Settings;
 
 namespace Visitz.Authentication.Keycloak
 {
@@ -14,11 +15,29 @@ namespace Visitz.Authentication.Keycloak
         {
             try
             {
-                WebAuthenticatorResult result = await WebAuthenticator.Default.AuthenticateAsync(
-                    new Uri(options.StartUrl),
-                    new Uri(options.EndUrl));
+                var callbackUrl = options.EndUrl?.Length > 0 
+                    ? options.EndUrl 
+                    : new AppSettings().Oidc.RedirectUri;
 
-                var url = new RequestUrl(options.EndUrl)
+                WebAuthenticatorResult result = await WebAuthenticator.Default.AuthenticateAsync(new()
+                {
+                    Url = new Uri(options.StartUrl),
+                    CallbackUrl = new Uri(callbackUrl),
+
+                    /*
+                        Right now, the best way to accomodate a "real" logout feature is to enable this option.
+                        Keeping it disabled for now because, at worst, a user has to go through the login process 
+                        every 30 minutes: enter work email, redirect to siteminder, enter gov email login, approve
+                        login via authenticator (potentially a phone call), then they'd choose "remember me" even
+                        though we would immediately dump the cookie because of this setting.
+
+                        So while we should be using this setting, I think we shouldn't enable it until we implement
+                        FIDO2 logins.
+                     */
+                    PrefersEphemeralWebBrowserSession = false,
+                });
+
+                var url = new RequestUrl(callbackUrl)
                     .Create(new Parameters(result.Properties));
 
                 return new BrowserResult
