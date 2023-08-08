194b764436969f88f8caca61a47bc427cd69e95d
Todd Scharien <todd.scharien@gov.bc.ca>
2023-08-08 13:40:42 -0700

    Implement EntitySubTypeTagBehavior

---
+++ visitz/Visitz/Behaviors/Tags/EntitySubTypeTagBehavior.cs
@@ -0,0 +1,19 @@
+﻿using Visitz.Models;
+using Visitz.Resources.Styles;
+using Visitz.Views;
+
+namespace Visitz.Behaviors;
+
+public class EntitySubTypeTagBehavior : TagStyleBehavior
+{
+    protected override void ApplyTagStyle(TagView tag)
+    {
+        if (tag.BindingContext is not CaseloadItem item)
+            return;
+
+        tag.Text = item.CaseIncidentType;
+
+        tag.BackgroundColor = Colors.Transparent;
+        tag.BorderColor = VisitzColors.BC_Blue;
+    }
+}
