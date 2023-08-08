23e595a86d1dc03f7f4ea40b6b18c3d4c923c109
Todd Scharien <todd.scharien@gov.bc.ca>
2023-08-08 13:25:26 -0700

    Implement TagStyle base behavior

---
+++ visitz/Visitz/Behaviors/Tags/TagStyleBehavior.cs
@@ -0,0 +1,38 @@
+﻿using Visitz.Extensions;
+using Visitz.Resources.Styles;
+using Visitz.Views;
+
+namespace Visitz.Behaviors;
+
+public abstract class TagStyleBehavior : Behavior<TagView>
+{
+    public Color BackgroundColor { get; set; } = Colors.Transparent;
+
+    public Color BorderColor { get; set; } = Colors.Black;
+
+    public string IconName { get; set; }
+
+    public Color TextColor { get; set; } = VisitzColors.BC_TextColor;
+
+    protected TagStyleBehavior() { }
+
+    protected override void OnAttachedTo(TagView tag)
+    {
+        base.OnAttachedTo(tag);
+        tag.BindingContextChanged += TagView_OnBindingContextChanged;
+        ApplyTagStyle(tag);
+    }
+
+    protected override void OnDetachingFrom(TagView tag)
+    {
+        base.OnDetachingFrom(tag);
+        tag.BindingContextChanged -= TagView_OnBindingContextChanged;
+    }
+
+    private void TagView_OnBindingContextChanged(object sender, EventArgs e)
+    {
+        ApplyTagStyle(sender as TagView);
+    }
+
+    protected abstract void ApplyTagStyle(TagView tag);
+}
