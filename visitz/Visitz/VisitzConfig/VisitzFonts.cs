022a3d8180446959dc2cdba46dc5e70fc76d3a78
Todd Scharien <todd.scharien@gov.bc.ca>
2023-08-09 15:58:18 -0700

    Rename font files for android naming rules

--- visitz/Visitz/VisitzConfig/VisitzFonts.cs
+++ visitz/Visitz/VisitzConfig/VisitzFonts.cs
@@ -2,16 +2,16 @@
 {
     public static class VisitzFonts
     {
-        private static readonly string BcSansBoldFile = "BCSans-Bold.ttf";
+        private static readonly string BcSansBoldFile = "bcsans_bold.ttf";
         private static readonly string BcSansBoldAlias = "BCSansBold";
 
-        private static readonly string BcSansBoldItalicFile = "BCSans-BoldItalic.ttf";
+        private static readonly string BcSansBoldItalicFile = "bcsans_bolditalic.ttf";
         private static readonly string BcSansBoldItalicAlias = "BCSansBoldItalic";
 
-        private static readonly string BcSansItalicFile = "BCSans-Italic.ttf";
+        private static readonly string BcSansItalicFile = "bcsans_italic.ttf";
         private static readonly string BcSansItalicAlias = "BCSansItalic";
 
-        private static readonly string BcSansRegularFile = "BCSans-Regular.ttf";
+        private static readonly string BcSansRegularFile = "bcsans_regular.ttf";
         private static readonly string BcSansRegularAlias = "BCSansRegular";
 
         public static MauiAppBuilder ConfigureVisitzFonts(this MauiAppBuilder builder)
