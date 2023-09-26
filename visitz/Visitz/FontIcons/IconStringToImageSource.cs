fa86ffe350a2a9330e6487826b391bbfc1793bab
Todd Scharien <todd.scharien@gov.bc.ca>
2023-09-26 15:58:01 -0700

    Add convenience wrappers for icon manipulation and glyphs

---
+++ visitz/Visitz/FontIcons/IconStringToImageSource.cs
@@ -0,0 +1,14 @@
+﻿namespace Visitz.FontIcons;
+
+public static class IconStringToImageSource
+{
+    public static ImageSource Make(string glyph, string fontFamily, Color color = null)
+    {
+        return new FontImageSource()
+        {
+            Glyph = glyph,
+            FontFamily = fontFamily,
+            Color = color ?? Colors.Black,
+        };
+    }
+}
