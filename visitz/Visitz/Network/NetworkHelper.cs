65735e1238d2778cc70bf7cd5b64ddf8af999cb3
Todd Scharien <todd.scharien@gov.bc.ca>
2023-09-28 09:40:48 -0700

    Add internet connection assertion utility

--- visitz/Visitz/Network/NetworkHelper.cs
+++ visitz/Visitz/Network/NetworkHelper.cs
@@ -1,6 +1,16 @@
-﻿namespace Visitz.Network;
+﻿using Visitz.Resources.Localization;
+
+namespace Visitz.Network;
 
 public static class NetworkHelper
 {
     public static bool InternetAvailable => Connectivity.Current.NetworkAccess == NetworkAccess.Internet;
+
+    public static void AssertInternetAvailable(string messageIfUnavailable = null)
+    {
+        messageIfUnavailable ??= LocalizedStrings.NoInternet;
+
+        if (!InternetAvailable)
+            throw new InternetUnavailableException(messageIfUnavailable);
+    }
 }
