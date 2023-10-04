a9eff9c13b44fd6fb559578c84b1812d034e02c1
Todd Scharien <todd.scharien@gov.bc.ca>
2023-10-04 11:34:02 -0700

    Add convenience class to register iOS handlers

---
+++ visitz/Visitz/Platforms/iOS/IOSHandlers.cs
@@ -0,0 +1,30 @@
+﻿#if IOS
+
+namespace Visitz.Platforms.iOS;
+
+public class IOSHandlers
+{
+    public static void RegisterHandlers()
+    {
+        RemoveInputAccessoryViewFromEditor();
+    }
+
+    /// <summary>
+    /// <para>MAUI adds a "done" bar above the soft keyboard, reducing screen availability for "dismiss" functionality
+    /// that is already present on the soft keyboard. This mapping removes that bar.</para>
+    /// 
+    /// <para>iPhone's soft keyboard apparently does not have the OS "dismiss" functionality, so we may need to revisit
+    /// this in the future.</para>
+    /// 
+    /// https://github.com/dotnet/maui/issues/17768#issuecomment-1744244568
+    /// </summary>
+    private static void RemoveInputAccessoryViewFromEditor()
+    {
+        Microsoft.Maui.Handlers.EditorHandler.Mapper.AppendToMapping("EditorChange", (handler, view) =>
+        {
+            handler.PlatformView.InputAccessoryView = null;
+        });
+    }
+}
+
+#endif
