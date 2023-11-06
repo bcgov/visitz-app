382d3c3f2a6ad320acf6ad7fb02ec88fc8de0ec4
Todd Scharien <todd.scharien@gov.bc.ca>
2023-11-06 12:30:18 -0800

    Fix font files' extension typos

--- visitz/Visitz/FontIcons/FluentIcons.cs
+++ visitz/Visitz/FontIcons/FluentIcons.cs
@@ -4,7 +4,7 @@ public static partial class FluentIcons
 {
     public readonly struct FontConfig
     {
-        public static readonly string Filepath = "fluentsystemicons_resizable.otf";
+        public static readonly string Filepath = "fluentsystemicons_resizable.ttf";
         public static readonly string FontFamily = "FluentIconsRegular";
     }
 
