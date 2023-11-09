2ac7805fa8306565561da63ca4fbe57fdf44929f
Todd Scharien <todd.scharien@gov.bc.ca>
2023-11-09 13:42:27 -0800

    Show subtype initials

--- visitz/Visitz/Views/CaseloadItemView.xaml.cs
+++ visitz/Visitz/Views/CaseloadItemView.xaml.cs
@@ -1,3 +1,4 @@
+using Visitz.Extensions;
 using Visitz.Models;
 
 namespace Visitz.Views;
@@ -16,5 +17,15 @@ public partial class CaseloadItemView : ContentView
         var item = (CaseloadItem)BindingContext;
 
         OpenDateLabel.IsVisible = item.DisplayDate?.Length > 0;
+
+        if (item.CaseIncidentType?.Length > 2)
+        {
+            var initials = item.CaseIncidentType.GetInitials();
+
+            if (initials.Length > 2)
+                initials = initials[..2];
+
+            SubtypeLabel.Text = initials;
+        }
     }
 }
