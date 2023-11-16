7df0896a5f2fd8b9b2755c829dab110d0c857b24
Todd Scharien <todd.scharien@gov.bc.ca>
2023-11-16 15:44:16 -0800

    Rework/redesign ContactItemView

--- visitz/Visitz/Views/ContactItemView.xaml.cs
+++ visitz/Visitz/Views/ContactItemView.xaml.cs
@@ -1,7 +1,3 @@
-using Visitz.Extensions;
-using Visitz.Models;
-using Visitz.Resources.Styles;
-
 namespace Visitz.Views;
 
 public partial class ContactItemView : ContentView
@@ -10,30 +6,4 @@ public partial class ContactItemView : ContentView
 	{
 		InitializeComponent();
 	}
-
-    protected override void OnBindingContextChanged()
-    {
-        base.OnBindingContextChanged();
-        UpdateUI();
-    }
-
-    private void UpdateUI()
-    {
-        var familyMember = BindingContext as FamilyMember;
-
-        CellPhoneTagView.IsVisible = familyMember.CellPhone?.Length > 0;
-        HomePhoneTagView.IsVisible = familyMember.HomePhone?.Length > 0;
-        PhoneRow.IsVisible = CellPhoneTagView.IsVisible || HomePhoneTagView.IsVisible;
-
-        if (familyMember.IsKeyPlayer)
-        {
-            ContainerBorder.BackgroundColor = VisitzColors.KeyPlayerInfoPurpleBackground;
-            ContainerBorder.Stroke = VisitzColors.ContactRelationshipTagText;
-        }
-        else
-        {
-            ContainerBorder.BackgroundColor = Colors.White;
-            ContainerBorder.Stroke = VisitzColors.FamilyMemberInfoGrayBorder;
-        }
-    }
 }
