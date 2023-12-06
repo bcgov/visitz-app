39a5362854123170a353bfc4256be811a40123fb
Todd Scharien <todd.scharien@gov.bc.ca>
2023-12-05 16:31:01 -0800

    Convert SegmentedOptions to readonly and implement operators

--- visitz/Visitz/Views/SegmentedButtons/SegmentedOptions.cs
+++ visitz/Visitz/Views/SegmentedButtons/SegmentedOptions.cs
@@ -1,10 +1,36 @@
-﻿namespace Visitz.Views.SegmentedButtons;
+﻿
+namespace Visitz.Views.SegmentedButtons;
 
-public struct SegmentedOptions
+public readonly struct SegmentedOptions(string id, string text, ImageSource imageSource)
 {
-    public string Id { get; set; }
+    public static readonly SegmentedOptions Empty = new();
 
-    public string Text { get; set; }
+    public readonly string Id { get; } = id;
 
-    public ImageSource ImageSource { get; set; }
+    public readonly string Text { get; } = text;
+
+    public readonly ImageSource ImageSource { get; } = imageSource;
+
+    public override readonly bool Equals(object obj)
+    {
+        return obj is SegmentedOptions opts &&
+               Id == opts.Id &&
+               Text == opts.Text &&
+               EqualityComparer<ImageSource>.Default.Equals(ImageSource, opts.ImageSource);
+    }
+
+    public override readonly int GetHashCode()
+    {
+        return HashCode.Combine(Id, Text, ImageSource);
+    }
+
+    public static bool operator ==(SegmentedOptions a, SegmentedOptions b)
+    {
+        return a.Equals(b);
+    }
+
+    public static bool operator !=(SegmentedOptions a, SegmentedOptions b)
+    {
+        return !a.Equals(b);
+    }
 }
