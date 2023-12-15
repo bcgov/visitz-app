a2645f72851244840d53ddac3021f998393b2bec
Todd Scharien <todd.scharien@gov.bc.ca>
2023-12-14 18:00:37 -0800

    Create FormCheckBox
    The vanilla checkbox doesn't intrinsically come with label support. This
    component includes a label that allows toggling the checkbox on tap.

---
+++ visitz/Visitz/Views/FormControls/FormCheckBox.xaml.cs
@@ -0,0 +1,32 @@
+namespace Visitz.Views.FormControls;
+
+public partial class FormCheckBox : ContentView
+{
+	public static readonly BindableProperty IsCheckedProperty =
+		BindableProperty.Create(nameof(IsChecked), typeof(bool), typeof(FormCheckBox));
+
+    public static readonly BindableProperty TextProperty =
+        BindableProperty.Create(nameof(Text), typeof(string), typeof(FormCheckBox));
+
+    public bool IsChecked
+	{
+		get => (bool)GetValue(IsCheckedProperty);
+		set => SetValue(IsCheckedProperty, value);
+	}
+
+    public string Text
+    {
+        get => (string)GetValue(TextProperty);
+        set => SetValue(TextProperty, value);
+    }
+
+    public FormCheckBox()
+	{
+		InitializeComponent();
+	}
+
+    private void TapGestureRecognizer_Tapped(object sender, TappedEventArgs e)
+    {
+		IsChecked = !IsChecked;
+    }
+}
