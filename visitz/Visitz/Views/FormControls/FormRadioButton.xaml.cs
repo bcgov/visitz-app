cc96b5be2b78e920c87566e9527737183603bb08
Todd Scharien <todd.scharien@gov.bc.ca>
2023-12-19 13:56:50 -0800

    Extend IsChecked for FormRadioButton

--- visitz/Visitz/Views/FormControls/FormRadioButton.xaml.cs
+++ visitz/Visitz/Views/FormControls/FormRadioButton.xaml.cs
@@ -11,6 +11,10 @@ public partial class FormRadioButton : ContentView
 	public static readonly BindableProperty ValueProperty =
 		BindableProperty.Create(nameof(Value), typeof(object), typeof(FormRadioButton));
 
+    public static readonly BindableProperty IsCheckedProperty =
+        BindableProperty.Create(nameof(Value), typeof(bool), typeof(FormRadioButton),
+            defaultBindingMode: BindingMode.TwoWay);
+
     public string Text
     {
         get => (string)GetValue(TextProperty);
@@ -27,6 +31,12 @@ public partial class FormRadioButton : ContentView
     {
         get => GetValue(ValueProperty);
         set => SetValue(ValueProperty, value);
+    }
+
+    public object IsChecked
+    {
+        get => GetValue(IsCheckedProperty);
+        set => SetValue(IsCheckedProperty, value);
     }
 
     public FormRadioButton()
