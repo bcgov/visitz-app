7848b8a1d582aef12634775586abacaf169bc443
Todd Scharien <todd.scharien@gov.bc.ca>
2023-12-19 14:02:08 -0800

    Add convenience conversion property for EntitySafetyAssessViewModel

--- visitz/Visitz/Views/Entity/EntitySafetyAssessView.xaml.cs
+++ visitz/Visitz/Views/Entity/EntitySafetyAssessView.xaml.cs
@@ -4,10 +4,12 @@ namespace Visitz.Views.Entity;
 
 public partial class EntitySafetyAssessView : ViewModelContentView, ICaseloadItemHolder
 {
+	protected new EntitySafetyAssessViewModel ViewModel => (EntitySafetyAssessViewModel)base.ViewModel;
+
     public CaseloadItem CaseloadItem 
 	{
-		get => (ViewModel as EntitySafetyAssessViewModel).CaseloadItem;
-		set => (ViewModel as EntitySafetyAssessViewModel).CaseloadItem = value;
+		get => ViewModel.CaseloadItem;
+		set => ViewModel.CaseloadItem = value;
 	}
 
 	public EntitySafetyAssessView() : base(ServiceProvider.GetService<EntitySafetyAssessViewModel>())
