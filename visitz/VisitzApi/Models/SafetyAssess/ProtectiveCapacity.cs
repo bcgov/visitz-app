612ce847de68ff647c2ad61287ed2a152e5e16da
Todd Scharien <todd.scharien@gov.bc.ca>
2023-12-19 16:05:44 -0800

    Implement API models for Safety assessment

---
+++ visitz/VisitzApi/Models/SafetyAssess/ProtectiveCapacity.cs
@@ -0,0 +1,32 @@
+﻿namespace VisitzApi.Models.SafetyAssess;
+
+public class ProtectiveCapacityEntity
+{
+    public string ChildCognitive { get; set; }
+
+    public string ParentCognitive { get; set; }
+
+    public string ParentWillingness { get; set; }
+
+    public string ParentResources { get; set; }
+
+    public string ParentSupportive { get; set; }
+
+    public string ParentProtect { get; set; }
+
+    public string ParentAccept { get; set; }
+
+    public string ParentRelationship { get; set; }
+
+    public string ParentAware { get; set; }
+
+    public string ParentProbSolving { get; set; }
+
+    public string NoProCapPresent { get; set; }
+
+    public string CapacitiesOther { get; set; }
+
+    public string CmtProtectiveCapacity01 { get; set; }
+
+    public string CmtProtectiveCapacity02 { get; set; }
+}
---
+++ visitz/VisitzApi/Models/SafetyAssess/SafetyAssessment.cs
@@ -0,0 +1,26 @@
+﻿namespace VisitzApi.Models.SafetyAssess;
+
+public class SafetyAssessmentEntity
+{
+    public string IncidentNumber { get; set; }
+
+    public string WorkerId { get; set; }
+
+    public string FamilyName { get; set; }
+
+    public string DateOfAssessment { get; set; }
+
+    public string Operation { get; set; }
+
+    public FactorInfluenceEntity FactorInfluence { get; set; }
+
+    public SafetyFactorsEntity SafetyFactors { get; set; }
+
+    public ProtectiveCapacityEntity ProtectiveCapacity { get; set; }
+
+    public SafetyInterventionsEntity SafetyInterventions { get; set; }
+
+    public SafetyDecisionsEntity SafetyDecisions { get; set; }
+
+    public IList<string> ChildsInOutCare { get; }
+}
