4028cf38944ec26c845823f26d5ed7e82dde8d99
Todd Scharien <todd.scharien@gov.bc.ca>
2023-12-19 17:25:46 -0800

    Begin INT 622 API implementation

---
+++ visitz/VisitzApi/Requests/SubmitSafetyAssessmentEndpoint.cs
@@ -0,0 +1,46 @@
+﻿using System.Text.Json;
+using System.Text.Json.Nodes;
+using VisitzApi.Models.SafetyAssess;
+
+namespace VisitzApi.Requests;
+
+internal class SubmitSafetyAssessmentEndpoint(string baseUrl, SafetyAssessmentEntity safetyAssessment)
+    : VisitzBaseEndpoint<bool>(baseUrl, SubmitSafetyAssessmentPath)
+{
+    public static readonly string SubmitSafetyAssessmentPath = "/v1/622";
+
+    public static readonly string SafetyAssessmentKey = "safetyAssessment";
+
+    public readonly SafetyAssessmentEntity SafetyAssessment = safetyAssessment;
+
+    private string RequestPayload
+    {
+        get
+        {
+            var assessmentJson = JsonSerializer.Serialize(SafetyAssessment, PayloadOptions.Default);
+
+            return new JsonObject
+            {
+                [SafetyAssessmentKey] = new JsonObject
+                {
+                    [JsonKey.Payload] = JsonNode.Parse(assessmentJson)
+                }
+            }.ToString();
+        }
+    }
+
+    public override HttpRequestMessage MakeRequest()
+    {
+        return new HttpRequestMessage()
+        {
+            Content = new FormUrlEncodedContent(FormDataCollection(JsonKey.DocRequest, RequestPayload)),
+            Method = HttpMethod.Post,
+            RequestUri = RequestUri,
+        };
+    }
+
+    public override bool HandleResponse(string responseContent)
+    {
+        throw new NotImplementedException();
+    }
+}
