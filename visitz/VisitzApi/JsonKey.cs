886b1dbbd29e3dc1723752035ffb217418888258
Todd Scharien <todd.scharien@gov.bc.ca>
2023-12-22 11:28:41 -0800

    Rework WebMethods error parsing

--- visitz/VisitzApi/JsonKey.cs
+++ visitz/VisitzApi/JsonKey.cs
@@ -4,6 +4,7 @@
     {
         public static readonly string DocRequest = "docRequest";
         public static readonly string PayLoad = "payLoad";
+        public static readonly string Payload = "payload";
         public static readonly string Status = "status";
         public static readonly string Success = "Success";
     }
