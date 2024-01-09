79786480d2b37a0eb4f64c18e13c67f8500d2936
Todd Scharien <todd.scharien@gov.bc.ca>
2024-01-08 16:04:31 -0800

    Implement orientation responsiveness

--- visitz/Visitz/ViewModels/RootViewModel.cs
+++ visitz/Visitz/ViewModels/RootViewModel.cs
@@ -1,5 +1,39 @@
-﻿namespace Visitz.ViewModels;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
 
-internal class RootViewModel : VisitzViewModel
+namespace Visitz.ViewModels;
+
+internal partial class RootViewModel : VisitzViewModel
 {
+    [ObservableProperty]
+    public bool isLandscape = false;
+
+    [ObservableProperty]
+    public bool isPortrait = false;
+
+    public override void PageCreated()
+    {
+        base.PageCreated();
+
+        UpdateOrientationVisibility(DeviceDisplay.Current.MainDisplayInfo.Orientation);
+
+        DeviceDisplay.Current.MainDisplayInfoChanged += Current_MainDisplayInfoChanged;
+    }
+
+    public override void PageDestroyed()
+    {
+        DeviceDisplay.Current.MainDisplayInfoChanged -= Current_MainDisplayInfoChanged;
+
+        base.PageDestroyed();
+    }
+
+    private void Current_MainDisplayInfoChanged(object sender, DisplayInfoChangedEventArgs e)
+    {
+        UpdateOrientationVisibility(e.DisplayInfo.Orientation);
+    }
+
+    private void UpdateOrientationVisibility(DisplayOrientation orientation)
+    {
+        IsPortrait = orientation.Equals(DisplayOrientation.Portrait);
+        IsLandscape = !IsPortrait;
+    }
 }
