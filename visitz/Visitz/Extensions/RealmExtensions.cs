ee3f1849c69a8f6347d31c854fc588e3f055b818
Todd Scharien <todd.scharien@gov.bc.ca>
2024-01-16 11:45:29 -0800

    Add convenience functions to check if realm object is managed

---
+++ visitz/Visitz/Extensions/RealmExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Realms;
+
+namespace Visitz.Extensions;
+
+internal static class RealmExtensions
+{
+    public static void Commit(this IRealmObject realmObject, Action action)
+    {
+        if (realmObject.IsManaged)
+            realmObject.Realm.Write(action);
+        else
+            action.Invoke();
+    }
+
+    public static async Task CommitAsync(this IRealmObject realmObject, Action action)
+    {
+        if (realmObject.IsManaged)
+            await realmObject.Realm.WriteAsync(action);
+        else
+            action.Invoke();
+    }
+}
