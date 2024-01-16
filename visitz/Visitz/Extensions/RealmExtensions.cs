44ddc82994e185991bb989f8de23ae17f8461a46
Todd Scharien <todd.scharien@gov.bc.ca>
2024-01-16 13:58:14 -0800

    Invert if's and check if in transaction

--- visitz/Visitz/Extensions/RealmExtensions.cs
+++ visitz/Visitz/Extensions/RealmExtensions.cs
@@ -6,17 +6,17 @@ internal static class RealmExtensions
 {
     public static void Commit(this IRealmObject realmObject, Action action)
     {
-        if (realmObject.IsManaged)
-            realmObject.Realm.Write(action);
-        else
+        if (!realmObject.IsManaged || realmObject.Realm.IsInTransaction)
             action.Invoke();
+        else
+            realmObject.Realm.Write(action);
     }
 
     public static async Task CommitAsync(this IRealmObject realmObject, Action action)
     {
-        if (realmObject.IsManaged)
-            await realmObject.Realm.WriteAsync(action);
-        else
+        if (!realmObject.IsManaged || realmObject.Realm.IsInTransaction)
             action.Invoke();
+        else
+            await realmObject.Realm.WriteAsync(action);
     }
 }
