ed5beecb88a3d0fa50683016bfb3c129e29de92c
Todd Scharien <todd.scharien@gov.bc.ca>
2024-01-17 15:34:49 -0800

    Implement realm compiled bindings workaround

---
+++ visitz/Visitz/Models/SafetyAssess/SafetyDecisions.Binding.cs
@@ -0,0 +1,48 @@
+﻿/*
+	Partial class implementation of a Realm + compiled bindings workaround.
+
+	https://github.com/realm/realm-dotnet/issues/2270#issuecomment-786720318
+ */
+
+using Visitz.Extensions;
+
+namespace Visitz.Models.SafetyAssess;
+
+public partial class SafetyDecisions
+{
+    public SafetyDecisionOption? DecisionBinding
+    {
+        get => IsValid ? Decision : default;
+        set => this.Commit(() => Decision = value);
+    }
+
+    public string DecisionUnsafeBinding
+    {
+        get => IsValid ? DecisionUnsafe : default;
+        set => this.Commit(() => DecisionUnsafe = value);
+    }
+
+    public string CommentsBinding
+    {
+        get => IsValid ? Comments : default;
+        set => this.Commit(() => Comments = value);
+    }
+
+    public string NarrativeBinding
+    {
+        get => IsValid ? Narrative : default;
+        set => this.Commit(() => Narrative = value);
+    }
+
+    public bool ReadyFinalizeBinding
+    {
+        get => IsValid ? ReadyFinalize : default;
+        set => this.Commit(() => ReadyFinalize = value);
+    }
+
+    public DateTimeOffset ReadyFinalizeDateBinding
+    {
+        get => IsValid ? ReadyFinalizeDate : default;
+        set => this.Commit(() => ReadyFinalizeDate = value);
+    }
+}
