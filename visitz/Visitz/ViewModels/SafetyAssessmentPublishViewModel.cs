c0d141e22b0f3fd34f014703c5d4b7818e8b53f8
Todd Scharien <todd.scharien@gov.bc.ca>
2024-01-24 15:07:16 -0800

    Don't freeze assessment so draft can be discarded on successful publish

--- visitz/Visitz/ViewModels/SafetyAssessmentPublishViewModel.cs
+++ visitz/Visitz/ViewModels/SafetyAssessmentPublishViewModel.cs
@@ -15,7 +15,7 @@ internal partial class SafetyAssessmentPublishViewModel : PublishViewModel, IRec
         get => assessment;
         set
         {
-            assessment = value.Freeze();
+            assessment = value;
             var date = assessment.DateOfAssessment.ToString(SafetyAssessment.DateFormat);
 
             Title = string.Format(LocalizedStrings.PublishSATitle, assessment.FamilyName, date);
