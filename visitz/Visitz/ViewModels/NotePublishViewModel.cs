37b601acf26f664ec26be5dc14500cb255b69c05
Todd Scharien <todd.scharien@gov.bc.ca>
2024-01-24 17:15:56 -0800

    Add Init() to set up submitNoteEntity

--- visitz/Visitz/ViewModels/NotePublishViewModel.cs
+++ visitz/Visitz/ViewModels/NotePublishViewModel.cs
@@ -1,12 +1,38 @@
 ﻿using CommunityToolkit.Mvvm.Messaging;
+using Visitz.Authentication.Keycloak;
 using Visitz.Models;
 using Visitz.Services;
+using VisitzApi.Models;
 
 namespace Visitz.ViewModels
 {
-    public partial class NotePublishViewModel : PublishViewModel, IRecipient<ServiceStateMessage>, ICaseloadItemHolder
+    public partial class NotePublishViewModel : PublishViewModel, IRecipient<ServiceStateMessage>
     {
-        public CaseloadItem CaseloadItem { get; set; }
+        public CaseloadItem CaseloadItem { get; private set; }
+
+        public NoteItem NoteItem { get; private set; }
+
+        private SubmitNoteEntity submitNoteEntity;
+
+        public async void Init(CaseloadItem caseloadItem, NoteItem noteItem, string draft)
+        {
+            CaseloadItem = caseloadItem;
+            NoteItem = noteItem;
+
+            Title = noteItem?.PeriodOrPageNumber != null
+                ? $"{caseloadItem.DisplayName} • {noteItem?.PeriodOrPageNumber}"
+                : caseloadItem.DisplayName;
+
+            var info = await VisitzSessionInfo.GetAsync();
+            submitNoteEntity = new()
+            {
+                EntityNumber = caseloadItem.CaseIncidentNumber,
+                EntityType = caseloadItem.EntityType,
+                NotePeriod = noteItem?.NotePeriod ?? NoteItem.NotePeriodFrom(DateTime.Now),
+                Content = NoteItem.WrapContent(info.Idir, DateTime.Now, draft),
+                CreatedBy = info.Idir,
+            };
+        }
 
         public override void Publish()
         {
