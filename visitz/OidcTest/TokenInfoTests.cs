70269e5de651d97b691e33fd4eba66c0ddf8ed93
Todd Scharien <todd.scharien@gov.bc.ca>
2024-02-09 14:46:57 -0800

    Add tests for reading collections from TokenInfo

--- visitz/OidcTest/TokenInfoTests.cs
+++ visitz/OidcTest/TokenInfoTests.cs
@@ -10,6 +10,9 @@ public class TokenInfoTests
     const string displayNameKey = "display_name";
     const string arbitraryDisplayName = "an arbitrary display name";
 
+    const string stringCollectionClaimKey = "items";
+    const string arbitraryItem = "this collection is full of these";
+
     const string issuer = "";
     const string audience = "";
     static readonly DateTime ArbitraryNotBefore = DateTime.Parse("2024-02-09 12:00:00");
@@ -47,5 +50,33 @@ public class TokenInfoTests
         var actualName = tokenInfo.TryGet<string>(displayNameKey, out var claim) ? claim : null;
 
         Assert.Equal(arbitraryDisplayName, actualName);
+    }
+
+    [Fact]
+    public void CanCreateTokenAndReadListClaim()
+    {
+        var tokenInfo = CreateTokenInfo(ArbitraryNotBefore, ArbitraryExpires,
+        [
+            new Claim(displayNameKey, arbitraryDisplayName),
+            new Claim(stringCollectionClaimKey, arbitraryItem),
+            new Claim(stringCollectionClaimKey, arbitraryItem),
+        ]);
+
+        if (tokenInfo.TryGet<List<object>>(stringCollectionClaimKey, out var actualOutput))
+            Assert.Equal(arbitraryItem, actualOutput.FirstOrDefault());
+    }
+
+    [Fact]
+    public void CanCreateTokenAndReadJsonArrayClaim()
+    {
+        var jsonItems = JsonSerializer.Serialize(new List<string>() { arbitraryItem, arbitraryItem });
+        var tokenInfo = CreateTokenInfo(ArbitraryNotBefore, ArbitraryExpires,
+        [
+            new Claim(displayNameKey, arbitraryDisplayName),
+            new Claim(stringCollectionClaimKey, jsonItems, JsonClaimValueTypes.JsonArray),
+        ]);
+
+        if (tokenInfo.TryGet<JsonElement>(stringCollectionClaimKey, out var actualOutput))
+            Assert.Equal(arbitraryItem, actualOutput.EnumerateArray().FirstOrDefault().ToString());
     }
 }
