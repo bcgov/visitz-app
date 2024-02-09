40d07624668cb0bf57c474dc7217c87572277aa5
Todd Scharien <todd.scharien@gov.bc.ca>
2024-02-09 14:09:32 -0800

    Add OidcTest xunit project and include initial tests

---
+++ visitz/OidcTest/TokenInfoTests.cs
@@ -0,0 +1,51 @@
+﻿using Oidc;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text.Json;
+
+namespace OidcTest;
+
+public class TokenInfoTests
+{
+    const string displayNameKey = "display_name";
+    const string arbitraryDisplayName = "an arbitrary display name";
+
+    const string issuer = "";
+    const string audience = "";
+    static readonly DateTime ArbitraryNotBefore = DateTime.Parse("2024-02-09 12:00:00");
+    static readonly DateTime ArbitraryExpires = DateTime.Parse("2024-02-09 12:05:00");
+
+    static TokenInfo CreateTokenInfo(DateTime notBefore, DateTime expires, IEnumerable<Claim> claims)
+    {
+        return new TokenInfo(new JwtSecurityToken(
+            issuer: issuer,
+            audience: audience,
+            claims: claims,
+            notBefore: notBefore,
+            expires: expires));
+    }
+
+    [Fact]
+    public void CanCreateTokenWithStringClaim()
+    {
+        var tokenInfo = CreateTokenInfo(ArbitraryNotBefore, ArbitraryExpires,
+        [
+            new Claim(displayNameKey, arbitraryDisplayName),
+        ]);
+
+        Assert.NotNull(tokenInfo);
+    }
+
+    [Fact]
+    public void CanCreateAndReadTokenWithStringClaim()
+    {
+        var tokenInfo = CreateTokenInfo(ArbitraryNotBefore, ArbitraryExpires,
+        [
+            new Claim(displayNameKey, arbitraryDisplayName),
+        ]);
+
+        var actualName = tokenInfo.TryGet<string>(displayNameKey, out var claim) ? claim : null;
+
+        Assert.Equal(arbitraryDisplayName, actualName);
+    }
+}
