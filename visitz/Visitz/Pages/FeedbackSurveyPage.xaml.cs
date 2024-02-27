885eb0549d00bcaba712ddb0a7d3fe018b14f35b
Todd Scharien <todd.scharien@gov.bc.ca>
2024-02-27 15:15:22 -0800

    Open survey in web modal when button clicked

--- visitz/Visitz/Pages/FeedbackSurveyPage.xaml.cs
+++ visitz/Visitz/Pages/FeedbackSurveyPage.xaml.cs
@@ -1,6 +1,7 @@
 #if IOS
 using Microsoft.Maui.Controls.PlatformConfiguration;
 using Microsoft.Maui.Controls.PlatformConfiguration.iOSSpecific;
+using Visitz.Settings;
 #endif
 
 namespace Visitz.Pages;
@@ -14,5 +15,19 @@ public partial class FeedbackSurveyPage : ContentPage
 #endif
 
 		InitializeComponent();
+	}
+
+	private async void StartSurvey_Clicked(object sender, EventArgs e)
+	{
+		var feedbackUri = new Uri(new AppSettings().ContactInfo.FeedbackSurveyUrl);
+
+		await Navigator.Navigation.PopModalAsync();
+
+		await Browser.Default.OpenAsync(feedbackUri, new BrowserLaunchOptions
+		{
+			LaunchMode = BrowserLaunchMode.SystemPreferred,
+			TitleMode = BrowserTitleMode.Hide,
+			Flags = BrowserLaunchFlags.PresentAsFormSheet,
+		});
 	}
 }
