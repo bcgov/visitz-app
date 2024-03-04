b84fbb89715ece85bfc15ba21123afd5c8fab3f2
Todd Scharien <todd.scharien@gov.bc.ca>
2024-03-04 08:53:24 -0800

    Add DocumentHeroImage view

---
+++ visitz/Visitz/Views/DocumentHeroImage.xaml.cs
@@ -0,0 +1,11 @@
+namespace Visitz.Views;
+
+public partial class DocumentHeroImage : ContentView
+{
+	public static readonly double MaximumSize = 250;
+
+	public DocumentHeroImage()
+	{
+		InitializeComponent();
+	}
+}
