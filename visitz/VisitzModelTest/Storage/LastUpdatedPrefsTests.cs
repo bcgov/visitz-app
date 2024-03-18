34047b22b0a1847bcb18cd637fd731375e97cb02
Todd Scharien <todd.scharien@gov.bc.ca>
2024-03-18 10:25:22 -0700

    Add tests for LastUpdatedPrefs

---
+++ visitz/VisitzModelTest/Storage/LastUpdatedPrefsTests.cs
@@ -0,0 +1,64 @@
+using VisitzModel.Events;
+using VisitzModel.Extensions;
+using VisitzModel.Storage;
+
+namespace VisitzModelTest.Storage;
+
+public class LastUpdatedPrefsTests
+{
+	static readonly string ArbitraryKey = nameof(LastUpdatedPrefsTests) + ".testkey";
+
+	static LastUpdatedPrefs GetLastUpdatedPrefsMock()
+	{
+		return new(new LocalPreferencesMock());
+	}
+
+	[Fact]
+	public void DefaultValueReturnedWhenRequestedKeyMissing()
+	{
+		var luPrefs = GetLastUpdatedPrefsMock();
+		var minDate = DateTime.MinValue;
+
+		Assert.Equal(minDate, luPrefs.Get(ArbitraryKey, minDate));
+	}
+
+	[Fact]
+	public void LocalTimeStoresAndRetrievesTheSame()
+	{
+		var luPrefs = GetLastUpdatedPrefsMock();
+		var localNow = DateTimeExtensions.LocalNow;
+
+		luPrefs.Set(ArbitraryKey, localNow);
+
+		Assert.Equal(localNow, luPrefs.Get(ArbitraryKey, DateTime.MinValue));
+	}
+
+	[Fact]
+	public void NotifiedWhenValueSet()
+	{
+		var luPrefs = GetLastUpdatedPrefsMock();
+		var localNow = DateTimeExtensions.LocalNow;
+
+		Assert.Raises<LastUpdatedChangedEventArgs>(
+			handler => luPrefs.LastUpdatedChanged += handler,
+			handler => luPrefs.LastUpdatedChanged -= handler,
+			() => luPrefs.Set(ArbitraryKey, localNow));
+	}
+
+	[Fact]
+	public void ValueIsCorrectWhenNotified()
+	{
+		var luPrefs = GetLastUpdatedPrefsMock();
+		var localNow = DateTimeExtensions.LocalNow;
+		DateTime valueAfterEvent = DateTime.MinValue;
+
+		luPrefs.LastUpdatedChanged += (sender, args) =>
+		{
+			valueAfterEvent = args.NewLastUpdatedValue;
+		};
+
+		luPrefs.Set(ArbitraryKey, localNow);
+
+		Assert.Equal(localNow, valueAfterEvent);
+	}
+}
