62e907be344ce1c8d8ba2f0f38e027fa5cb60569
Todd Scharien <todd.scharien@gov.bc.ca>
2024-03-25 11:03:05 -0700

    Add EntityTypeUiExtensions

---
+++ visitz/Visitz/Extensions/EntityTypeUiExtensions.cs
@@ -0,0 +1,57 @@
+using Visitz.FontIcons;
+using Visitz.Resources.Styles;
+using VisitzModel.Models.EntityTypes;
+
+namespace VisitzModel.Extensions;
+
+public static class EntityTypeUiExtensions
+{
+	public static string GetIconGlyph(this EntityType entityType)
+	{
+		return entityType switch
+		{
+			EntityType.Case => MaterialIcons.Folder,
+			EntityType.Incident => MaterialIcons.Warning,
+			EntityType.Memo => MaterialIcons.Note_alt,
+			EntityType.ServiceRequest => MaterialIcons.Headset_mic,
+			_ => throw new NotImplementedException(),
+		};
+	}
+
+	public static Color GetTextColor(this EntityType entityType)
+	{
+		return entityType switch
+		{
+			EntityType.Case => VisitzColors.EntityCaseTagText,
+			EntityType.Incident => VisitzColors.EntityIncidentTagText,
+			EntityType.Memo => VisitzColors.EntityMemoTagText,
+			EntityType.ServiceRequest => VisitzColors.EntityServiceRequestTagText,
+			_ => throw new NotImplementedException(),
+		};
+	}
+
+	public static Color GetBackgroundColor(this EntityType entityType)
+	{
+		return entityType switch
+		{
+			EntityType.Case => VisitzColors.EntityCaseTagBackground,
+			EntityType.Incident => VisitzColors.EntityIncidentTagBackground,
+			EntityType.Memo => VisitzColors.EntityMemoTagBackground,
+			EntityType.ServiceRequest => VisitzColors.EntityServiceRequestTagBackground,
+			_ => throw new NotImplementedException(),
+		};
+	}
+
+	public static FontImageSource GetIcon(this EntityType entityType)
+	{
+		if (entityType == EntityType.Unknown || entityType > EntityType.ServiceRequest)
+			throw new NotImplementedException();
+
+		return new FontImageSource()
+		{
+			Glyph = entityType.GetIconGlyph(),
+			FontFamily = MaterialIcons.RoundedFilled.FontFamily,
+			Color = entityType.GetTextColor(),
+		};
+	}
+}
