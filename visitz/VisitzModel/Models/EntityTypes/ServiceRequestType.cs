90e5f24e3d8a6684ad8feefb4607a5ad059b3e6d
Todd Scharien <todd.scharien@gov.bc.ca>
2024-03-25 10:53:34 -0700

    Add enums for ICM types

---
+++ visitz/VisitzModel/Models/EntityTypes/ServiceRequestType.cs
@@ -0,0 +1,17 @@
+namespace VisitzModel.Models.EntityTypes;
+
+public enum ServiceRequestType
+{
+	Unknown = 0,
+	AfterHours = 1,
+	AutismFundingUnit = 2,
+	CommunityDevelopment = 3,
+	FamilyNeedsAssessment = 4,
+	MedicalBenefits = 5,
+	PostMajorityServices = 6,
+	RequestServiceCfs = 7,
+	RequestServiceCapp = 8,
+	RequestForFamilySupport = 9,
+	RequestForInformation = 10,
+	YouthServices = 11,
+}
