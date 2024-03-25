2a1edeceadedf9739a55d17878b89ab3e4afc979
Todd Scharien <todd.scharien@gov.bc.ca>
2024-03-25 11:43:04 -0700

    Use EntityTypeBadge

--- visitz/Visitz/Views/CaseloadItemView.xaml.cs
+++ visitz/Visitz/Views/CaseloadItemView.xaml.cs
@@ -1,4 +1,3 @@
-using Visitz.Resources.Styles;
 using VisitzModel.Models;
 
 namespace Visitz.Views;
@@ -17,39 +16,6 @@ public partial class CaseloadItemView : ContentView
         if (BindingContext is CaseloadItem item)
 		{
 			OpenDateLabel.IsVisible = item.DisplayDate?.Length > 0;
-
-			UpdateTagViewStyles(item);
 		}
-    }
-
-    private void UpdateTagViewStyles(CaseloadItem item)
-    {
-        TagView.StrokeThickness = 0;
-
-        if (item.EntityType == IcmEntity.Case)
-        {
-            TagView.BackgroundColor = VisitzColors.EntityCaseTagBackground;
-            TagView.TextColor = VisitzColors.EntityCaseTagText;
-        }
-        else if (item.EntityType == IcmEntity.Incident)
-        {
-            TagView.BackgroundColor = VisitzColors.EntityIncidentTagBackground;
-            TagView.TextColor = VisitzColors.EntityIncidentTagText;
-        }
-        else if (item.EntityType == IcmEntity.Memo)
-        {
-            TagView.BackgroundColor = VisitzColors.EntityMemoTagBackground;
-            TagView.TextColor = VisitzColors.EntityMemoTagText;
-        }
-        else if (item.EntityType == IcmEntity.ServiceRequest)
-        {
-            TagView.BackgroundColor = VisitzColors.EntityServiceRequestTagBackground;
-            TagView.TextColor = VisitzColors.EntityServiceRequestTagText;
-        }
-        else
-        {
-            TagView.BackgroundColor = Colors.Transparent;
-            TagView.TextColor = VisitzColors.BC_TextColor;
-        }
     }
 }
