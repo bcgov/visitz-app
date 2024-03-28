50ca2c8d50c7cfd090e81f7844d5815421696adb
Todd Scharien <todd.scharien@gov.bc.ca>
2024-03-28 09:27:48 -0700

    Add PhoneNumberFormatter for 7 & 10 digit numbers

---
+++ visitz/VisitzModel/Utilities/PhoneNumberFormatter.cs
@@ -0,0 +1,17 @@
+namespace VisitzModel.Utilities;
+
+public static class PhoneNumberFormatter
+{
+	public static string Format(string phoneNumber)
+	{
+		if (phoneNumber == null)
+			return null;
+
+		return phoneNumber.Length switch
+		{
+			10 => $"({phoneNumber[..3]}) {phoneNumber[3..6]}-{phoneNumber[6..]}",
+			7 => $"{phoneNumber[..3]}-{phoneNumber[3..]}",
+			_ => phoneNumber,
+		};
+	}
+}
