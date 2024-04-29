916ddbe95daec76cd5758c351ff5b97f53f0fb2f
Todd Scharien <todd.scharien@gov.bc.ca>
2024-04-29 10:09:23 -0700

    Add tests for ParseEmptyWordTruthiness

---
+++ visitz/VisitzModelTest/Extensions/StringExtensionsTests.cs
@@ -0,0 +1,25 @@
+using VisitzModel.Extensions;
+
+namespace VisitzModelTest.Extensions;
+
+public class StringExtensionsTests
+{
+	[Theory]
+	[InlineData("", null)]
+	[InlineData(" ", null)]
+	[InlineData("\t", null)]
+	[InlineData("Y", true)]
+	[InlineData("Yes", true)]
+	[InlineData("Yeah", true)]
+	[InlineData("Yep", true)]
+	[InlineData("N", false)]
+	[InlineData("No", false)]
+	[InlineData("Nope", false)]
+	[InlineData("Negative", false)]
+	public void ParseEmptyWordTruthiness_Valid(string input, bool? expected)
+	{
+		bool? emptyWordTruthiness = input.ParseEmptyWordTruthiness();
+
+		Assert.Equal(expected, emptyWordTruthiness);
+	}
+}
