d127e1954c9798a4cb6b234eee4cb385e869ac10
Todd Scharien <todd.scharien@gov.bc.ca>
2024-05-17 16:45:38 -0700

    Implement DraftsList

--- visitz/Visitz/ViewModels/Drafts/DraftsListViewModel.cs
+++ visitz/Visitz/ViewModels/Drafts/DraftsListViewModel.cs
@@ -1,5 +1,54 @@
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Messaging;
+using Realms;
+using System.Collections.ObjectModel;
+using VisitzModel.Messaging;
+using VisitzModel.Models;
+using VisitzModel.Models.Drafts;
+
 namespace Visitz.ViewModels.Drafts;
 
 internal partial class DraftsListViewModel : VisitzViewModel
 {
+	[ObservableProperty]
+	public ObservableCollection<object> draftItems = [];
+
+	readonly ObservableRealmQueryMap<IDraftItem> queryMap = new();
+
+	public override void Create()
+	{
+		base.Create();
+
+		queryMap.ItemsChanged += QueryMap_ItemsChanged;
+
+		StrongReferenceMessenger.Default.Register<DraftMasterSelectedMessage>(this, DraftMasterSelected);
+	}
+
+	public override void Destroy()
+	{
+		base.Destroy();
+
+		StrongReferenceMessenger.Default.UnregisterAll(this);
+	}
+
+	private void DraftMasterSelected(object _, DraftMasterSelectedMessage message)
+	{
+		queryMap.UnsubscribeAll();
+
+		var (type, realm) = message.Value;
+
+		if (type == typeof(NoteDraft))
+			queryMap.Subscribe(realm, realm.All<NoteDraft>());
+	}
+
+	private void QueryMap_ItemsChanged(object _, (Type, IRealmCollection<IDraftItem> Items, ChangeSet Changes) e)
+	{
+		if (e.Changes == null)
+		{
+			DraftItems.Clear();
+
+			foreach (var item in e.Items)
+				DraftItems.Add(item);
+		}
+	}
 }
