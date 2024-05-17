fd33994548fb74a279489ac69b7b42870f65f05d
Todd Scharien <todd.scharien@gov.bc.ca>
2024-05-17 13:38:25 -0700

    Pull empty view into its own file

---
+++ visitz/Visitz/Views/EmptyCollectionMessage.xaml.cs
@@ -0,0 +1,18 @@
+namespace Visitz.Views;
+
+public partial class EmptyCollectionMessage : ContentView
+{
+	public static readonly BindableProperty TextProperty =
+		BindableProperty.Create(nameof(Text), typeof(string), typeof(EmptyCollectionMessage));
+
+	public string Text
+	{
+		get => (string)GetValue(TextProperty);
+		set => SetValue(TextProperty, value);
+	}
+
+	public EmptyCollectionMessage()
+	{
+		InitializeComponent();
+	}
+}
