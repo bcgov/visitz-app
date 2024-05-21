19bdbd78ac29823e23745705381cbae2291a7deb
Todd Scharien <todd.scharien@gov.bc.ca>
2024-05-21 10:12:34 -0700

    Rename to Version2_3_3
    To keep its name consistent with the app's visible version number.

--- visitz/VisitzModel/Storage/IcmData.cs
+++ visitz/VisitzModel/Storage/IcmData.cs
@@ -8,7 +8,7 @@ namespace VisitzModel.Storage;
 public class IcmData(byte[] encryptionKey) : VisitzRealmBase(Name, CurrentVersion, encryptionKey)
 {
     public static readonly string Name = "icmDataCopies.realm";
-    public static readonly ulong CurrentVersion = Version2_1;
+    public static readonly ulong CurrentVersion = Version2_3_3;
 
     protected override RealmSchema MakeRealmSchema()
     {
