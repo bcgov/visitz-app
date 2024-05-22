72f2fb5cc3326c1771adc8b15c2ee8e5a8c91272
Todd Scharien <todd.scharien@gov.bc.ca>
2024-05-22 14:22:01 -0700

    Add "Unknown" entity badge colors

--- visitz/Visitz/Resources/Styles/VisitzColors.cs
+++ visitz/Visitz/Resources/Styles/VisitzColors.cs
@@ -82,8 +82,11 @@ public static class VisitzColors
     public static readonly Color EntitySubTypeBackground = TryGetColor(nameof(EntitySubTypeBackground));
     public static readonly Color EntitySubTypeTagTextBackground = TryGetColor(nameof(EntitySubTypeTagTextBackground));
 
-    // Family Information Colors
-    public static readonly Color KeyPlayerInfoPurpleBackground = TryGetColor(nameof(KeyPlayerInfoPurpleBackground));
+	public static readonly Color EntityUnknownTypeBackground = TryGetColor(nameof(EntityUnknownTypeBackground));
+	public static readonly Color EntityUnknownTypeTagText = TryGetColor(nameof(EntityUnknownTypeTagText));
+
+	// Family Information Colors
+	public static readonly Color KeyPlayerInfoPurpleBackground = TryGetColor(nameof(KeyPlayerInfoPurpleBackground));
     public static readonly Color FamilyMemberInfoGrayBorder = TryGetColor(nameof(FamilyMemberInfoGrayBorder));
 
     public static readonly Color LightGrayText = TryGetColor(nameof(LightGrayText));
