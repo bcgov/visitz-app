47c31c310f1a6063b4ef123013cf6b062f2b2a05
Todd Scharien <todd.scharien@gov.bc.ca>
2024-05-23 10:54:35 -0700

    Add Upsert() to AssessmentDraft

--- visitz/VisitzModel/Models/SafetyAssess/AssessmentDraft.cs
+++ visitz/VisitzModel/Models/SafetyAssess/AssessmentDraft.cs
@@ -29,4 +29,32 @@ public partial class AssessmentDraft : IRealmObject, IDraftItem
 		get => (EntitySubtype)RelatedEntitySubtypeInt;
 		set => RelatedEntitySubtypeInt = (int)value;
 	}
+
+	public static async Task<AssessmentDraft> Upsert(
+		SafetyAssessment assessment,
+		string draftLocation,
+		EntityType type = EntityType.Incident,
+		EntitySubtype subtype = EntitySubtype.ChildProtection)
+	{
+		var realm = assessment.Realm;
+
+		var draft = realm.Find<AssessmentDraft>(assessment.IncidentNumber) ?? new()
+		{
+			DraftEntityId = assessment.IncidentNumber,
+		};
+
+		await realm.WriteAsync(async () =>
+		{
+			await SafetyAssessment.Save(realm, assessment);
+
+			draft.DraftLocation = draftLocation;
+			draft.RelatedEntityType = type;
+			draft.RelatedEntitySubtype = subtype;
+			draft.LastUpdated = DateTimeOffset.Now;
+
+			realm.Add(draft, update: true);
+		});
+
+		return draft;
+	}
 }
