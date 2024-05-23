681d4922cd962cabf93775fc8acf5368789820aa
Todd Scharien <todd.scharien@gov.bc.ca>
2024-05-23 08:47:55 -0700

    Migrate SafetyAssessmentDrafts Realm

---
+++ visitz/VisitzModel/Storage/Migrations/SafetyAssessmentMigrations.cs
@@ -0,0 +1,13 @@
+using Realms;
+using VisitzModel.Storage.Migrations.SafetyAssessments;
+
+namespace VisitzModel.Storage.Migrations;
+
+internal static class SafetyAssessmentMigrations
+{
+	public static void MigrateRealm(Migration migration, ulong oldSchemaVersion)
+	{
+		if (oldSchemaVersion < VisitzRealmBase.Version2_3_3)
+			SafetyAssessment2_3_3.Migrate(migration);
+	}	
+}
