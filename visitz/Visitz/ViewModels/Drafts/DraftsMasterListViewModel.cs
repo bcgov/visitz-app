505e98ffd059e90c9071e159f8afea664423db7a
Todd Scharien <todd.scharien@gov.bc.ca>
2024-05-23 11:37:42 -0700

    Remove unused using

--- visitz/Visitz/ViewModels/Drafts/DraftsMasterListViewModel.cs
+++ visitz/Visitz/ViewModels/Drafts/DraftsMasterListViewModel.cs
@@ -1,7 +1,6 @@
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using CommunityToolkit.Mvvm.Messaging;
-using Realms;
 using System.Collections.ObjectModel;
 using Visitz.Resources.Localization;
 using Visitz.Storage;
