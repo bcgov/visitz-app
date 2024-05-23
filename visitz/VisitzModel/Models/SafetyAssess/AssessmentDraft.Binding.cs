d0c8996dca06392b073abcc624e6b594a644cf2f
Todd Scharien <todd.scharien@gov.bc.ca>
2024-05-23 08:47:20 -0700

    Add AssessmentDraft

---
+++ visitz/VisitzModel/Models/SafetyAssess/AssessmentDraft.Binding.cs
@@ -0,0 +1,47 @@
+using VisitzModel.Extensions;
+using VisitzModel.Models.EntityTypes;
+
+namespace VisitzModel.Models.SafetyAssess;
+
+public partial class AssessmentDraft
+{
+	public EntityType RelatedEntityTypeBinding
+	{
+		get => IsValid ? RelatedEntityType : default;
+		set
+		{
+			this.Commit(() => RelatedEntityType = value);
+			RaisePropertyChanged(nameof(RelatedEntityType));
+		}
+	}
+
+	public EntitySubtype RelatedEntitySubtypeBinding
+	{
+		get => IsValid ? RelatedEntitySubtype : default;
+		set
+		{
+			this.Commit(() => RelatedEntitySubtype = value);
+			RaisePropertyChanged(nameof(RelatedEntitySubtype));
+		}
+	}
+
+	public DateTimeOffset LastUpdatedBinding
+	{
+		get => IsValid ? LastUpdated : default;
+		set
+		{
+			this.Commit(() => LastUpdated = value);
+			RaisePropertyChanged(nameof(LastUpdated));
+		}
+	}
+
+	public string DraftLocationBinding
+	{
+		get => IsValid ? DraftLocation : default;
+		set
+		{
+			this.Commit(() => DraftLocation = value);
+			RaisePropertyChanged(nameof(DraftLocation));
+		}
+	}
+}
---
+++ visitz/VisitzModel/Models/SafetyAssess/AssessmentDraft.cs
@@ -0,0 +1,32 @@
+using Realms;
+using VisitzModel.Models.Drafts;
+using VisitzModel.Models.EntityTypes;
+using VisitzModel.Resources.Localization;
+
+namespace VisitzModel.Models.SafetyAssess;
+public partial class AssessmentDraft : IRealmObject, IDraftItem
+{
+	[PrimaryKey]
+	public string DraftEntityId { get; set; }
+
+	public DateTimeOffset DraftCreated { get; set; } = DateTimeOffset.Now;
+	public DateTimeOffset LastUpdated { get; set; } = DateTimeOffset.Now;
+
+	public string Preview => GeneralStrings.SafetyAssessment;
+
+	public string DraftLocation { get; set; }
+
+	int RelatedEntityTypeInt { get; set; } = (int)EntityType.Unknown;
+	public EntityType RelatedEntityType
+	{
+		get => (EntityType)RelatedEntityTypeInt;
+		set => RelatedEntityTypeInt = (int)value;
+	}
+
+	int RelatedEntitySubtypeInt { get; set; } = (int)EntitySubtype.Unknown;
+	public EntitySubtype RelatedEntitySubtype
+	{
+		get => (EntitySubtype)RelatedEntitySubtypeInt;
+		set => RelatedEntitySubtypeInt = (int)value;
+	}
+}
