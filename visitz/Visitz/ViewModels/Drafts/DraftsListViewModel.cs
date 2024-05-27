b31d02204a6e5e895a44e59dbe403920b3cc3e81
Todd Scharien <todd.scharien@gov.bc.ca>
2024-05-27 09:23:49 -0700

    Open caseload item when draft item tapped

--- visitz/Visitz/ViewModels/Drafts/DraftsListViewModel.cs
+++ visitz/Visitz/ViewModels/Drafts/DraftsListViewModel.cs
@@ -1,10 +1,14 @@
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using CommunityToolkit.Mvvm.Messaging;
 using Realms;
 using System.Collections.ObjectModel;
+using Visitz.Storage;
+using Visitz.Views.Caseload;
 using VisitzModel.Messaging;
 using VisitzModel.Models;
 using VisitzModel.Models.Drafts;
+using VisitzModel.Models.EntityTypes;
 using VisitzModel.Models.SafetyAssess;
 
 namespace Visitz.ViewModels.Drafts;
@@ -16,9 +20,15 @@ internal partial class DraftsListViewModel : VisitzViewModel
 
 	readonly ObservableRealmQueryMap queryMap = new();
 
-	public override void Create()
+	Realm DataRealm { get; set; }
+
+	EntitySection SectionToOpen { get; set; }
+
+	public override async void Create()
 	{
 		base.Create();
+
+		DataRealm = await VisitzRealms.GetIcmDataRealmAsync();
 
 		queryMap.ItemsChanged += QueryMap_ItemsChanged;
 
@@ -41,9 +51,15 @@ internal partial class DraftsListViewModel : VisitzViewModel
 		var (type, realm) = message.Value;
 
 		if (type == typeof(NoteDraft))
+		{
 			SortAndSubscribe(realm, realm.All<NoteDraft>());
+			SectionToOpen = EntitySection.NoteEntry;
+		}
 		else if (type == typeof(AssessmentDraft))
+		{
 			SortAndSubscribe(realm, realm.All<AssessmentDraft>());
+			SectionToOpen = EntitySection.SafetyAssessment;
+		}
 		else
 			throw new InvalidOperationException($"Type {type} not supported in Drafts view.");
 	}
@@ -60,5 +76,20 @@ internal partial class DraftsListViewModel : VisitzViewModel
 
 		foreach (var item in e.Items)
 			DraftItems.Add(item);
+	}
+
+	[RelayCommand]
+	private void DraftItemSelected(IDraftItem draftItem)
+	{
+		var caseloadItem = DataRealm
+			.All<CaseloadItem>()
+			.Where(item => item.CaseIncidentNumber == draftItem.RelatedEntityId)
+			.FirstOrDefault(); 
+
+		var caseloadNav = new CaseloadItemSelectedMessage(caseloadItem, SectionToOpen);
+		StrongReferenceMessenger.Default.Send(caseloadNav);
+
+		var appNav = new AppNavMessage(new() { ContentViewType = typeof(CaseloadContainerView) });
+		StrongReferenceMessenger.Default.Send(appNav);
 	}
 }
