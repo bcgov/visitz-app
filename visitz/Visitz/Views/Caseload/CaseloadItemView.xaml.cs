6ee6842e1bdf57410646d9e11e41d79e314083c7
Todd Scharien <todd.scharien@gov.bc.ca>
2024-05-30 16:17:36 -0700

    Show draft indicator if bound CaseloadItem has any draft

--- visitz/Visitz/Views/Caseload/CaseloadItemView.xaml.cs
+++ visitz/Visitz/Views/Caseload/CaseloadItemView.xaml.cs
@@ -1,9 +1,23 @@
+using VisitzModel.Extensions.EntityTypes;
 using VisitzModel.Models;
+using VisitzModel.Models.EntityTypes;
 
 namespace Visitz.Views.Caseload;
 
 public partial class CaseloadItemView : ContentView
 {
+	public static readonly BindableProperty DraftedItemsProperty = BindableProperty.Create(
+		nameof(DraftedItems), typeof(HashSet<(string, EntityType)>), typeof(CaseloadItemView), propertyChanged: (boundObj, oldVal, newVal) =>
+		{
+			(boundObj as CaseloadItemView).OnBindingContextChanged();
+		});
+
+	public HashSet<(string, EntityType)> DraftedItems
+	{
+		get => (HashSet<(string, EntityType)>)GetValue(DraftedItemsProperty);
+		set => SetValue(DraftedItemsProperty, value);
+	}
+
 	public CaseloadItemView()
 	{
 		InitializeComponent();
@@ -14,8 +28,17 @@ public partial class CaseloadItemView : ContentView
         base.OnBindingContextChanged();
 
         if (BindingContext is CaseloadItem item)
-		{
-			OpenDateLabel.IsVisible = item.DisplayDate?.Length > 0;
-		}
+			ApplyCaseloadItem(item);
     }
+
+	private void ApplyCaseloadItem(CaseloadItem item)
+	{
+		OpenDateLabel.IsVisible = item.DisplayDate?.Length > 0;
+
+		if (DraftedItems != null)
+		{
+			var tuple = (item.CaseIncidentNumber, item.EntityType.ParseEntityType());
+			DraftIndicator.IsVisible = DraftedItems.Contains(tuple);
+		}
+	}
 }
