6328c6b39b51e976bb32c6603e9fde1eb20018b6
Todd Scharien <todd.scharien@gov.bc.ca>
2024-05-30 09:22:07 -0700

    Add HasDraft to EntityNavItem

--- visitz/VisitzModel/Models/Navigation/EntityNavItem.cs
+++ visitz/VisitzModel/Models/Navigation/EntityNavItem.cs
@@ -3,4 +3,6 @@ namespace VisitzModel.Models.Navigation;
 public class EntityNavItem : NavItem
 {
 	public EntitySection Section { get; set; } = EntitySection.Details;
+
+	public bool HasDraft { get; set; }
 }
