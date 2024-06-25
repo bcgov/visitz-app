89832aa06468c4b64611099720e3d9941006dd71
Todd Scharien <todd.scharien@gov.bc.ca>
2024-06-25 11:35:32 -0700

    Move app lock source into feature namespace

--- visitz/Visitz/VisitzApp.xaml.cs
+++ visitz/Visitz/VisitzApp.xaml.cs
@@ -3,6 +3,7 @@ using Oidc.Events;
 using Visitz.Pages;
 using Visitz.Services;
 using Visitz.Storage;
+using Visitz.Views.AppLock;
 using Visitz.Views.Debugging;
 using Visitz.Views.Root;
 using Visitz.Views.User;
