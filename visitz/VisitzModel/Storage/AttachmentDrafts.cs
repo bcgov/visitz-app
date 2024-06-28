30a50706a61a7c2621bdacaf8d4bf6fe3e20e167
Todd Scharien <todd.scharien@gov.bc.ca>
2024-06-28 08:26:44 -0700

    Implement Initial AttachmentDrafts

---
+++ visitz/VisitzModel/Storage/AttachmentDrafts.cs
@@ -0,0 +1,24 @@
+using Realms;
+using Realms.Schema;
+using VisitzModel.Models;
+
+namespace VisitzModel.Storage;
+
+public partial class AttachmentDrafts(byte[] encryptionKey) : VisitzRealmBase(Name, CurrentVersion, encryptionKey)
+{
+	public static readonly string Name = "attachmentDraftsRealm.realm";
+	public static readonly ulong CurrentVersion = Version2_3_3;
+
+	protected override RealmSchema MakeRealmSchema()
+	{
+		return new[]
+		{
+			typeof(AttachmentDraft),
+		};
+	}
+
+	protected override void MigrateRealm(Migration migration, ulong oldSchemaVersion)
+	{
+		// TODO...
+	}
+}
