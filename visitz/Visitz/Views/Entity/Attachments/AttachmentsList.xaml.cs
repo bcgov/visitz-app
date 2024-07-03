128e8c5e7001a38e2ceb5f6e8f04c69de08c2cb2
Todd Scharien <todd.scharien@gov.bc.ca>
2024-07-03 12:18:17 -0700

    Implement AttachmentsList

--- visitz/Visitz/Views/Entity/Attachments/AttachmentsList.xaml.cs
+++ visitz/Visitz/Views/Entity/Attachments/AttachmentsList.xaml.cs
@@ -1,9 +1,18 @@
 using Visitz.Views.BaseClasses;
+using VisitzModel.Models;
 
 namespace Visitz.Views.Entity.Attachments;
 
-public partial class AttachmentsList : ViewModelContentView
+public partial class AttachmentsList : ViewModelContentView, ICaseloadItemHolder
 {
+	new AttachmentsListViewModel ViewModel => base.ViewModel as AttachmentsListViewModel;
+
+	public CaseloadItem CaseloadItem
+	{
+		get => ViewModel.CaseloadItem;
+		set => ViewModel.CaseloadItem = value;
+	}
+
 	public AttachmentsList() : base(ServiceProvider.GetService<AttachmentsListViewModel>())
 	{
 		InitializeComponent();
