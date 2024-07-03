7fc01cf060d0c52fff807692cde365f98ce4abe0
Todd Scharien <todd.scharien@gov.bc.ca>
2024-07-03 12:18:55 -0700

    Begin SaveEncryptFileAsync implementation

--- visitz/VisitzModel/Storage/Filesystem/AttachmentFiler.cs
+++ visitz/VisitzModel/Storage/Filesystem/AttachmentFiler.cs
@@ -1,3 +1,4 @@
+using Realms;
 using System.Globalization;
 using VisitzModel.Formats;
 using VisitzModel.Models;
@@ -18,6 +19,8 @@ public class AttachmentFiler(string basePath, CaseloadItem caseloadItem) : ICase
 
 	string CachePath => Path.Combine(FileSystem.CacheDirectory, basePath, CaseloadItemId);
 
+	string AppDataPath => Path.Combine(FileSystem.AppDataDirectory, basePath, CaseloadItemId);
+
 	static string MakeTimestamp() => DateTimeOffset.Now
 		.ToString(IcmDateFormats.ImageTimestamp, CultureInfo.InvariantCulture);
 
@@ -26,20 +29,35 @@ public class AttachmentFiler(string basePath, CaseloadItem caseloadItem) : ICase
 		return $"{prepend}_{ContextualName}_{MakeTimestamp()}.{extension.Trim('.')}";
 	}
 
+	async Task<string> WriteFile(Stream stream, string path, string prepend, string extension)
+	{
+		Directory.CreateDirectory(path);
+
+		string fullpath = Path.Combine(path, MakeFilename(prepend, extension));
+		await using var localFileStream = File.Create(fullpath);
+
+		stream.Seek(0, SeekOrigin.Begin);
+		await stream.CopyToAsync(localFileStream);
+
+		return fullpath;
+	}
+
 	/// <summary>
 	/// Takes in a stream and saves it to cache as a file with the provided extension.
 	/// </summary>
 	/// <param name="stream"></param>
 	/// <param name="extension"></param>
 	/// <returns>Full path of the file that was created.</returns>
-	public async Task<string> CacheFile(Stream stream, string prepend, string extension)
+	public async Task<string> CacheFileAsync(Stream stream, string prepend, string extension)
 	{
-		Directory.CreateDirectory(CachePath);
+		return await WriteFile(stream, CachePath, prepend, extension);
+	}
 
-		string fullpath = Path.Combine(CachePath, MakeFilename(prepend, extension));
-		await using var localFileStream = File.Create(fullpath);
+	public async Task<string> SaveEncryptFileAsync(Stream stream, string prepend, string extension)
+	{
+		string fullpath = await WriteFile(stream, AppDataPath, prepend, extension);
 
-		await stream.CopyToAsync(localFileStream);
+		// TODO: Encrypt file
 
 		return fullpath;
 	}
