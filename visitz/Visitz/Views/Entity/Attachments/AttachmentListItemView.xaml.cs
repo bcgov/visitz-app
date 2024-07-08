d6dc659a2d73b3291da9668ee3886f6c74dbe548
Todd Scharien <todd.scharien@gov.bc.ca>
2024-07-08 15:54:31 -0700

    Pull list item impl into its own class

---
+++ visitz/Visitz/Views/Entity/Attachments/AttachmentListItemView.xaml.cs
@@ -0,0 +1,11 @@
+using Visitz.Views.BaseClasses;
+
+namespace Visitz.Views.Entity.Attachments;
+
+public partial class AttachmentListItemView : BaseContentView
+{
+	public AttachmentListItemView()
+	{
+		InitializeComponent();
+	}
+}
