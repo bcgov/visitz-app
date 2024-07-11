e54ff41905de2fe839a6cb672a066435ba28c182
Todd Scharien <todd.scharien@gov.bc.ca>
2024-07-11 09:40:54 -0700

    Add milliseconds to timestamp

--- visitz/VisitzModel/Formats/IcmDateFormats.cs
+++ visitz/VisitzModel/Formats/IcmDateFormats.cs
@@ -5,5 +5,5 @@ public static class IcmDateFormats
 	public static readonly string BasicTimestamp = "yyyy-MMM-dd hh:mm:ss tt";
 	public static readonly string BasicTimestampShort = "yyyy-MMM-dd";
 	public static readonly string NotePeriod = "MMM yyyy";
-	public static readonly string ImageTimestamp = "yyyyMMdd-HHmmss";
+	public static readonly string ImageTimestamp = "yyyyMMdd-HHmmss-fff";
 }
