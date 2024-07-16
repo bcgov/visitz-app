14e85ca7e67818aed8e73eef075f2604577fe94d
Todd Scharien <todd.scharien@gov.bc.ca>
2024-07-16 15:06:56 -0700

    Catch parsing exceptions in KongJsonMessage

--- visitz/VisitzApi/ErrorHandling/KongJsonMessage.cs
+++ visitz/VisitzApi/ErrorHandling/KongJsonMessage.cs
@@ -1,4 +1,4 @@
-﻿using System.Text.Json;
+using System.Text.Json;
 
 namespace VisitzApi.ErrorHandling
 {
@@ -8,16 +8,24 @@ namespace VisitzApi.ErrorHandling
 
         internal static bool TryFindMessage(string content, out string message)
         {
-            var json = JsonDocument.Parse(content).RootElement;
+			try
+			{
+				var json = JsonDocument.Parse(content).RootElement;
 
-            if (json.TryGetProperty(MessageKey, out var element))
-            {
-                message = element.GetString();
-                return true;
-            }
+				if (json.TryGetProperty(MessageKey, out var element))
+				{
+					message = element.GetString();
+					return true;
+				}
 
-            message = "";
-            return false;
+				message = "";
+				return false;
+			}
+			catch (Exception ex)
+			{
+				message = ex.Message;
+				return false;
+			}
         }
     }
 }
