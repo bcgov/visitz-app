11f5f9666774ae94f5a3f3a938f5d8e8a8a44e86
Todd Scharien <todd.scharien@gov.bc.ca>
2024-07-16 11:21:11 -0700

    Add submit attachment entities

---
+++ visitz/VisitzApi/Models/Attachments/CfaDetailsPayload.cs
@@ -0,0 +1,9 @@
+namespace VisitzApi.Models.Attachments;
+
+public class CfaDetailsPayload
+{
+	public string RecordingType { get; set; }
+	public string DangerStatement { get; set; }
+	public string DecisionMaking { get; set; }
+	public string SafetyGoals { get; set; }
+}
---
+++ visitz/VisitzApi/Models/Attachments/Section13Payload.cs
@@ -0,0 +1,30 @@
+namespace VisitzApi.Models.Attachments;
+
+public partial class Section13Payload
+{
+	public string _1A { get; set; }
+	public string _1AFinding { get; set; }
+	public string _1B { get; set; }
+	public string _1BFinding { get; set; }
+	public string _1C { get; set; }
+	public string _1CFinding { get; set; }
+	public string _1DFinding { get; set; }
+	public string _1E1 { get; set; }
+	public string _1E1Finding { get; set; }
+	public string _1E2 { get; set; }
+	public string _1E2Finding { get; set; }
+	public string _1F { get; set; }
+	public string _1FFinding { get; set; }
+	public string _1G { get; set; }
+	public string _1GFinding { get; set; }
+	public string _1H { get; set; }
+	public string _1HFinding { get; set; }
+	public string _1I { get; set; }
+	public string _1IFinding { get; set; }
+	public string _1J { get; set; }
+	public string _1JFinding { get; set; }
+	public string _1K { get; set; }
+	public string _1KFinding { get; set; }
+	public string _1L { get; set; }
+	public string _1LFinding { get; set; }
+}
