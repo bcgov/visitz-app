f9139b2fee6c38c8c6900f3e46d7c9325e7c0b37
Todd Scharien <todd.scharien@gov.bc.ca>
2024-07-18 11:39:07 -0700

    Use Jpeg for default

--- visitz/Visitz/Extensions/StreamImageExtensions.cs
+++ visitz/Visitz/Extensions/StreamImageExtensions.cs
@@ -5,9 +5,13 @@ namespace Visitz.Extensions;
 
 public static class StreamImageExtensions
 {
-	public static IImage MakeThumbnail(this Stream stream, float maxWidthOrHeight, bool disposeStream = false)
+	public static IImage MakeThumbnail(
+		this Stream stream,
+		float maxWidthOrHeight,
+		ImageFormat imageFormat = ImageFormat.Jpeg,
+		bool disposeStream = false)
 	{
 		stream.Seek(0, SeekOrigin.Begin);
-		return PlatformImage.FromStream(stream).Downsize(maxWidthOrHeight, disposeStream);
+		return PlatformImage.FromStream(stream, imageFormat).Downsize(maxWidthOrHeight, disposeStream);
 	}
 }
