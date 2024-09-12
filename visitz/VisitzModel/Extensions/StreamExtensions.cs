2e2019c865780ff2b36a90945d1cb8d037a9e1cf
Todd Scharien <todd.scharien@gov.bc.ca>
2024-09-12 14:46:57 -0700

    Add AsBytesAsync extension method

---
+++ visitz/VisitzModel/Extensions/StreamExtensions.cs
@@ -0,0 +1,14 @@
+namespace VisitzModel.Extensions;
+
+public static class StreamExtensions
+{
+	public static async Task<byte[]> AsBytesAsync(this Stream stream)
+	{
+		byte[] bytesOut = new byte[stream.Length];
+
+		stream.Seek(0, SeekOrigin.Begin);
+		await stream.ReadAsync(bytesOut.AsMemory());
+
+		return bytesOut;
+	}
+}
