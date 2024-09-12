c08dbe7005900c62d5a4d0cb202d4c0bd02f7cd7
Todd Scharien <todd.scharien@gov.bc.ca>
2024-09-12 14:48:05 -0700

    Rename and add overload in ResizeImageValues

--- visitz/VisitzModel/Utilities/ResizeImageValues.cs
+++ visitz/VisitzModel/Utilities/ResizeImageValues.cs
@@ -2,11 +2,20 @@ namespace VisitzModel.Utilities;
 
 internal static class ResizeImageValues
 {
-	public static (float newWidth, float newHeight) ResizeByFileSize(
+	public static (float newWidth, float newHeight) NewDimensionsByFileSize(
 		float currentWidth,
 		float currentHeight,
 		long desiredFileSize)
 	{
 		return (desiredFileSize / currentHeight, desiredFileSize / currentWidth);
+	}
+
+	public static float MaxNewDimensionByFileSize(
+		float currentWidth,
+		float currentHeight,
+		long desiredFileSize)
+	{
+		var (newWidth, newHeight) = NewDimensionsByFileSize(currentWidth, currentHeight, desiredFileSize);
+		return Math.Max(newWidth, newHeight);
 	}
 }
