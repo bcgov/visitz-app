0f9f4f889445dac93e550b8b7d773611c8031e55
George Babu <george.babu@gov.bc.ca>
2024-09-23 08:47:01 -0700

    Hide soft keyboard
    STRY0032750: Hide soft keyboard when touched anywhere else in the app

--- visitz/Visitz/Views/Root/RootPage.xaml.cs
+++ visitz/Visitz/Views/Root/RootPage.xaml.cs
@@ -17,6 +17,7 @@ public partial class RootPage : VisitzPage, ISnackbarPresenter
         BindingContext = ViewModel;
 
         StrongReferenceMessenger.Default.Register<AppNavMessage>(this, ReceiveAppNavMessage);
+		HideSoftInputOnTapped = true;
 	}
 
     private void ReceiveAppNavMessage(object recipient, AppNavMessage message)
