e91986af18a429e83927da52711f35270b1f4380
Todd Scharien <todd.scharien@gov.bc.ca>
2024-10-10 09:00:51 -0700

    Rework Windows local auth
    Namely: remove it. We're going to rely on Windows' OS lock instead.

--- visitz/Visitz/VisitzWindow.cs
+++ visitz/Visitz/VisitzWindow.cs
@@ -2,12 +2,6 @@ namespace Visitz;
 
 public partial class VisitzWindow : Window
 {
-	private static readonly double ValidityTimeoutMinutes = 5.0d;
-
-	private DateTime? LastDeactivated { get; set; }
-
-	public event EventHandler ActivatedWhenInvalid;
-
 	public VisitzWindow() { }
 
 	public VisitzWindow(Page page) : base(page) { }
@@ -25,26 +19,4 @@ public partial class VisitzWindow : Window
 	private static partial Window ApplyDefaultWindowLayout(Window window);
 #endif
 
-	protected override void OnActivated()
-	{
-		base.OnActivated();
-
-		if (LastDeactivated is DateTime last)
-		{
-			var validUntil = last.AddMinutes(ValidityTimeoutMinutes);
-			var isValid = DateTime.UtcNow <= validUntil;
-
-			if (!isValid)
-				ActivatedWhenInvalid?.Invoke(this, EventArgs.Empty);
-
-			LastDeactivated = null;
-		}
-	}
-
-	protected override void OnDeactivated()
-	{
-		base.OnDeactivated();
-
-		LastDeactivated = DateTime.UtcNow;
-	}
 }
