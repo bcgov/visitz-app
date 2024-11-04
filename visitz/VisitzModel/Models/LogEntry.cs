da6e5c5798984a7114d73283f09e039fce25c2b1
George Babu <george.babu@gov.bc.ca>
2024-11-04 15:11:03 -0800

    Add Log realm

---
+++ visitz/VisitzModel/Models/LogEntry.cs
@@ -0,0 +1,47 @@
+using System.Globalization;
+using Realms;
+using VisitzModel.Formats;
+
+namespace VisitzModel.Models
+{
+    public partial class LogEntry : IRealmObject
+    {
+        public string Type { get; set; }
+
+        public string Message { get; set; }
+
+        public string Source { get; set; }
+
+        public string Timestamp { get; set; }
+
+        public static string GetTimestamp(DateTime dateTime)
+        {
+            var timestamp = dateTime.ToString(IcmDateFormats.BasicTimestamp, CultureInfo.InvariantCulture);
+            return timestamp;
+        }
+
+        public static async Task AddLogEntry(string logType, string logMessage, string logSource, string timeStamp, Realm realm)
+        {
+            var logEntry = new LogEntry
+            {
+                Type = logType,
+                Message = logMessage,
+                Source = logSource,
+                Timestamp = timeStamp
+            };
+            try
+            {
+                await realm.WriteAsync(() => realm.Add(logEntry));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+
+        public static List<LogEntry> GetLogEntries(Realm realm)
+        {
+            return realm.All<LogEntry>().ToList();
+        }
+    }
+}
