85df033c21468ec5d8c505d95cecb0093a6cb9af
Todd Scharien <todd.scharien@gov.bc.ca>
2024-12-19 15:24:54 -0800

    Use file-scoped namespace

--- visitz/VisitzModel/Storage/VisitzKey.cs
+++ visitz/VisitzModel/Storage/VisitzKey.cs
@@ -1,76 +1,75 @@
 using System.Security.Cryptography;
 
-namespace VisitzModel.Storage
+namespace VisitzModel.Storage;
+
+public static class VisitzKey
 {
-    public static class VisitzKey
+    private static readonly int _defaultKeySize = 64;
+
+    private static readonly string _encryptionKeyName = "visitz.encryption.key.";
+
+    private static readonly SemaphoreSlim _semaphore = new(1);
+
+    private static byte[] NewKey(int keySize)
     {
-        private static readonly int _defaultKeySize = 64;
+        var encryptionKey = new byte[keySize];
+        using var rng = RandomNumberGenerator.Create();
 
-        private static readonly string _encryptionKeyName = "visitz.encryption.key.";
+        rng.GetBytes(encryptionKey);
+        return encryptionKey;
+    }
 
-        private static readonly SemaphoreSlim _semaphore = new(1);
+    private static async Task SetKeyInStorage(string keyName, byte[] encryptionKey)
+    {
+        var encodedKey = Convert.ToBase64String(encryptionKey);
+        var namespacedKey = _encryptionKeyName + keyName;
 
-        private static byte[] NewKey(int keySize)
+        await SecureStorage.Default.SetAsync(namespacedKey, encodedKey);
+    }
+
+    private static async Task<byte[]> GetKeyFromStorage(string keyName)
+    {
+        var namespacedKey = _encryptionKeyName + keyName;
+        var encodedKey = await SecureStorage.Default.GetAsync(namespacedKey);
+
+        return encodedKey != null
+            ? Convert.FromBase64String(encodedKey)
+            : null;
+    }
+
+    public static async Task<byte[]> GetKey(string keyName, int? keySizeIfNew = null)
+    {
+        await _semaphore.WaitAsync();
+
+        try
         {
-            var encryptionKey = new byte[keySize];
-            using var rng = RandomNumberGenerator.Create();
-
-            rng.GetBytes(encryptionKey);
-            return encryptionKey;
+            return await DoGetKey(keyName, keySizeIfNew);
         }
-
-        private static async Task SetKeyInStorage(string keyName, byte[] encryptionKey)
+        finally
         {
-            var encodedKey = Convert.ToBase64String(encryptionKey);
-            var namespacedKey = _encryptionKeyName + keyName;
-
-            await SecureStorage.Default.SetAsync(namespacedKey, encodedKey);
-        }
-
-        private static async Task<byte[]> GetKeyFromStorage(string keyName)
-        {
-            var namespacedKey = _encryptionKeyName + keyName;
-            var encodedKey = await SecureStorage.Default.GetAsync(namespacedKey);
-
-            return encodedKey != null
-                ? Convert.FromBase64String(encodedKey)
-                : null;
-        }
-
-        public static async Task<byte[]> GetKey(string keyName, int? keySizeIfNew = null)
-        {
-            await _semaphore.WaitAsync();
-
             try
             {
-                return await DoGetKey(keyName, keySizeIfNew);
+                _semaphore.Release();
             }
-            finally
-            {
-                try
-                {
-                    _semaphore.Release();
-                }
-                catch {}
-            }
+            catch {}
         }
+    }
 
-        static async Task<byte[]> DoGetKey(string keyName, int? keySizeIfNew = null)
+    static async Task<byte[]> DoGetKey(string keyName, int? keySizeIfNew = null)
+    {
+        byte[] encryptionKey = await GetKeyFromStorage(keyName);
+
+        if (encryptionKey == null)
         {
-            byte[] encryptionKey = await GetKeyFromStorage(keyName);
-
-            if (encryptionKey == null)
-            {
-                await SetKeyInStorage(keyName, NewKey(keySizeIfNew ?? _defaultKeySize));
-                encryptionKey = await GetKeyFromStorage(keyName);
-            }
-
-            return encryptionKey;
+            await SetKeyInStorage(keyName, NewKey(keySizeIfNew ?? _defaultKeySize));
+            encryptionKey = await GetKeyFromStorage(keyName);
         }
 
-        public static void RemoveKey(string keyName)
-        {
-            SecureStorage.Default.Remove(_encryptionKeyName + keyName);
-        }
+        return encryptionKey;
+    }
+
+    public static void RemoveKey(string keyName)
+    {
+        SecureStorage.Default.Remove(_encryptionKeyName + keyName);
     }
 }
