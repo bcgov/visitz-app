00c205a791e7995b7b97cfd0cf85058d836c103d
Todd Scharien <todd.scharien@gov.bc.ca>
2025-01-08 09:18:48 -0800

    Implement IDisposable in VisitzViewModel and obselete Destroy()

--- visitz/Visitz/Views/BaseClasses/VisitzViewModel.cs
+++ visitz/Visitz/Views/BaseClasses/VisitzViewModel.cs
@@ -6,11 +6,12 @@ namespace Visitz.Views.BaseClasses
 	/// <summary>
 	/// The base class for all the view models. Common functionality can be defined here.
 	/// </summary>
-	public partial class VisitzViewModel : ObservableObject
+	public partial class VisitzViewModel : ObservableObject, IDisposable
 	{
 		bool created;
+        bool _disposedValue;
 
-		public void OnCreate()
+        public void OnCreate()
 		{
 			if (!created)
 			{
@@ -24,9 +25,32 @@ namespace Visitz.Views.BaseClasses
 			ConsoleTrace.TraceMethod(this);
 		}
 
+        [Obsolete("Use Dispose instead")]
 		public virtual void Destroy()
 		{
 			ConsoleTrace.TraceMethod(this);
 		}
-	}
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (_disposedValue)
+                return;
+
+            if (disposing)
+            {
+                ConsoleTrace.TraceMethod(this);
+#pragma warning disable CS0618 // Type or member is obsolete
+                Destroy(); // Used until all other references are removed
+#pragma warning restore CS0618 // Type or member is obsolete
+            }
+
+            _disposedValue = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+    }
 }
