dc0476f205d6177825dc409056833c9a55703292
Todd Scharien <todd.scharien@gov.bc.ca>
2025-01-08 11:35:58 -0800

    Implement lifecycle stub for ChildYouthVisitListViewModel

--- visitz/Visitz/Views/Entity/ChildYouthVisits/ChildYouthVisitListViewModel.cs
+++ visitz/Visitz/Views/Entity/ChildYouthVisits/ChildYouthVisitListViewModel.cs
@@ -1,8 +1,29 @@
+using CommunityToolkit.Mvvm.ComponentModel;
 using Visitz.Views.BaseClasses;
+using VisitzModel.Models;
 
 namespace Visitz.Views.Entity.ChildYouthVisits;
 
-internal partial class ChildYouthVisitListViewModel : VisitzViewModel
+internal partial class ChildYouthVisitListViewModel : VisitzViewModel, ICaseloadItemHolder
 {
+    private bool _disposed;
 
+    [ObservableProperty]
+    public CaseloadItem caseloadItem;
+
+    protected override async Task InitAsync()
+    {
+        await base.InitAsync();
+    }
+
+    protected override void Dispose(bool disposing)
+    {
+        if (!_disposed && disposing)
+        {
+            // TODO
+            _disposed = true;
+        }
+
+        base.Dispose(disposing);
+    }
 }
