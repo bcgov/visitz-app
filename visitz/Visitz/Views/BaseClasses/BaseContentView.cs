0d1910acb7c6e3896a19034fb015ff5e066fc1b5
Todd Scharien <todd.scharien@gov.bc.ca>
2025-01-08 09:01:03 -0800

    Convert whitespace to spaces

--- visitz/Visitz/Views/BaseClasses/BaseContentView.cs
+++ visitz/Visitz/Views/BaseClasses/BaseContentView.cs
@@ -8,28 +8,28 @@ public abstract class BaseContentView : ContentView, IDisposable
     private bool _disposedValue;
 
     protected override void OnHandlerChanging(HandlerChangingEventArgs args)
-	{
-		base.OnHandlerChanging(args);
+    {
+        base.OnHandlerChanging(args);
 
-		if (args.AttachingToHandler())
-			Creating();
-		else if (args.DetachingFromHandler())
-			Destroying();
-	}
+        if (args.AttachingToHandler())
+            Creating();
+        else if (args.DetachingFromHandler())
+            Destroying();
+    }
 
-	public void Dispose()
-	{
+    public void Dispose()
+    {
         Dispose(disposing: true);
         GC.SuppressFinalize(this);
     }
 
-	protected virtual void Creating()
+    protected virtual void Creating()
     {
         ConsoleTrace.TraceMethod(this);
     }
 
     [Obsolete("Use Dispose instead")]
-	protected virtual void Destroying()
+    protected virtual void Destroying()
     {
         ConsoleTrace.TraceMethod(this);
     }
