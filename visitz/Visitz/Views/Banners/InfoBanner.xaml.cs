6a0da895b48a4f696730883a7bcc77dc1f4560c6
Todd Scharien <todd.scharien@gov.bc.ca>
2025-01-09 12:23:58 -0800

    Add LevelProperty

--- visitz/Visitz/Views/Banners/InfoBanner.xaml.cs
+++ visitz/Visitz/Views/Banners/InfoBanner.xaml.cs
@@ -5,14 +5,45 @@ public partial class InfoBanner : ContentView
 	public static readonly BindableProperty TextProperty =
 		BindableProperty.Create(nameof(Text), typeof(string), typeof(InfoBanner));
 
+    public static readonly BindableProperty LevelProperty =
+        BindableProperty.Create(nameof(Level), typeof(AlertLevel), typeof(InfoBanner), propertyChanged: LevelChanged);
+
 	public string Text
 	{
 		get => (string)GetValue(TextProperty);
 		set => SetValue(TextProperty, value);
 	}
 
+    public AlertLevel Level
+    {
+        get => (AlertLevel)GetValue(LevelProperty);
+        set => SetValue(LevelProperty, value);
+    }
+
 	public InfoBanner()
 	{
 		InitializeComponent();
 	}
+
+    private static void LevelChanged(BindableObject boundObj, object oldVal, object newVal)
+    {
+        AlertLevel newLevel = (AlertLevel)newVal;
+
+        switch (newLevel)
+        {
+            case AlertLevel.Info:
+                // TODO ...
+                break;
+            case AlertLevel.Warning:
+                break;
+            case AlertLevel.Danger:
+                break;
+            case AlertLevel.Critical:
+                break;
+            case AlertLevel.Unknown:
+            case AlertLevel.Success:
+            default:
+                throw new NotImplementedException(newLevel.ToString());
+        }
+    }
 }
