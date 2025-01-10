71b66171c3298bfc511a40979a3cb5ad62faa26c
George Babu <george.babu@gov.bc.ca>
2025-01-10 15:42:38 -0800

    In person visit listing

--- visitz/Visitz/Views/Entity/ChildYouthVisits/ChildYouthVisitListView.xaml.cs
+++ visitz/Visitz/Views/Entity/ChildYouthVisits/ChildYouthVisitListView.xaml.cs
@@ -1,12 +1,19 @@
 using Visitz.Views.BaseClasses;
+using VisitzModel.Models;
 
 namespace Visitz.Views.Entity.ChildYouthVisits;
 
-public partial class ChildYouthVisitListView : ViewModelContentView
+public partial class ChildYouthVisitListView : ViewModelContentView, ICaseloadItemHolder
 {
+	public CaseloadItem CaseloadItem
+	{
+		get => (ViewModel as ChildYouthVisitListViewModel).CaseloadItem;
+		set => (ViewModel as ChildYouthVisitListViewModel).CaseloadItem = value;
+	}
+
 	public ChildYouthVisitListView() : base(ServiceProvider.GetService<ChildYouthVisitListViewModel>())
 	{
 		InitializeComponent();
-        BindingContext = ViewModel;
+		BindingContext = ViewModel;
 	}
 }
--- visitz/Visitz/Views/Entity/ChildYouthVisits/ChildYouthVisitListViewModel.cs
+++ visitz/Visitz/Views/Entity/ChildYouthVisits/ChildYouthVisitListViewModel.cs
@@ -1,4 +1,8 @@
+using System.Collections.ObjectModel;
 using CommunityToolkit.Mvvm.ComponentModel;
+using Visitz.Resources.Localization;
+using Visitz.Storage;
+using Visitz.Views.Banners;
 using Visitz.Views.BaseClasses;
 using VisitzModel.Models;
 
@@ -11,9 +15,37 @@ internal partial class ChildYouthVisitListViewModel : VisitzViewModel, ICaseload
     [ObservableProperty]
     public CaseloadItem caseloadItem;
 
+    [ObservableProperty]
+    public ObservableCollection<PersonVisit> inPersonVisitList;
+
+    [ObservableProperty]
+    public DateTimeOffset dateOfVisit;
+
+    [ObservableProperty]
+    public string type;
+
+    [ObservableProperty]
+    public string visitDescription;
+
+    [ObservableProperty]
+    public string createdBy;
+
+    [ObservableProperty]
+    public AlertLevel bannerLevel;
+
+    [ObservableProperty]
+    public string bannerText;
+
+    [ObservableProperty]
+    public bool hasVisitData = true;
+
+    [ObservableProperty]
+    public bool showEmptyIcon = false;
+
     protected override async Task InitAsync()
     {
         await base.InitAsync();
+        await LoadInPersonVisitData();
     }
 
     protected override void Dispose(bool disposing)
@@ -25,5 +57,60 @@ internal partial class ChildYouthVisitListViewModel : VisitzViewModel, ICaseload
         }
 
         base.Dispose(disposing);
+    }
+
+    private async Task LoadInPersonVisitData()
+    {
+        var icmData = await VisitzRealms.GetIcmDataRealmAsync();
+        var persons = icmData.All<PersonVisit>()
+                      .Where(person => person.ParentId == CaseloadItem.CaseIncidentNumber)
+                      .OrderByDescending(person => person.DateOfVisit)
+                      .ToList();
+        InPersonVisitList = new ObservableCollection<PersonVisit>(persons);
+        if (persons.Count != 0)
+        {
+            var lastVisit = persons.FirstOrDefault();
+            if (lastVisit != null)
+            {
+                DateTimeOffset currentDate = DateTimeOffset.Now;
+                DateTimeOffset nextVisitDate = lastVisit.DateOfVisit.AddDays(90);
+                string dueDate = nextVisitDate.ToString("MMMM d, yyyy");
+                var dateDifference = nextVisitDate - currentDate;
+                SetBannerInfo(dateDifference.Days, dueDate);
+            }
+        }
+        else
+        {
+            HasVisitData = false;
+            ShowEmptyIcon = true;
+        }
+    }
+
+    private void SetBannerInfo(int daysDifference, string dateInBanner)
+    {
+        if (daysDifference > 30 && daysDifference <= 90)
+        {
+            BannerLevel = AlertLevel.Info;
+            BannerText = string.Format(
+                LocalizedStrings.NextVisitDueBy, dateInBanner);
+        }
+        else if (daysDifference <= 30 && daysDifference > 5)
+        {
+            BannerLevel = AlertLevel.Warning;
+            BannerText = string.Format(
+                LocalizedStrings.VisitDueBy, dateInBanner);
+        }
+        else if (daysDifference <= 5 && daysDifference >= 0)
+        {
+            BannerLevel = AlertLevel.Danger;
+            BannerText = string.Format(
+                LocalizedStrings.VisitDueBy, dateInBanner);
+        }
+        else if (daysDifference < 0)
+        {
+            BannerLevel = AlertLevel.Critical;
+            BannerText = string.Format(
+                LocalizedStrings.OverdueVisitOn, dateInBanner);
+        }
     }
 }
