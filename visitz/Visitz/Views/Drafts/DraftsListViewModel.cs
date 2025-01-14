a4befb4bf0af2e8019fc53d2136bc168bb02acaf
Todd Scharien <todd.scharien@gov.bc.ca>
2025-01-14 13:38:27 -0800

    Convert indentation to spaces

--- visitz/Visitz/Views/Drafts/DraftsListViewModel.cs
+++ visitz/Visitz/Views/Drafts/DraftsListViewModel.cs
@@ -18,42 +18,42 @@ internal partial class DraftsListViewModel : VisitzViewModel
 {
     bool _disposed;
 
-	[ObservableProperty]
-	public ObservableCollection<object> draftItems = [];
+    [ObservableProperty]
+    public ObservableCollection<object> draftItems = [];
 
-	readonly ObservableRealmQueryMap queryMap = new();
+    readonly ObservableRealmQueryMap queryMap = new();
 
-	Realm DataRealm { get; set; }
+    Realm DataRealm { get; set; }
 
-	EntitySection SectionToOpen { get; set; }
+    EntitySection SectionToOpen { get; set; }
 
-	public event EventHandler<IDraftItem> SelectedItemRelatedMissing;
+    public event EventHandler<IDraftItem> SelectedItemRelatedMissing;
 
-	protected override async Task InitAsync()
-	{
-		await base.InitAsync();
+    protected override async Task InitAsync()
+    {
+        await base.InitAsync();
 
-		StrongReferenceMessenger.Default.Register<DraftMasterSelectedMessage>(this, DraftMasterSelected);
+        StrongReferenceMessenger.Default.Register<DraftMasterSelectedMessage>(this, DraftMasterSelected);
 
         DataRealm = await VisitzRealms.GetIcmDataRealmAsync();
 
-		queryMap.ItemsChanged += QueryMap_ItemsChanged;
-	}
+        queryMap.ItemsChanged += QueryMap_ItemsChanged;
+    }
 
-	protected override void Dispose(bool disposing)
-	{
+    protected override void Dispose(bool disposing)
+    {
         if (!_disposed && disposing)
         {
-		    StrongReferenceMessenger.Default.UnregisterAll(this);
+            StrongReferenceMessenger.Default.UnregisterAll(this);
 
-		    queryMap.ItemsChanged -= QueryMap_ItemsChanged;
+            queryMap.ItemsChanged -= QueryMap_ItemsChanged;
             queryMap.Dispose();
 
             _disposed = true;
         }
 
-		base.Dispose(disposing);
-	}
+        base.Dispose(disposing);
+    }
 
 #pragma warning disable SS001 // Async methods should return a Task to make them awaitable
     // Ignoring SS001 because this function is used like an EventHandler
@@ -63,84 +63,84 @@ internal partial class DraftsListViewModel : VisitzViewModel
 
         queryMap.UnsubscribeAll();
 
-		var (type, realm) = message.Value;
+        var (type, realm) = message.Value;
 
-		if (type == typeof(NoteDraft))
-		{
-			SortAndSubscribe(realm, realm.All<NoteDraft>());
-			SectionToOpen = EntitySection.NoteEntry;
-		}
-		else if (type == typeof(AssessmentDraft))
-		{
-			SortAndSubscribe(realm, realm.All<AssessmentDraft>());
-			SectionToOpen = EntitySection.SafetyAssessment;
-		}
-		else if (type == typeof(AttachmentDraft))
-		{
-			SortAndSubscribe(realm, realm.All<AttachmentDraft>());
-			SectionToOpen = EntitySection.Attachments;
-		}
-		else
-			throw new InvalidOperationException($"Type {type} not supported in Drafts view.");
-	}
+        if (type == typeof(NoteDraft))
+        {
+            SortAndSubscribe(realm, realm.All<NoteDraft>());
+            SectionToOpen = EntitySection.NoteEntry;
+        }
+        else if (type == typeof(AssessmentDraft))
+        {
+            SortAndSubscribe(realm, realm.All<AssessmentDraft>());
+            SectionToOpen = EntitySection.SafetyAssessment;
+        }
+        else if (type == typeof(AttachmentDraft))
+        {
+            SortAndSubscribe(realm, realm.All<AttachmentDraft>());
+            SectionToOpen = EntitySection.Attachments;
+        }
+        else
+            throw new InvalidOperationException($"Type {type} not supported in Drafts view.");
+    }
 #pragma warning restore SS001 // Async methods should return a Task to make them awaitable
 
-	private void SortAndSubscribe<T>(Realm realm, IQueryable<T> query) where T : IRealmObject
-	{
-		var sortedQuery = query.Filter($"TRUEPREDICATE SORT({nameof(IDraftItem.LastUpdated)} DESC)");
-		queryMap.Subscribe(realm, sortedQuery);
-	}
+    private void SortAndSubscribe<T>(Realm realm, IQueryable<T> query) where T : IRealmObject
+    {
+        var sortedQuery = query.Filter($"TRUEPREDICATE SORT({nameof(IDraftItem.LastUpdated)} DESC)");
+        queryMap.Subscribe(realm, sortedQuery);
+    }
 
-	private void QueryMap_ItemsChanged(object _, (Type, IRealmCollection<IRealmObject> Items, ChangeSet Changes) e)
-	{
-		DraftItems.Clear();
+    private void QueryMap_ItemsChanged(object _, (Type, IRealmCollection<IRealmObject> Items, ChangeSet Changes) e)
+    {
+        DraftItems.Clear();
 
-		foreach (var item in e.Items)
-			DraftItems.Add(item);
-	}
+        foreach (var item in e.Items)
+            DraftItems.Add(item);
+    }
 
-	[RelayCommand]
-	private void DraftItemSelected(IDraftItem draftItem)
-	{
-		var caseloadItem = DataRealm
-			.All<CaseloadItem>()
-			.Where(item => item.CaseIncidentNumber == draftItem.RelatedEntityId)
-			.FirstOrDefault();
+    [RelayCommand]
+    private void DraftItemSelected(IDraftItem draftItem)
+    {
+        var caseloadItem = DataRealm
+            .All<CaseloadItem>()
+            .Where(item => item.CaseIncidentNumber == draftItem.RelatedEntityId)
+            .FirstOrDefault();
 
-		if (caseloadItem != null)
-			NavigateTo(caseloadItem, SectionToOpen, draftItem);
-		else
-			SelectedItemRelatedMissing?.Invoke(this, draftItem);
-	}
+        if (caseloadItem != null)
+            NavigateTo(caseloadItem, SectionToOpen, draftItem);
+        else
+            SelectedItemRelatedMissing?.Invoke(this, draftItem);
+    }
 
-	static void NavigateTo(CaseloadItem caseloadItem, EntitySection section, IDraftItem draftItem)
-	{
-		var caseloadNav = new CaseloadItemSelectedMessage(caseloadItem, section, draftItem);
-		StrongReferenceMessenger.Default.Send(caseloadNav);
+    static void NavigateTo(CaseloadItem caseloadItem, EntitySection section, IDraftItem draftItem)
+    {
+        var caseloadNav = new CaseloadItemSelectedMessage(caseloadItem, section, draftItem);
+        StrongReferenceMessenger.Default.Send(caseloadNav);
 
-		var appNav = new AppNavMessage(new() { ContentViewType = typeof(CaseloadContainerView) });
-		StrongReferenceMessenger.Default.Send(appNav);
-	}
+        var appNav = new AppNavMessage(new() { ContentViewType = typeof(CaseloadContainerView) });
+        StrongReferenceMessenger.Default.Send(appNav);
+    }
 
-	public static async Task DeleteDraft(IDraftItem draft)
-	{
-		var realm = draft.Realm;
+    public static async Task DeleteDraft(IDraftItem draft)
+    {
+        var realm = draft.Realm;
 
-		await realm.WriteAsync(async () =>
-		{
-			if (draft is AssessmentDraft)
-			{
-				var assessment = SafetyAssessment.FindByIncidentNumber(realm, draft.RelatedEntityId);
+        await realm.WriteAsync(async () =>
+        {
+            if (draft is AssessmentDraft)
+            {
+                var assessment = SafetyAssessment.FindByIncidentNumber(realm, draft.RelatedEntityId);
 
-				if (assessment != null)
-					realm.Remove(assessment);
+                if (assessment != null)
+                    realm.Remove(assessment);
 
-				realm.Remove(draft);
-			}
-			else if (draft is AttachmentDraft attachmentDraft)
-				await attachmentDraft.Attachment.DeleteAsync();
-			else
-				realm.Remove(draft);
-		});
-	}
+                realm.Remove(draft);
+            }
+            else if (draft is AttachmentDraft attachmentDraft)
+                await attachmentDraft.Attachment.DeleteAsync();
+            else
+                realm.Remove(draft);
+        });
+    }
 }
