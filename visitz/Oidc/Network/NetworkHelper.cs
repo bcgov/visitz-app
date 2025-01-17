0cad43dc155b48e68559e58d6dc14622a219cc72
Todd Scharien <todd.scharien@gov.bc.ca>
2025-01-16 16:51:28 -0800

    Force workaround to run on MainThread
    Had to use a TaskCompletionSource in case the calling thread wasn't the
    main thread, or else the available boolean would not be set in time for
    it to be returned.
    
    This solution isn't ideal since it blocks the background thread, but the
    Windows network calls seem to work fast anyway.

--- visitz/Oidc/Network/NetworkHelper.cs
+++ visitz/Oidc/Network/NetworkHelper.cs
@@ -1,19 +1,61 @@
 namespace Oidc.Network;
 
 #if WINDOWS
+using Oidc.Util;
 using Windows.Networking.Connectivity;
 #endif
 
 public static class NetworkHelper
 {
-    public static bool InternetAvailable =>
+    public static bool InternetAvailable
 #if WINDOWS
-        // WORKAROUND https://github.com/dotnet/maui/issues/22228#issuecomment-2118235512
-        // NetworkAccess reported incorrectly on Windows on VPN
-        NetworkInformation.GetInternetConnectionProfile().GetNetworkConnectivityLevel()
-            == NetworkConnectivityLevel.InternetAccess;
+    // WORKAROUND https://github.com/dotnet/maui/issues/22228#issuecomment-2118235512
+    // NetworkAccess reported incorrectly on Windows on VPN
+    {
+        get
+        {
+            bool available = false;
+
+            try
+            {
+                available = GetConnectivityLevel() == NetworkConnectivityLevel.InternetAccess;
+            }
+            catch (Exception ex)
+            {
+                ConsoleTrace.TraceMethod(typeof(NetworkHelper), ex.ToString());
+            }
+
+            return available;
+        }
+    }
+
+    static NetworkConnectivityLevel GetConnectivityLevel()
+    {
+        TaskCompletionSource src = new();
+        NetworkConnectivityLevel level = NetworkConnectivityLevel.None;
+
+        MainThread.BeginInvokeOnMainThread(delegate
+        {
+            try
+            {
+                var connectionProfile = NetworkInformation.GetInternetConnectionProfile()
+                    ?? throw new InvalidOperationException("Network connection profile unavailable");
+
+                level = connectionProfile.GetNetworkConnectivityLevel();
+
+                src.SetResult();
+            }
+            catch (Exception ex)
+            {
+                src.TrySetException(ex);
+            }
+        });
+
+        src.Task.Wait();
+        return level;
+    }
 #else
-        Connectivity.Current.NetworkAccess == NetworkAccess.Internet;
+        => Connectivity.Current.NetworkAccess == NetworkAccess.Internet;
 #endif
 
     public static void AssertInternetAvailable(string messageIfUnavailable)
