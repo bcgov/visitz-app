511d4d1c86f39dfa9b4faa4304ec81579b7cc28e
Todd Scharien <todd.scharien@gov.bc.ca>
2025-01-23 09:10:17 -0800

    Add JsonSerializerOptions for Siebel GET

--- visitz/VisitzApi/Json/PayloadOptions.cs
+++ visitz/VisitzApi/Json/PayloadOptions.cs
@@ -4,8 +4,25 @@ namespace VisitzApi.Json;
 
 public static class PayloadOptions
 {
-	public static readonly JsonSerializerOptions Default = new()
-	{
-		PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
-	};
+    public static readonly JsonSerializerOptions Default = new()
+    {
+        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+    };
+
+    static readonly JsonSerializerOptions _siebelGet = new()
+    {
+    };
+
+    static readonly DateTimeOffsetParseConverter _parseConverter = new(SiebelFormats.SiebelDateFormat);
+
+    public static JsonSerializerOptions SiebelGet
+    {
+        get
+        {
+            if (!_siebelGet.Converters.Contains(_parseConverter))
+                _siebelGet.Converters.Add(_parseConverter);
+
+            return _siebelGet;
+        }
+    }
 }
