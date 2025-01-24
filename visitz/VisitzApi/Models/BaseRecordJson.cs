2b44833e2b48a7b091aa4a59de69716a73a9a43d
Todd Scharien <todd.scharien@gov.bc.ca>
2025-01-24 09:53:03 -0800

    Add whitespace for readability

--- visitz/VisitzApi/Models/BaseRecordJson.cs
+++ visitz/VisitzApi/Models/BaseRecordJson.cs
@@ -6,22 +6,31 @@ public class BaseRecordJson
 {
     [JsonRequired]
     public string Id { get; set; }
+
     [JsonRequired]
     public string RowId { get; set; }
+
     [JsonRequired]
     public string AssignedTo { get; set; }
+
     [JsonRequired]
     public string AssignedToId { get; set; }
+
     [JsonRequired]
     public string CreatedBy { get; set; }
+
     [JsonRequired]
     public string CreatedById { get; set; }
+
     [JsonRequired]
     public string UpdatedBy { get; set; }
+
     [JsonRequired]
     public string UpdatedById { get; set; }
+
     [JsonRequired]
     public DateTimeOffset CreatedDate { get; set; }
+
     [JsonRequired]
     public DateTimeOffset UpdatedDate { get; set; }
 }
--- visitz/VisitzApi/Models/CaseJson.cs
+++ visitz/VisitzApi/Models/CaseJson.cs
@@ -5,25 +5,45 @@ namespace VisitzApi.Models;
 public class CaseJson : BaseRecordJson
 {
     public string Caseload { get; set; }
+
     public string CaseNum { get; set; }
+
     public string ClosedDate { get; set; }
+
     public string CloseReason { get; set; }
+
     public string EarlyOpenReason { get; set; }
+
     public string IntegrationState { get; set; }
+
     public string LegacyFileNumber { get; set; }
+
     public string MiddleName { get; set; }
+
     [JsonPropertyName("MyFS Flag")]
     public string MyFSFlag { get; set; }
+
     public string Name { get; set; }
+
     public string OfficeName { get; set; }
+
     public string Organization { get; set; }
+
     public string RegionName { get; set; }
+
     public DateTimeOffset RenewReviewDate { get; set; }
+
     public DateTimeOffset ReopenedDate { get; set; }
+
     public string RestrictedFlag { get; set; }
+
     public string Status { get; set; }
+
     public string SubjectContactFirstName { get; set; }
+
     public string SubjectContactLastName { get; set; }
+
     public string Type { get; set; }
+
     public string WorkQueue { get; set; }
 }
