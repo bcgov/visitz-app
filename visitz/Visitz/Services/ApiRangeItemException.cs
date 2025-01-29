075c04ed8a057684db0cb0edc6e6d47506f531c1
Todd Scharien <todd.scharien@gov.bc.ca>
2025-01-29 11:00:59 -0800

    Add abstract VisitzApiRangeService class

---
+++ visitz/Visitz/Services/ApiRangeItemException.cs
@@ -0,0 +1,6 @@
+namespace Visitz.Services;
+
+internal class ApiRangeItemException<T>(T item, Exception exception) : Exception(exception.Message, exception)
+{
+    public T Item { get; private set; } = item;
+}
