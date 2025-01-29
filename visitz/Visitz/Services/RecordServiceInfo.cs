bc8bf014927e1fbafc9a6b0e52e64f900929063f
Todd Scharien <todd.scharien@gov.bc.ca>
2025-01-29 12:39:26 -0800

    Add convenience constructors

--- visitz/Visitz/Services/RecordServiceInfo.cs
+++ visitz/Visitz/Services/RecordServiceInfo.cs
@@ -9,4 +9,20 @@ internal class RecordServiceInfo
     public string Id { get; set; }
 
     public string Label { get; set; }
+
+    RecordServiceInfo(string id, string label)
+    {
+        Id = id;
+        Label = label;
+    }
+
+    public RecordServiceInfo(CaseRecord @case) : this(@case.Id, @case.Name)
+    {
+        Type = EntityType.Case;
+    }
+
+    public RecordServiceInfo(IncidentRecord incident) : this(incident.Id, incident.Name)
+    {
+        Type = EntityType.Incident;
+    }
 }
