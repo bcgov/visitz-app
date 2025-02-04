3814e057eb870930e22764dec378fe4e4a715813
Todd Scharien <todd.scharien@gov.bc.ca>
2025-02-04 08:30:40 -0800

    Increase IcmData and PersonVisitDrafts version
    There is no data to migrate.

--- visitz/VisitzModel/Storage/IcmData.cs
+++ visitz/VisitzModel/Storage/IcmData.cs
@@ -12,7 +12,7 @@ namespace VisitzModel.Storage;
 public class IcmData(byte[] encryptionKey) : VisitzRealmBase(Name, CurrentVersion, encryptionKey)
 {
     public static readonly string Name = "icmDataCopies.realm";
-    public static readonly ulong CurrentVersion = Version2_3_3;
+    public static readonly ulong CurrentVersion = Version2_6_0;
 
     protected override RealmSchema MakeRealmSchema()
     {
--- visitz/VisitzModel/Storage/VisitzRealmBase.cs
+++ visitz/VisitzModel/Storage/VisitzRealmBase.cs
@@ -13,6 +13,7 @@ public abstract class VisitzRealmBase(string realmName, ulong version, byte[] en
 {
     public static readonly ulong Version2_0 = 1;
     public static readonly ulong Version2_3_3 = 2;
+    public static readonly ulong Version2_6_0 = 3;
 
     public string RealmName { get; private set; } = realmName;
 
