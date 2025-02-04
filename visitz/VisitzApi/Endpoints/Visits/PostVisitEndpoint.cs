33a28dbf23aa9c7e8c003059e6c2680ac9e249a2
Todd Scharien <todd.scharien@gov.bc.ca>
2025-02-04 12:58:58 -0800

    Add PostVisitEndpoint

---
+++ visitz/VisitzApi/Endpoints/Visits/PostVisitEndpoint.cs
@@ -0,0 +1,37 @@
+using System.Net;
+using System.Net.Http.Json;
+using System.Text.Json;
+using VisitzApi.Json;
+using VisitzApi.Models.Visits;
+using VisitzApi.Requests;
+
+namespace VisitzApi.Endpoints.Visits;
+
+internal class PostVisitEndpoint(string baseUrl, string caseId, PostVisitJson visitToSend)
+    : VisitzBaseEndpoint<bool>(baseUrl, Vpi.V2, MakePath(caseId))
+{
+    static readonly string PostVisitPath = "/case/{0}/visits";
+
+    PostVisitJson VisitToSend => visitToSend;
+
+    static string MakePath(string caseId)
+    {
+        return string.Format(PostVisitPath, caseId);
+    }
+
+    public override HttpRequestMessage MakeRequest()
+    {
+        var test = JsonSerializer.Serialize(VisitToSend, PayloadOptions.MiddlewarePost);
+        return new()
+        {
+            Content = JsonContent.Create(VisitToSend, options: PayloadOptions.MiddlewarePost),
+            Method = HttpMethod.Post,
+            RequestUri = RequestUri,
+        };
+    }
+
+    public override bool HandleResponse(HttpResponseMessage response, string _)
+    {
+        return response.StatusCode == HttpStatusCode.OK;
+    }
+}
