b9ed284845810fc1171b7dc0dcc7643a6942c0a3
George Babu <George.Babu@gov.bc.ca>
2025-02-20 11:24:12 -0800

    basic listing of support network items list

--- visitz/Visitz/Views/Entity/SupportNetwork/SupportNetworkListViewModel.cs
+++ visitz/Visitz/Views/Entity/SupportNetwork/SupportNetworkListViewModel.cs
@@ -1,8 +1,76 @@
+using System.Collections.ObjectModel;
+using CommunityToolkit.Mvvm.ComponentModel;
+using Realms;
+using Visitz.Storage;
 using Visitz.Views.BaseClasses;
+using VisitzModel.Interfaces;
+using VisitzModel.Models;
+using VisitzModel.Models.Navigation;
+using VisitzModel.Models.People;
 
 namespace Visitz.Views.Entity.SupportNetwork;
 
-internal partial class SupportNetworkListViewModel : VisitzViewModel
+internal partial class SupportNetworkListViewModel : VisitzViewModel, ICaseloadItemHolder, IRequestedEntitySection
 {
+    private bool _disposed;
 
+    readonly ObservableRealmQueryMap realmQuery = new();
+    
+    public EntitySection RequestedSection { get; set; }
+
+    [ObservableProperty]
+    ObservableCollection<SupportNetworkItem> supportNetworksList = [];
+
+    [ObservableProperty]
+    public CaseloadItem caseloadItem;
+
+    [ObservableProperty]
+    public string name;
+
+    [ObservableProperty]
+    public string relationship;
+
+    protected override async Task InitAsync()
+    {
+        await base.InitAsync();
+
+        Realm icmDataRealm = await VisitzRealms.GetIcmDataRealmAsync();
+        realmQuery.ItemsChanged += RealmQuery_ItemsChanged;
+
+        realmQuery.Subscribe(icmDataRealm, SupportNetworkItem.GetSupportNetworkByCaseId(icmDataRealm, CaseloadItem.RowId));
+    }
+
+    private void RealmQuery_ItemsChanged(object sender, (Type Type, IRealmCollection<IRealmObject> Items, ChangeSet Changes) e)
+    {
+        if (e.Type == typeof(SupportNetworkItem))
+            UpdateSupportNetworkList(e.Items, e.Changes);
+    }
+
+    private void UpdateSupportNetworkList(IRealmCollection<IRealmObject> items, ChangeSet changes)
+    {
+        if (changes == null)
+        {
+            foreach (var item in items)
+                SupportNetworksList.Add(item as SupportNetworkItem);
+        }
+        else
+        {
+            foreach (int deleted in changes.DeletedIndices)
+                SupportNetworksList.RemoveAt(deleted);
+
+            foreach (int inserted in changes.InsertedIndices)
+                SupportNetworksList.Insert(inserted, items[inserted] as SupportNetworkItem);
+        }
+    }
+
+    protected override void Dispose(bool disposing)
+    {
+        if (!_disposed && disposing)
+        {
+            realmQuery.ItemsChanged -= RealmQuery_ItemsChanged;
+            realmQuery.Dispose();
+            _disposed = true;
+        }
+        base.Dispose(disposing);
+    }
 }
