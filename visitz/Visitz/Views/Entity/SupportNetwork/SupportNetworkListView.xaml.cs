9054bc3480105412b96665daec6f108c03dd7c0c
George Babu <George.Babu@gov.bc.ca>
2025-02-20 11:24:12 -0800

    basic listing of support network items list

--- visitz/Visitz/Views/Entity/SupportNetwork/SupportNetworkListView.xaml.cs
+++ visitz/Visitz/Views/Entity/SupportNetwork/SupportNetworkListView.xaml.cs
@@ -1,9 +1,24 @@
 using Visitz.Views.BaseClasses;
+using VisitzModel.Interfaces;
+using VisitzModel.Models;
+using VisitzModel.Models.Navigation;
 
 namespace Visitz.Views.Entity.SupportNetwork;
 
-public partial class SupportNetworkListView : ViewModelContentView
+public partial class SupportNetworkListView : ViewModelContentView, ICaseloadItemHolder, IRequestedEntitySection
 {
+    new SupportNetworkListViewModel ViewModel => base.ViewModel as SupportNetworkListViewModel;
+    public CaseloadItem CaseloadItem
+    {
+        get => ViewModel.CaseloadItem;
+        set => ViewModel.CaseloadItem = value;
+    }
+
+    public EntitySection RequestedSection
+    {
+        get => ViewModel.RequestedSection;
+        set => ViewModel.RequestedSection = value;
+    }
 	public SupportNetworkListView() : base(ServiceProvider.GetService<SupportNetworkListViewModel>())
 	{
 		InitializeComponent();
