7de945caf44d8e95ca590b458e18fac01702306c
Babu, George SDPR:EX <George.Babu+bcgovent@gov.bc.ca>
2025-03-03 12:03:56 -0800

    Updated the tag to support the SupportNetworkItemUi

--- visitz/Visitz/Behaviors/Tags/SupportNetworkRelationshipTagBehavior.cs
+++ visitz/Visitz/Behaviors/Tags/SupportNetworkRelationshipTagBehavior.cs
@@ -1,6 +1,6 @@
 using Visitz.Resources.Styles;
+using Visitz.Views.Entity.SupportNetwork;
 using Visitz.Views.TagViews;
-using VisitzModel.Models.People;
 
 namespace Visitz.Behaviors.Tags;
 
@@ -8,13 +8,15 @@ public class SupportNetworkRelationshipTagBehavior : TagStyleBehavior
 {
     protected override void ApplyTagStyle(TagView tag)
     {
-        if (tag.BindingContext is not SupportNetworkItem item)
+        if (tag.BindingContext is not SupportNetworkItemUi itemUi)
             return;
+        var item = itemUi.SupportNetworkObj;
         if (!string.IsNullOrWhiteSpace(item?.Relationship))
         {
             tag.Text = item.Relationship;
             tag.BackgroundColor = VisitzColors.BC_Gold;
             tag.TextColor = VisitzColors.BC_TextColor;
+            tag.FontSize = 12;
         }
     }
 }
