44b0edc9fbacaed88cc20e84466432522940c868
Babu, George SDPR:EX <George.Babu+bcgovent@gov.bc.ca>
2025-03-06 14:35:57 -0800

    Attachments API Initial commit with endpoints and json

--- visitz/VisitzApi/Vpi.cs
+++ visitz/VisitzApi/Vpi.cs
@@ -94,5 +94,13 @@ namespace VisitzApi
         {
             return await CallApi(new GetSupportNetworkEndpoint(BaseVisitzApiUrl, type, id, after));
         }
+
+        public async Task<IEnumerable<ContactJson>> GetAttachmentsAsync(
+            ApiRecordType type,
+            string id,
+            DateTimeOffset? after = null)
+        {
+            return await CallApi(new GetAttachmentsEndpoint(BaseVisitzApiUrl, type, id, after));
+        }
     }
 }
