8f7e9bde532193b3b3d54693d3045b6758ba6eee
Babu, George SDPR:EX <George.Babu+bcgovent@gov.bc.ca>
2025-03-10 12:00:40 -0700

    Indentaion fixed

--- visitz/Visitz/Views/Entity/SupportNetwork/SupportNetworkItemUi.cs
+++ visitz/Visitz/Views/Entity/SupportNetwork/SupportNetworkItemUi.cs
@@ -14,19 +14,19 @@ public partial class SupportNetworkItemUi : ObservableObject
     [ObservableProperty]
     public bool showRelationshipTag;
     public string CapitalizedRelationship
-{
-    get
     {
-        if (string.IsNullOrEmpty(SupportNetwork?.Relationship))
-            return string.Empty;
-        var words = SupportNetwork.Relationship.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
-        for (int i = 0; i < words.Length; i++)
+        get
         {
-            words[i] = char.ToUpper(words[i][0]) + words[i].Substring(1).ToLower();
+            if (string.IsNullOrEmpty(SupportNetwork?.Relationship))
+                return string.Empty;
+            var words = SupportNetwork.Relationship.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            for (int i = 0; i < words.Length; i++)
+            {
+                words[i] = char.ToUpper(words[i][0]) + words[i].Substring(1).ToLower();
+            }
+            return string.Join(" ", words);
         }
-        return string.Join(" ", words);
     }
-}
 
     public SupportNetworkItemUi(SupportNetworkItem item)
     {
