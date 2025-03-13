8ac63627d33617b0f82bc3265b655136686a1fce
Babu, George SDPR:EX <George.Babu+bcgovent@gov.bc.ca>
2025-03-13 08:33:28 -0700

    attachment id passed as another path segment

--- visitz/VisitzApi/Endpoints/GetAttachmentDetailsEndpoint.cs
+++ visitz/VisitzApi/Endpoints/GetAttachmentDetailsEndpoint.cs
@@ -7,16 +7,16 @@ using VisitzApi.Requests;
 
 namespace VisitzApi.Endpoints;
 
-internal class GetAttachmentDetailsEndpoint(string baseUrl, ApiRecordType type, string rowId, DateTimeOffset? after = null)
-    : VisitzBaseEndpoint<IEnumerable<AttachmentJson>>(baseUrl, Vpi.V2, MakePath(type, rowId))
+internal class GetAttachmentDetailsEndpoint(string baseUrl, ApiRecordType type, string rowId, string attachmentId, DateTimeOffset? after = null)
+    : VisitzBaseEndpoint<IEnumerable<AttachmentJson>>(baseUrl, Vpi.V2, MakePath(type, rowId, attachmentId))
 {
-    static readonly string AttachmentsPath = "/{0}/{1}/attachment-details";
+    static readonly string AttachmentsPath = "/{0}/{1}/attachment-details/{2}";
 
     readonly DateTimeOffset? After = after;
 
-    static string MakePath(ApiRecordType recordType, string rowId)
+    static string MakePath(ApiRecordType recordType, string rowId, string attachmentId)
     {
-        return string.Format(AttachmentsPath, recordType.ToString().ToLowerInvariant(), rowId);
+        return string.Format(AttachmentsPath, recordType.ToString().ToLowerInvariant(), rowId, attachmentId);
     }
 
     public override HttpRequestMessage MakeRequest()
