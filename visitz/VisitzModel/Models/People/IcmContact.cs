e250b482b0dbd7e162822f4bae1fb99433bafe35
Todd Scharien <todd.scharien@gov.bc.ca>
2025-03-24 16:56:33 -0700

    Update contact models with new fields

--- visitz/VisitzModel/Models/People/IcmContact.cs
+++ visitz/VisitzModel/Models/People/IcmContact.cs
@@ -4,6 +4,7 @@ using VisitzModel.Extensions;
 using VisitzModel.Interfaces;
 using VisitzModel.Models.EntityTypes;
 using VisitzModel.Models.Interfaces;
+using VisitzModel.Utilities;
 
 namespace VisitzModel.Models.People;
 
@@ -36,49 +37,147 @@ public partial class IcmContact : IRealmObject, IRowMetadata, IApiJson<ContactJs
         set => ParentTypeInt = (int)value;
     }
 
-    public string AboriginalCalc { get; set; }
+    public string _921Agt { get; set; }
+
+    public int ActiveAddresses { get; set; }
 
     public int Age { get; set; }
 
-    public string CaseConEndDt { get; set; }
+    public string AkaFirstName { get; set; }
 
-    public string CaseConOriginalStartDt { get; set; }
+    public string AkaLastName { get; set; }
 
-    public string CaseConParentCaregiver { get; set; }
+    public string Alerts { get; set; }
 
-    public string CaseConReportedOn { get; set; }
+    public string AutismFundingPaused { get; set; }
 
-    public string CaseConStartDt { get; set; }
+    public string BceIdUserName { get; set; }
 
-    public string CaseConSubjectChild { get; set; }
+    public string CanadianCitizen { get; set; }
 
-    public string CaseRelTypeCode { get; set; }
+    public string CellPhone { get; set; }
 
-    public string CaseSubject { get; set; }
+    public string Citizen { get; set; }
+
+    public string Citizenship { get; set; }
+
+    public string City { get; set; }
+
+    public string CollaborateId { get; set; }
+
+    public string Comments { get; set; }
+
+    public string ConcernsOutcome { get; set; }
 
     public string CoordinationAgtCa { get; set; }
 
-    public string CysnCalc { get; set; }
+    public string Country { get; set; }
 
-    public string CysnPstScore { get; set; }
+    public string CountryOfBirth { get; set; }
 
-    public string DateOfBirth { get; set; }
+    public DateTimeOffset? CurrentStartDate { get; set; }
 
-    public string DependentSequenceNumber { get; set; }
+    public string Cysn { get; set; }
 
-    public string GivenName { get; set; }
+    public DateTimeOffset DateOfBirth { get; set; }
 
-    public string InvolvedFamilyAlerts { get; set; }
+    public DateTimeOffset? CitizenUpdatedDate { get; set; }
 
-    public string Is921BandFoundCalc { get; set; }
+    public DateTimeOffset? CitizenshipUpdatedDate { get; set; }
+
+    public string Deceased { get; set; }
+
+    public DateTimeOffset? DeceasedDate { get; set; }
+
+    public string EndDate { get; set; }
+
+    public string FirstName { get; set; }
+
+    public string Gender { get; set; }
+
+    public string GivenNames { get; set; }
+
+    public string HomePhone { get; set; }
+
+    public string ImmigrationStatus { get; set; }
+
+    public string ImmigrationStatusUpdated { get; set; }
+
+    public string Indigenous { get; set; }
+
+    public string IntegrationState { get; set; }
+
+    public string InvestigationOutcomeSummary { get; set; }
 
     public string LastName { get; set; }
 
+    public string LegacyDependentSequence { get; set; }
+
     public string LegalStatus { get; set; }
 
-    public string Sex { get; set; }
+    public string MessagePhone { get; set; }
 
-    public string SsaPrimaryField { get; set; }
+    public string MiddleNames { get; set; }
+
+    public DateTimeOffset? OriginalStartDate { get; set; }
+
+    public bool IsParentCaregiver { get; set; }
+
+    public string PersonIdIcm { get; set; }
+
+    public string PersonIdMis { get; set; }
+
+    public bool ResponsibleForAllegedMaltreatment { get; set; }
+
+    public string PersonalHealthNumber { get; set; }
+
+    public string PersonalHealthNumberVerified { get; set; }
+
+    public string PostalCode { get; set; }
+
+    public string PotentialDuplicate { get; set; }
+
+    public string PotentialDuplicateComments { get; set; }
+
+    public string PreferredLanguage { get; set; }
+
+    public string Primary { get; set; }
+
+    public string PrimaryAddress { get; set; }
+
+    public string PrimaryEmail { get; set; }
+
+    public string ProjectCode { get; set; }
+
+    public string Province { get; set; }
+
+    public string PstScore { get; set; }
+
+    public string Relationship { get; set; }
+
+    public string Role { get; set; }
+
+    public string RowId { get; set; }
+
+    public string SaetPaused { get; set; }
+
+    public string SocialInsuranceNumber { get; set; }
+
+    public DateTimeOffset? StartDate { get; set; }
+
+    public string StreetAddress { get; set; }
+
+    public string StreetAddress2 { get; set; }
+
+    public string Subject { get; set; }
+
+    public bool IsSubjectChild { get; set; }
+
+    public string Title { get; set; }
+
+    public string UnitNumber { get; set; }
+
+    public string WorkPhone { get; set; }
 
     public IcmContact() { }
 
@@ -94,28 +193,87 @@ public partial class IcmContact : IRealmObject, IRowMetadata, IApiJson<ContactJs
         UpdatedDate = DateTimeOffset.Parse(json.UpdatedDate);
         ParentId = parentId;
         ParentType = type;
-        AboriginalCalc = json.AboriginalCalc;
-        Age = int.Parse(json.Age);
-        CaseConEndDt = json.CaseConEndDt;
-        CaseConOriginalStartDt = json.CaseConOriginalStartDt;
-        CaseConParentCaregiver = json.CaseConParentCaregiver;
-        CaseConReportedOn = json.CaseConReportedOn;
-        CaseConStartDt = json.CaseConStartDt;
-        CaseConSubjectChild = json.CaseConSubjectChild;
-        CaseRelTypeCode = json.CaseRelTypeCode;
-        CaseSubject = json.CaseSubject;
+        _921Agt = json._92_1AGT;
+        ActiveAddresses = int.TryParse(json.ActiveAddresses, out int addresses) ? addresses : -1;
+        AkaFirstName = json.AKAFirstName;
+        AkaLastName = json.AKALastName;
+        Alerts = json.Alerts;
+        AutismFundingPaused = json.AutismFundingPaused;
+        BceIdUserName = json.BCeIDUserName;
+        CanadianCitizen = json.CanadianCitizen;
+        CellPhone = json.CellPhone;
+        Citizen = json.Citizen;
+        Citizenship = json.Citizenship;
+        City = json.City;
+        CollaborateId = json.CollaborateID;
+        Comments = json.Comments;
+        ConcernsOutcome = json.ConcernsOutcome;
         CoordinationAgtCa = json.CoordinationAGTCA;
-        CysnCalc = json.CYSNCalc;
-        CysnPstScore = json.CYSNPSTScore;
-        DateOfBirth = json.DateofBirth;
-        DependentSequenceNumber = json.DependentSequenceNumber;
-        GivenName = json.GivenName;
-        InvolvedFamilyAlerts = json.InvolvedFamilyAlerts;
-        Is921BandFoundCalc = json.Is921BandFoundCalc;
+        Country = json.Country;
+        CountryOfBirth = json.CountryofBirth;
+        CurrentStartDate = Timestamp.ParseDateTimeOffsetNullable(json.CurrentStartDate);
+        Cysn = json.CYSN;
+        DateOfBirth = DateTimeOffset.Parse(json.DateofBirth);
+        CitizenUpdatedDate = Timestamp.ParseDateTimeOffsetNullable(json.DateUpdated_CitizenUpdatedDate);
+        CitizenshipUpdatedDate = Timestamp.ParseDateTimeOffsetNullable(json.DateUpdated_CitizenshipUpdatedDate);
+        Deceased = json.Deceased;
+        DeceasedDate = Timestamp.ParseDateTimeOffsetNullable(json.DeceasedDate);
+        EndDate = json.EndDate;
+        FirstName = json.FirstName;
+        Gender = json.Gender;
+        GivenNames = json.GivenNames;
+        HomePhone = json.HomePhone;
+        ImmigrationStatus = json.ImmigrationStatus;
+        ImmigrationStatusUpdated = json.ImmigrationStatusUpdated;
+        Indigenous = json.Indigenous;
+        IntegrationState = json.IntegrationState;
+        InvestigationOutcomeSummary = json.InvestigationOutcomeSummary;
         LastName = json.LastName;
+        LegacyDependentSequence = json.LegacyDependentSequence;
         LegalStatus = json.LegalStatus;
-        Sex = json.Sex;
-        SsaPrimaryField = json.SSAPrimaryField;
+        MessagePhone = json.MessagePhone;
+        MiddleNames = json.MiddleNames;
+        OriginalStartDate = Timestamp.ParseDateTimeOffsetNullable(json.OriginalStartDate);
+        IsParentCaregiver = json.Parent_Caregiver?.ParseWordTruthiness() ?? false;
+        PersonIdIcm = json.PersonIDICM;
+        PersonIdMis = json.PersonIDMIS;
+        ResponsibleForAllegedMaltreatment = json.PersonResponsibleforAllegedMaltreatment?
+            .ParseWordTruthiness() ?? false;
+        PersonalHealthNumber = json.PHN;
+        PersonalHealthNumberVerified = json.PHNVerified;
+        PostalCode = json.PostalCode;
+        PotentialDuplicate = json.PotentialDuplicate;
+        PotentialDuplicateComments = json.PotentialDuplicateComments;
+        PreferredLanguage = json.PreferredLanguage;
+        Primary = json.Primary;
+        PrimaryAddress = json.PrimaryAddress;
+        PrimaryEmail = json.PrimaryEmail;
+        ProjectCode = json.ProjectCode;
+        Province = json.Prov;
+        PstScore = json.PSTScore;
+        Relationship = json.Relationship;
+        Role = json.Role;
+        RowId = json.RowId;
+        SaetPaused = json.SAETPaused;
+        SocialInsuranceNumber = json.SIN;
+        StartDate = Timestamp.ParseDateTimeOffsetNullable(json.StartDate);
+        StreetAddress = json.StreetAddress;
+        StreetAddress2 = json.StreetAddress2;
+        Subject = json.Subject;
+        IsSubjectChild = json.SubjectChild.ParseWordTruthiness();
+        Title = json.Title;
+        UnitNumber = json.UnitNumber;
+        WorkPhone = json.WorkPhone;
+
+        Age = TryParseAge(json.Age, json.DateofBirth);
+    }
+
+    static int TryParseAge(string age, string dateOfBirth)
+    {
+        if (string.IsNullOrWhiteSpace(age) && !string.IsNullOrWhiteSpace(dateOfBirth))
+            return (DateTimeOffset.UtcNow - DateTimeOffset.Parse(dateOfBirth)).Days / 365;
+        else
+            return int.TryParse(age, out int parsed) ? parsed : -1;
     }
 
     static string MakeLocalId(string contactId, string parentId)
@@ -134,28 +292,77 @@ public partial class IcmContact : IRealmObject, IRowMetadata, IApiJson<ContactJs
             UpdatedById = UpdatedById,
             CreatedDate = CreatedDate.ToString(dateFormat),
             UpdatedDate = UpdatedDate.ToString(dateFormat),
-            AboriginalCalc = AboriginalCalc,
+            _92_1AGT = _921Agt,
+            ActiveAddresses = ActiveAddresses.ToString(),
             Age = Age.ToString(),
-            CaseConEndDt = CaseConEndDt,
-            CaseConOriginalStartDt = CaseConOriginalStartDt,
-            CaseConParentCaregiver = CaseConParentCaregiver,
-            CaseConReportedOn = CaseConReportedOn,
-            CaseConStartDt = CaseConStartDt,
-            CaseConSubjectChild = CaseConSubjectChild,
-            CaseRelTypeCode = CaseRelTypeCode,
-            CaseSubject = CaseSubject,
+            AKAFirstName = AkaFirstName,
+            AKALastName = AkaLastName,
+            Alerts = Alerts,
+            AutismFundingPaused = AutismFundingPaused,
+            BCeIDUserName = BceIdUserName,
+            CanadianCitizen = CanadianCitizen,
+            CellPhone = CellPhone,
+            Citizen = Citizen,
+            Citizenship = Citizenship,
+            City = City,
+            CollaborateID = CollaborateId,
+            Comments = Comments,
+            ConcernsOutcome = ConcernsOutcome,
             CoordinationAGTCA = CoordinationAgtCa,
-            CYSNCalc = CysnCalc,
-            CYSNPSTScore = CysnPstScore,
-            DateofBirth = DateOfBirth,
-            DependentSequenceNumber = DependentSequenceNumber,
-            GivenName = GivenName,
-            InvolvedFamilyAlerts = InvolvedFamilyAlerts,
-            Is921BandFoundCalc = Is921BandFoundCalc,
+            Country = Country,
+            CountryofBirth = CountryOfBirth,
+            CurrentStartDate = CurrentStartDate?.ToString(dateFormat),
+            CYSN = Cysn,
+            DateofBirth = DateOfBirth.ToString(dateFormat),
+            DateUpdated_CitizenUpdatedDate = CitizenUpdatedDate?.ToString(dateFormat),
+            DateUpdated_CitizenshipUpdatedDate = CitizenshipUpdatedDate?.ToString(dateFormat),
+            Deceased = Deceased,
+            DeceasedDate = DeceasedDate?.ToString(dateFormat),
+            EndDate = EndDate,
+            FirstName = FirstName,
+            Gender = Gender,
+            GivenNames = GivenNames,
+            HomePhone = HomePhone,
+            ImmigrationStatus = ImmigrationStatus,
+            ImmigrationStatusUpdated = ImmigrationStatusUpdated,
+            Indigenous = Indigenous,
+            IntegrationState = IntegrationState,
+            InvestigationOutcomeSummary = InvestigationOutcomeSummary,
             LastName = LastName,
+            LegacyDependentSequence = LegacyDependentSequence,
             LegalStatus = LegalStatus,
-            Sex = Sex,
-            SSAPrimaryField = SsaPrimaryField,
+            MessagePhone = MessagePhone,
+            MiddleNames = MiddleNames,
+            OriginalStartDate = OriginalStartDate?.ToString(dateFormat),
+            Parent_Caregiver = IsParentCaregiver.AsTruthyChar(),
+            PersonIDICM = PersonIdIcm,
+            PersonIDMIS = PersonIdMis,
+            PersonResponsibleforAllegedMaltreatment = ResponsibleForAllegedMaltreatment.AsTruthyChar(),
+            PHN = PersonalHealthNumber,
+            PHNVerified = PersonalHealthNumberVerified,
+            PostalCode = PostalCode,
+            PotentialDuplicate = PotentialDuplicate,
+            PotentialDuplicateComments = PotentialDuplicateComments,
+            PreferredLanguage = PreferredLanguage,
+            Primary = Primary,
+            PrimaryAddress = PrimaryAddress,
+            PrimaryEmail = PrimaryEmail,
+            ProjectCode = ProjectCode,
+            Prov = Province,
+            PSTScore = PstScore,
+            Relationship = Relationship,
+            Role = Role,
+            RowId = RowId,
+            SAETPaused = SaetPaused,
+            SIN = SocialInsuranceNumber,
+            StartDate = StartDate?.ToString(dateFormat),
+            StreetAddress = StreetAddress,
+            StreetAddress2 = StreetAddress2,
+            Subject = Subject,
+            SubjectChild = IsSubjectChild.AsTruthyChar(),
+            Title = Title,
+            UnitNumber = UnitNumber,
+            WorkPhone = WorkPhone,
         };
     }
 
