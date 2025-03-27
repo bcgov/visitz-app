3d116fb5f4ff1034cc96383d9d6a586a53d85a5e
Todd Scharien <todd.scharien@gov.bc.ca>
2025-03-27 10:28:02 -0700

    Convert indentation to spaces

--- visitz/VisitzModel/Models/Attachments/Attachment.cs
+++ visitz/VisitzModel/Models/Attachments/Attachment.cs
@@ -12,30 +12,30 @@ namespace VisitzModel.Models.Attachments;
 
 public partial class Attachment : IRealmObject, IRecordInfo, IApiJson<AttachmentJson>
 {
-	public static readonly int MaxFilesize = 5 * Sizes.MB;
-	public static readonly int ThumbnailSize = 400;
+    public static readonly int MaxFilesize = 5 * Sizes.MB;
+    public static readonly int ThumbnailSize = 400;
 
-	public static readonly IEnumerable<string> AllowedImageTypes = [".jpg", ".jpeg"];
-	public static readonly IEnumerable<string> AllowedDocumentTypes = [".pdf"];
+    public static readonly IEnumerable<string> AllowedImageTypes = [".jpg", ".jpeg"];
+    public static readonly IEnumerable<string> AllowedDocumentTypes = [".pdf"];
 
-	[PrimaryKey]
-	public string Id {get; set;} = Guid.NewGuid().ToString();
+    [PrimaryKey]
+    public string Id {get; set;} = Guid.NewGuid().ToString();
 
-	public string RelatedEntityId { get; set; }
+    public string RelatedEntityId { get; set; }
 
-	private int RelatedEntityTypeInt { get; set; } = (int)EntityType.Unknown;
-	public EntityType RelatedEntityType
-	{
-		get => (EntityType)RelatedEntityTypeInt;
-		set => RelatedEntityTypeInt = (int)value;
-	}
+    private int RelatedEntityTypeInt { get; set; } = (int)EntityType.Unknown;
+    public EntityType RelatedEntityType
+    {
+        get => (EntityType)RelatedEntityTypeInt;
+        set => RelatedEntityTypeInt = (int)value;
+    }
 
-	private int RelatedEntitySubtypeInt { get; set; } = (int)EntitySubtype.Unknown;
-	public EntitySubtype RelatedEntitySubtype
-	{
-		get => (EntitySubtype)RelatedEntitySubtypeInt;
-		set => RelatedEntitySubtypeInt = (int)value;
-	}
+    private int RelatedEntitySubtypeInt { get; set; } = (int)EntitySubtype.Unknown;
+    public EntitySubtype RelatedEntitySubtype
+    {
+        get => (EntitySubtype)RelatedEntitySubtypeInt;
+        set => RelatedEntitySubtypeInt = (int)value;
+    }
 
     public string ServiceRequestNumber { get; set; }
     public string Categorie { get; set; }
@@ -72,52 +72,52 @@ public partial class Attachment : IRealmObject, IRecordInfo, IApiJson<Attachment
     public DateTimeOffset UpdatedDate { get; set; }
     public string UpdatedBy { get; set; }
 
-	public byte[] Thumbnail { get; set; }
+    public byte[] Thumbnail { get; set; }
 
-	/// <summary>
-	/// Relative path to file on virtualized file system. File name will be different than <see cref="Filename"/>.
-	/// </summary>
-	public string RelativePath { get; set; }
+    /// <summary>
+    /// Relative path to file on virtualized file system. File name will be different than <see cref="Filename"/>.
+    /// </summary>
+    public string RelativePath { get; set; }
 
-	/// <summary>
-	/// Virtual name of the attachment as stored in ICM, without the file type extension.
-	/// </summary>
-	public string Filename { get; set; }
+    /// <summary>
+    /// Virtual name of the attachment as stored in ICM, without the file type extension.
+    /// </summary>
+    public string Filename { get; set; }
 
-	/// <summary>
-	/// The file type extension including the dot '.'
-	/// </summary>
-	public string Extension { get; set; }
+    /// <summary>
+    /// The file type extension including the dot '.'
+    /// </summary>
+    public string Extension { get; set; }
 
-	[Backlink(nameof(AttachmentDraft.Attachment))]
-	public IQueryable<AttachmentDraft> AttachmentDrafts { get; }
+    [Backlink(nameof(AttachmentDraft.Attachment))]
+    public IQueryable<AttachmentDraft> AttachmentDrafts { get; }
 
 #pragma warning disable RLM025 // RealmObject/EmbeddedObject properties usually indicate a relationship
-	public AttachmentDraft Draft => AttachmentDrafts.FirstOrDefault();
+    public AttachmentDraft Draft => AttachmentDrafts.FirstOrDefault();
 #pragma warning restore RLM025 // RealmObject/EmbeddedObject properties usually indicate a relationship
 
-	public bool HasDraft => Draft != null;
+    public bool HasDraft => Draft != null;
 
-	public static async Task DeleteAsync(Realm realm, Attachment attachment)
-	{
-		string fullpath = AttachmentFiler.GetFullPath(attachment.RelativePath);
+    public static async Task DeleteAsync(Realm realm, Attachment attachment)
+    {
+        string fullpath = AttachmentFiler.GetFullPath(attachment.RelativePath);
 
-		if (File.Exists(fullpath))
-			File.Delete(fullpath);
+        if (File.Exists(fullpath))
+            File.Delete(fullpath);
 
-		await attachment.CommitAsync(() =>
-		{
-			if (attachment.HasDraft)
-				realm.Remove(attachment.Draft);
+        await attachment.CommitAsync(() =>
+        {
+            if (attachment.HasDraft)
+                realm.Remove(attachment.Draft);
 
-			realm.Remove(attachment);
-		});
-	}
+            realm.Remove(attachment);
+        });
+    }
 
-	public async Task DeleteAsync()
-	{
-		await DeleteAsync(Realm, this);
-	}
+    public async Task DeleteAsync()
+    {
+        await DeleteAsync(Realm, this);
+    }
 
     public Attachment() { }
 
