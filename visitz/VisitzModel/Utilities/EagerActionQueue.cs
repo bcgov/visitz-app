56d0c4145bd634b427e1cd5bfd0f4f813476e37a
Babu, George SDPR:EX <George.Babu+bcgovent@gov.bc.ca>
2025-03-28 09:33:13 -0700

    Converted indentation from tabs to space

--- visitz/VisitzModel/Utilities/EagerActionQueue.cs
+++ visitz/VisitzModel/Utilities/EagerActionQueue.cs
@@ -4,44 +4,44 @@ namespace VisitzModel.Utilities;
 
 public class EagerActionQueue(CancellationToken? cancellationToken = null)
 {
-	readonly CancellationToken cancelToken = cancellationToken ?? CancellationToken.None;
-	readonly ConcurrentQueue<(TaskCompletionSource, Func<Task>)> taskQueue = new();
-	Task writeFromQueue;
+    readonly CancellationToken cancelToken = cancellationToken ?? CancellationToken.None;
+    readonly ConcurrentQueue<(TaskCompletionSource, Func<Task>)> taskQueue = new();
+    Task writeFromQueue;
 
-	public bool HasCompleted => writeFromQueue?.IsCompleted ?? true;
+    public bool HasCompleted => writeFromQueue?.IsCompleted ?? true;
 
-	public Task EnqueueAsync(Func<Task> task)
-	{
-		TaskCompletionSource tcs = new();
+    public Task EnqueueAsync(Func<Task> task)
+    {
+        TaskCompletionSource tcs = new();
 
-		taskQueue.Enqueue((tcs, task));
+        taskQueue.Enqueue((tcs, task));
 
-		if (HasCompleted)
-			writeFromQueue = CreateWriteFromQueueTaskAsync();
+        if (HasCompleted)
+            writeFromQueue = CreateWriteFromQueueTaskAsync();
 
-		return tcs.Task;
-	}
+        return tcs.Task;
+    }
 
-	async Task CreateWriteFromQueueTaskAsync()
-	{
-		while (!taskQueue.IsEmpty)
-		{
-			cancelToken.ThrowIfCancellationRequested();
+    async Task CreateWriteFromQueueTaskAsync()
+    {
+        while (!taskQueue.IsEmpty)
+        {
+            cancelToken.ThrowIfCancellationRequested();
 
-			if (taskQueue.TryDequeue(out (TaskCompletionSource, Func<Task>) tuple))
-			{
-				var (tcs, task) = tuple;
+            if (taskQueue.TryDequeue(out (TaskCompletionSource, Func<Task>) tuple))
+            {
+                var (tcs, task) = tuple;
 
-				try
-				{
-					await task();
-					tcs.TrySetResult();
-				}
-				catch (Exception ex)
-				{
-					tcs.TrySetException(ex);
-				}
-			}
-		}
-	}
+                try
+                {
+                    await task();
+                    tcs.TrySetResult();
+                }
+                catch (Exception ex)
+                {
+                    tcs.TrySetException(ex);
+                }
+            }
+        }
+    }
 }
