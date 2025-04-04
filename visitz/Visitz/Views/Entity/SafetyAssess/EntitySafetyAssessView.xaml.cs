500e17bc08a7e85bdb2e34a81004d67784103380
Todd Scharien <todd.scharien@gov.bc.ca>
2025-04-04 16:53:52 -0700

    Rework EntitySafetyAssessView to use EditDraftAppBar
    Also, format files with space indentation and LF.

--- visitz/Visitz/Views/Entity/SafetyAssess/EntitySafetyAssessView.xaml.cs
+++ visitz/Visitz/Views/Entity/SafetyAssess/EntitySafetyAssessView.xaml.cs
@@ -4,34 +4,41 @@ using Visitz.Views.Snackbar;
 using VisitzModel.Events;
 using VisitzModel.Interfaces;
 using VisitzModel.Models;
+using VisitzModel.Models.SafetyAssess;
 
 namespace Visitz.Views.Entity.SafetyAssess;
 
 public partial class EntitySafetyAssessView : ViewModelContentView, ICaseloadItemHolder
 {
-	protected new EntitySafetyAssessViewModel ViewModel => (EntitySafetyAssessViewModel)base.ViewModel;
+    protected new EntitySafetyAssessViewModel ViewModel => (EntitySafetyAssessViewModel)base.ViewModel;
 
-	// It's preferable to use lifecycle methods to determine when auto-scrolling is allowed, but MAUI's lifecycles can
-	// be unreliable--so we'll use a time-delayed bool.
-	private bool canAutoScroll;
+    // It's preferable to use lifecycle methods to determine when auto-scrolling is allowed, but MAUI's lifecycles can
+    // be unreliable--so we'll use a time-delayed bool.
+    private bool canAutoScroll;
 
     private bool disposed;
 
     public CaseloadItem CaseloadItem 
-	{
-		get => ViewModel.CaseloadItem;
-		set => ViewModel.CaseloadItem = value;
-	}
+    {
+        get => ViewModel.CaseloadItem;
+        set => ViewModel.CaseloadItem = value;
+    }
 
-	public EntitySafetyAssessView() : base(ServiceProvider.GetService<EntitySafetyAssessViewModel>())
-	{
-		InitializeComponent();
-		BindingContext = ViewModel;
+    public SafetyAssessment Assessment
+    {
+        get => ViewModel.Assessment;
+        set => ViewModel.Assessment = value;
+    }
+
+    public EntitySafetyAssessView() : base(ServiceProvider.GetService<EntitySafetyAssessViewModel>())
+    {
+        InitializeComponent();
+        BindingContext = ViewModel;
 
         ViewModel.SaveStateHandler.SaveStateChanged += SaveStateHandler_SaveStateChanged;
-		
-		_ = DelayCanAutoScroll();
-	}
+        
+        _ = DelayCanAutoScroll();
+    }
 
     protected override void Dispose(bool disposing)
     {
@@ -45,29 +52,29 @@ public partial class EntitySafetyAssessView : ViewModelContentView, ICaseloadIte
     }
 
     private async Task DelayCanAutoScroll()
-	{
-		await Task.Delay(1500);
-		canAutoScroll = true;
-	}
+    {
+        await Task.Delay(1500);
+        canAutoScroll = true;
+    }
 
     private async void SaveStateHandler_SaveStateChanged(object sender, DraftSaveStatusEventArgs e)
     {
-        await DraftSavedIndicator.SetState(e.State);
+        await DraftAppBar.SetDraftState(e.State);
     }
 
     private async void DiscardButton_Clicked(object sender, EventArgs e)
     {
-		if (await PromptDiscard())
-		{
-			await ViewModel.Reset();
-			SnackbarHandler.ShowText(LocalizedStrings.DiscardedSafetyAssessmentDraft);
-		}
+        if (await PromptDiscard())
+        {
+            await ViewModel.Reset();
+            SnackbarHandler.ShowText(LocalizedStrings.DiscardedSafetyAssessmentDraft);
+        }
     }
 
-	private async static Task<bool> PromptDiscard()
-	{
+    private async static Task<bool> PromptDiscard()
+    {
         return await Navigator.CurrentOpenPage.DisplayAlert(
-			LocalizedStrings.DiscardDraftQuestion,
+            LocalizedStrings.DiscardDraftQuestion,
             LocalizedStrings.DiscardSafetyAssessmentDraftDescription,
             LocalizedStrings.Discard,
             LocalizedStrings.Cancel);
@@ -75,10 +82,10 @@ public partial class EntitySafetyAssessView : ViewModelContentView, ICaseloadIte
 
     private async void SomeChildrenPlaced_CheckedChanged(object sender, CheckedChangedEventArgs e)
     {
-		if (canAutoScroll && e.Value)
-		{
-			await Task.Delay(100);
-			await MainScrollView.ScrollToAsync(ChildrenInCareSection.X, ChildrenInCareSection.Y, true);
-		}
+        if (canAutoScroll && e.Value)
+        {
+            await Task.Delay(100);
+            await MainScrollView.ScrollToAsync(ChildrenInCareSection.X, ChildrenInCareSection.Y, true);
+        }
     }
 }
