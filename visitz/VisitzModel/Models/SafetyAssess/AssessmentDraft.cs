ada3b15a302006f8db11b100543a7ce78d43e87b
Todd Scharien <todd.scharien@gov.bc.ca>
2025-04-07 08:50:44 -0700

    Convert whitespace to spaces

--- visitz/VisitzModel/Models/SafetyAssess/AssessmentDraft.cs
+++ visitz/VisitzModel/Models/SafetyAssess/AssessmentDraft.cs
@@ -7,79 +7,79 @@ namespace VisitzModel.Models.SafetyAssess;
 
 public partial class AssessmentDraft : IRealmObject, IDraftItem
 {
-	[PrimaryKey]
-	public string DraftEntityId { get; set; }
+    [PrimaryKey]
+    public string DraftEntityId { get; set; }
 
-	public string RelatedEntityId { get => DraftEntityId; set { } }
+    public string RelatedEntityId { get => DraftEntityId; set { } }
 
-	public DateTimeOffset DraftCreated { get; set; } = DateTimeOffset.Now;
-	public DateTimeOffset LastUpdated { get; set; } = DateTimeOffset.Now;
+    public DateTimeOffset DraftCreated { get; set; } = DateTimeOffset.Now;
+    public DateTimeOffset LastUpdated { get; set; } = DateTimeOffset.Now;
 
-	public string Preview => GeneralStrings.SafetyAssessment;
+    public string Preview => GeneralStrings.SafetyAssessment;
 
-	public string DraftLocation { get; set; }
+    public string DraftLocation { get; set; }
 
-	int RelatedEntityTypeInt { get; set; } = (int)EntityType.Unknown;
-	public EntityType RelatedEntityType
-	{
-		get => (EntityType)RelatedEntityTypeInt;
-		set => RelatedEntityTypeInt = (int)value;
-	}
+    int RelatedEntityTypeInt { get; set; } = (int)EntityType.Unknown;
+    public EntityType RelatedEntityType
+    {
+        get => (EntityType)RelatedEntityTypeInt;
+        set => RelatedEntityTypeInt = (int)value;
+    }
 
-	int RelatedEntitySubtypeInt { get; set; } = (int)EntitySubtype.Unknown;
-	public EntitySubtype RelatedEntitySubtype
-	{
-		get => (EntitySubtype)RelatedEntitySubtypeInt;
-		set => RelatedEntitySubtypeInt = (int)value;
-	}
+    int RelatedEntitySubtypeInt { get; set; } = (int)EntitySubtype.Unknown;
+    public EntitySubtype RelatedEntitySubtype
+    {
+        get => (EntitySubtype)RelatedEntitySubtypeInt;
+        set => RelatedEntitySubtypeInt = (int)value;
+    }
 
     public static IQueryable<AssessmentDraft> GetAllByFileNumber(Realm realm, string fileNumber)
     {
         return realm.All<AssessmentDraft>().Where(d => d.DraftEntityId == fileNumber);
     }
 
-	public static async Task<AssessmentDraft> Upsert(
-		Realm realm,
-		SafetyAssessment assessment,
-		string draftLocation,
-		EntityType type = EntityType.Incident,
-		EntitySubtype subtype = EntitySubtype.ChildProtection)
-	{
-		var draft = realm.Find<AssessmentDraft>(assessment.IncidentNumber) ?? new()
-		{
-			DraftEntityId = assessment.IncidentNumber,
-		};
+    public static async Task<AssessmentDraft> Upsert(
+        Realm realm,
+        SafetyAssessment assessment,
+        string draftLocation,
+        EntityType type = EntityType.Incident,
+        EntitySubtype subtype = EntitySubtype.ChildProtection)
+    {
+        var draft = realm.Find<AssessmentDraft>(assessment.IncidentNumber) ?? new()
+        {
+            DraftEntityId = assessment.IncidentNumber,
+        };
 
-		await realm.WriteAsync(() =>
-		{
+        await realm.WriteAsync(() =>
+        {
             if (!assessment.IsManaged)
                 realm.Add(assessment);
 
-			draft.DraftLocation = draftLocation;
-			draft.RelatedEntityType = type;
-			draft.RelatedEntitySubtype = subtype;
-			draft.LastUpdated = DateTimeOffset.Now;
+            draft.DraftLocation = draftLocation;
+            draft.RelatedEntityType = type;
+            draft.RelatedEntitySubtype = subtype;
+            draft.LastUpdated = DateTimeOffset.Now;
 
-			realm.Add(draft, update: true);
-		});
+            realm.Add(draft, update: true);
+        });
 
-		return draft;
-	}
+        return draft;
+    }
 
-	public static async Task TryDeleteAsync(SafetyAssessment assessment)
-	{
+    public static async Task TryDeleteAsync(SafetyAssessment assessment)
+    {
         if (assessment?.Realm == null)
             return;
 
-		var realm = assessment.Realm;
+        var realm = assessment.Realm;
 
-		await realm.WriteAsync(() =>
-		{
-			if (realm.Find<AssessmentDraft>(assessment.IncidentNumber) is AssessmentDraft draft)
-				realm.Remove(draft);
+        await realm.WriteAsync(() =>
+        {
+            if (realm.Find<AssessmentDraft>(assessment.IncidentNumber) is AssessmentDraft draft)
+                realm.Remove(draft);
 
-			if (assessment.IsManaged)
-				realm.Remove(assessment);
-		});
-	}
+            if (assessment.IsManaged)
+                realm.Remove(assessment);
+        });
+    }
 }
