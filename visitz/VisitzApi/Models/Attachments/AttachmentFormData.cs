13e4d63c9f189b89a96628cdcdfddd68dc99a662
Todd Scharien <todd.scharien@gov.bc.ca>
2025-04-09 13:32:57 -0700

    Enable nullable and add constructor

--- visitz/VisitzApi/Models/Attachments/AttachmentFormData.cs
+++ visitz/VisitzApi/Models/Attachments/AttachmentFormData.cs
@@ -2,6 +2,8 @@ using System.Net.Http.Headers;
 
 namespace VisitzApi.Models.Attachments;
 
+#nullable enable
+
 public class AttachmentFormData
 {
     static readonly string ContentName = "Attachment Id";
@@ -10,21 +12,51 @@ public class AttachmentFormData
     static readonly string StatusName = "Status";
     static readonly string TemplateName = "Template";
 
+    public static readonly string DefaultStatus = "Complete";
     public static readonly string DefaultTemplate = "GENERICDOCUMENT";
 
     public string Filename { get; set; }
 
     public Stream FileContent { get; set; }
 
-    public string ContentType { get; set; }
+    public string ContentType { get; set; } = "";
 
-    public string Category { get; set; }
+    public string Category { get; set; } = "";
 
-    public string Description { get; set; }
+    public string Description { get; set; } = "";
 
-    public string Status { get; set; }
+    public string Status { get; set; } = DefaultStatus;
 
     public string Template { get; set; } = DefaultTemplate;
+
+
+    public AttachmentFormData(
+        string filename,
+        Stream contentStream,
+        string? contentType = null,
+        string? category = null,
+        string? description = null,
+        string? status = null,
+        string? template = null)
+    {
+        Filename = filename;
+        FileContent = contentStream;
+
+        if (contentType == null)
+            ContentType = MimeTypes.GetMimeType(filename);
+
+        if (category != null)
+            Category = category;
+
+        if (description != null)
+            Description = description;
+
+        if (status != null)
+            Status = status;
+
+        if (template != null)
+            Template = template;
+    }
 
     public MultipartFormDataContent ToFormDataContent()
     {
