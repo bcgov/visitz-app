36b4c56a47a72e5a2b9ddda41aae60fd0cbeb666
Todd Scharien <todd.scharien@gov.bc.ca>
2025-04-10 16:25:54 -0700

    Make DateOfAssessment nullable

--- visitz/VisitzModel/Models/SafetyAssess/SafetyAssessment.Bindings.cs
+++ visitz/VisitzModel/Models/SafetyAssess/SafetyAssessment.Bindings.cs
@@ -1,4 +1,4 @@
-﻿/*
+/*
 	Partial class implementation of a Realm + compiled bindings workaround.
 
 	https://github.com/realm/realm-dotnet/issues/2270#issuecomment-786720318
@@ -36,7 +36,7 @@ public partial class SafetyAssessment
         set => this.Commit(() => FamilyName = value);
     }
 
-    public DateTimeOffset DateOfAssessmentBinding
+    public DateTimeOffset? DateOfAssessmentBinding
     {
         get => IsValid ? DateOfAssessment : default;
         set => this.Commit(() => DateOfAssessment = value);
