058609b3d7c8aaec684e192d5e062139b424d801
Todd Scharien <todd.scharien@gov.bc.ca>
2025-04-15 11:55:28 -0700

    Move attachments list item impl into its own class

---
+++ visitz/Visitz/Views/Entity/Attachments/AttachmentsListItemView.xaml.cs
@@ -0,0 +1,13 @@
+using Visitz.Views.BaseClasses;
+
+namespace Visitz.Views.Entity.Attachments;
+
+#nullable enable
+
+public partial class AttachmentsListItemView : BaseContentView
+{
+	public AttachmentsListItemView()
+	{
+		InitializeComponent();
+	}
+}
