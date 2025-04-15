a0aa5319bbfafe04a3a4c20045135812ab9bfb1d
Todd Scharien <todd.scharien@gov.bc.ca>
2025-04-15 11:45:02 -0700

    Rename to AttachmentDraftsListItemView

--- visitz/Visitz/Views/Entity/Attachments/AttachmentDraftsListItemView.xaml.cs
+++ visitz/Visitz/Views/Entity/Attachments/AttachmentDraftsListItemView.xaml.cs
@@ -2,9 +2,9 @@ using Visitz.Views.BaseClasses;
 
 namespace Visitz.Views.Entity.Attachments;
 
-public partial class AttachmentListItemView : BaseContentView
+public partial class AttachmentDraftsListItemView : BaseContentView
 {
-    public AttachmentListItemView()
+    public AttachmentDraftsListItemView()
     {
         InitializeComponent();
     }
