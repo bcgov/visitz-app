22dab4e32ead010bd52719b602c0dbc305eae1ae
Todd Scharien <todd.scharien@gov.bc.ca>
2025-04-23 10:09:54 -0700

    Rework details' view models
    Pulled common code into a base, abstract view model.

--- visitz/Visitz/Views/Entity/Attachments/PdfDetailsViewModel.cs
+++ visitz/Visitz/Views/Entity/Attachments/PdfDetailsViewModel.cs
@@ -1,67 +1,27 @@
 using CommunityToolkit.Mvvm.ComponentModel;
-using CommunityToolkit.Mvvm.Input;
 using Microsoft.Extensions.Logging;
 using System.Reflection;
 using Visitz.Resources.Localization;
-using Visitz.Storage;
-using Visitz.Views.BaseClasses;
-using Visitz.Views.BaseClasses.Publishing;
-using Visitz.Views.Snackbar;
 using VisitzModel.Extensions;
 using VisitzModel.Interfaces;
-using VisitzModel.Models;
 using VisitzModel.Models.Attachments;
-using VisitzModel.Storage.Filesystem;
 
 namespace Visitz.Views.Entity.Attachments;
 
 #nullable enable
 
-internal partial class PdfDetailsViewModel : VisitzViewModel, ICaseloadItemHolder
+internal partial class PdfDetailsViewModel : AttachmentDetailsViewModel, ICaseloadItemHolder
 {
     static readonly string EmbedHtmlPath = Path.Join("PDF", "pdf-embed.html");
 
     [ObservableProperty]
     public WebViewSource? source;
 
-    [ObservableProperty]
-    public bool showActivityIndicator = true;
-
-    [ObservableProperty]
-    public string? errorText;
-
-    [ObservableProperty]
-    public bool hasError;
-
-    [ObservableProperty]
-    public bool showDraftButtons;
-
-    [ObservableProperty]
-    public bool isRemovable;
-
-    [ObservableProperty]
-    public Attachment? attachment;
-
-    public CaseloadItem? CaseloadItem { get; set; }
-
-    public bool IsDownloadedAttachment { get; set; }
-
-    AttachmentFiler? Filer { get; set; }
+    protected override string LoadErrorText => LocalizedStrings.PdfContentMissing;
 
     protected override async Task InitAsync()
     {
         await base.InitAsync();
-
-        if (Attachment == null || CaseloadItem == null)
-        {
-            ErrorText = LocalizedStrings.PdfContentMissing;
-            return;
-        }
-
-        Filer = await VisitzFiles.GetAsync(
-            Attachment,
-            CaseloadItem.KeyPlayer.FirstName,
-            CaseloadItem.KeyPlayer.LastName);
 
         if (Assembly.GetEntryAssembly() is Assembly entry)
             Source = GetEmbedPath(entry);
@@ -74,22 +34,11 @@ internal partial class PdfDetailsViewModel : VisitzViewModel, ICaseloadItemHolde
 
             return;
         }
-
-        ShowDraftButtons = Attachment.FileExistsLocally
-            && !IsDownloadedAttachment
-            && Attachment.HasDraft;
-
-        IsRemovable = Attachment.FileExistsLocally && IsDownloadedAttachment;
     }
 
     static string? GetEmbedPath(Assembly entry)
     {
         return Path.Join(Path.GetDirectoryName(entry.Location), EmbedHtmlPath);
-    }
-
-    partial void OnErrorTextChanged(string? value)
-    {
-        HasError = value?.Length > 0;
     }
 
     public async Task<string> MakeBase64Pdf()
@@ -101,67 +50,5 @@ internal partial class PdfDetailsViewModel : VisitzViewModel, ICaseloadItemHolde
         }
         else
             return "";
-    }
-
-    [RelayCommand]
-    async Task PromptRemoveFromDeviceAsync()
-    {
-        if (Attachment == null)
-            return;
-
-        bool shouldRemove = await Navigator.CurrentOpenPage.DisplayAlert(
-            LocalizedStrings.RemoveAttachmentFromDevice,
-            LocalizedStrings.RemoveAttachmentDescription,
-            LocalizedStrings.Remove,
-            LocalizedStrings.Cancel);
-
-        if (shouldRemove)
-        {
-            Attachment.RemoveFileFromDevice();
-
-            string removedText = string.Format(
-                LocalizedStrings.RemovedAttachmentFromDevice,
-                Attachment.Filename);
-
-            await Navigator.Navigation.PopAsync();
-            SnackbarHandler.ShowText(removedText);
-        }
-    }
-
-    [RelayCommand]
-    async Task PromptDiscardAttachmentAsync()
-    {
-        if (Attachment == null || !Attachment.HasDraft)
-            return;
-
-        bool shouldDiscard = await Navigator.CurrentOpenPage.DisplayAlert(
-            LocalizedStrings.DiscardDraft,
-            LocalizedStrings.DiscardAttachmentDraftDescription,
-            LocalizedStrings.Discard,
-            LocalizedStrings.Cancel);
-
-        if (shouldDiscard)
-        {
-            string filename = Attachment.Filename;
-
-            await Attachment.DeleteAsync();
-            await Navigator.Navigation.PopAsync();
-
-            SnackbarHandler.ShowText(LocalizedStrings.FileDiscarded.Format(filename));
-        }
-    }
-
-    [RelayCommand]
-    async Task PublishAttachmentDraftAsync()
-    {
-        var attachmentPublishVm = ServiceProvider.Current.GetService<AttachmentDraftPublishViewModel>();
-
-        if (Attachment?.Draft == null || attachmentPublishVm == null || Filer == null)
-            return;
-
-        attachmentPublishVm.AttachmentDraft = Attachment.Draft;
-        attachmentPublishVm.AttachmentFiler = Filer;
-
-        await Navigator.Navigation.PushModalAsync(new PublishPage(attachmentPublishVm));
     }
 }
--- visitz/Visitz/Views/Entity/Attachments/PhotoDetailsViewModel.cs
+++ visitz/Visitz/Views/Entity/Attachments/PhotoDetailsViewModel.cs
@@ -1,125 +1,26 @@
 using CommunityToolkit.Mvvm.ComponentModel;
-using CommunityToolkit.Mvvm.Input;
 using Visitz.Resources.Localization;
-using Visitz.Storage;
-using Visitz.Views.BaseClasses;
-using Visitz.Views.BaseClasses.Publishing;
-using Visitz.Views.Snackbar;
-using VisitzModel.Extensions;
-using VisitzModel.Extensions.EntityTypes;
 using VisitzModel.Interfaces;
-using VisitzModel.Models;
 using VisitzModel.Models.Attachments;
-using VisitzModel.Storage.Filesystem;
 
 namespace Visitz.Views.Entity.Attachments;
 
-internal partial class PhotoDetailsViewModel : VisitzViewModel, ICaseloadItemHolder
+internal partial class PhotoDetailsViewModel : AttachmentDetailsViewModel, ICaseloadItemHolder
 {
-    [ObservableProperty]
-    public Attachment attachment;
-
     [ObservableProperty]
     public ImageSource detailImage;
 
-    [ObservableProperty]
-    public CaseloadItem caseloadItem;
-
-    [ObservableProperty]
-    public bool showDraftButtons;
-
-    [ObservableProperty]
-    public bool isRemovable;
-
-    public bool IsDownloadedAttachment { get; set; }
-
-    AttachmentFiler attachmentFiler;
+    protected override string LoadErrorText => LocalizedStrings.ImageContentMissing;
 
     protected override async Task InitAsync()
     {
         await base.InitAsync();
-
-        ShowDraftButtons = Attachment.FileExistsLocally
-            && !IsDownloadedAttachment
-            && Attachment.HasDraft;
-
-        IsRemovable = Attachment.FileExistsLocally && IsDownloadedAttachment;
-
-        attachmentFiler = await VisitzFiles.GetAsync(
-            CaseloadItem.EntityType.ParseEntityType(),
-            CaseloadItem.CaseIncidentNumber,
-            CaseloadItem.KeyPlayer.FirstName,
-            CaseloadItem.KeyPlayer.LastName);
 
         DetailImage = ImageSource.FromStream(GetPhoto);
     }
 
     async Task<Stream> GetPhoto(CancellationToken token)
     {
-        return await attachmentFiler.GetAppDataFileAsync(Attachment.RelativePath, token);
-    }
-
-    [RelayCommand]
-    public static void DeleteAttachmentDraft(Attachment attachment)
-    {
-        if (attachment.HasDraft)
-            _ = PromptDiscardAttachmentAsync(attachment);
-    }
-
-    static async Task PromptDiscardAttachmentAsync(Attachment attachment)
-    {
-        bool shouldDiscard = await Navigator.CurrentOpenPage.DisplayAlert(
-            LocalizedStrings.DiscardDraft,
-            LocalizedStrings.DiscardAttachmentDraftDescription,
-            LocalizedStrings.Discard,
-            LocalizedStrings.Cancel);
-
-        if (shouldDiscard)
-        {
-            string filename = attachment.Filename;
-
-            await attachment.DeleteAsync();
-            await Navigator.Navigation.PopAsync();
-
-            SnackbarHandler.ShowText(LocalizedStrings.FileDiscarded.Format(filename));
-        }
-    }
-
-    [RelayCommand]
-    public void PublishAttachmentDraft(Attachment attachment)
-    {
-        if (attachment.HasDraft)
-            _ = DoPublishAttachmentDraft(attachment.Draft);
-    }
-
-    async Task DoPublishAttachmentDraft(AttachmentDraft draft)
-    {
-        var attachmentPublishVm = ServiceProvider.Current.GetService<AttachmentDraftPublishViewModel>();
-        attachmentPublishVm.AttachmentDraft = draft;
-        attachmentPublishVm.AttachmentFiler = attachmentFiler;
-
-        await Navigator.Navigation.PushModalAsync(new PublishPage(attachmentPublishVm));
-    }
-
-    [RelayCommand]
-    static async Task PromptRemoveFromDevice(Attachment attachment)
-    {
-        bool shouldRemove = await Navigator.CurrentOpenPage.DisplayAlert(
-            LocalizedStrings.RemoveAttachmentFromDevice,
-            LocalizedStrings.RemoveAttachmentDescription,
-            LocalizedStrings.Remove,
-            LocalizedStrings.Cancel);
-
-        if (shouldRemove)
-        {
-            attachment.RemoveFileFromDevice();
-
-            string removedText = string.Format(
-                LocalizedStrings.RemovedAttachmentFromDevice,
-                attachment.Filename);
-
-            await Navigator.Navigation.PopAsync();
-            SnackbarHandler.ShowText(removedText);
-        }
+        return await Filer.GetAppDataFileAsync(Attachment.RelativePath, token);
     }
 }
