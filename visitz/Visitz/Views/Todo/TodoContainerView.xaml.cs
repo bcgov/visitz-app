9a1b43d87a639f9855f90beca640600100919b2c
Babu, George SDPR:EX <George.Babu+bcgovent@gov.bc.ca>
2025-05-01 15:14:49 -0700

    Moved Creating code into the constructor and removed global var IView

--- visitz/Visitz/Views/Todo/TodoContainerView.xaml.cs
+++ visitz/Visitz/Views/Todo/TodoContainerView.xaml.cs
@@ -4,21 +4,14 @@ namespace Visitz.Views.Todo;
 
 public partial class TodoContainerView : SplitLayoutView
 {
-    static IView StartView;
-
     public TodoContainerView()
     {
         InitializeComponent();
-    }
-
-    protected override void Creating()
-    {
-        base.Creating();
 
         StartPaneColumnWidth = GridLength.Auto;
         StartPane.MinimumWidthRequest = SplitLayoutDimensions.MinimumStartPaneWidth;
 
-        StartView ??= ServiceProvider.GetService<TodoMasterList>();
+        var StartView = ServiceProvider.GetService<TodoMasterList>();
         SetStartPane(StartView);
     }
 }
