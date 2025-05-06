3e0a65f21e9cc88c6b959d92e22c67f57a75a8be
Babu, George SDPR:EX <George.Babu+bcgovent@gov.bc.ca>
2025-05-06 14:02:25 -0700

    Todo master listing - completed without new viewmodel

---
+++ visitz/Visitz/Views/Todo/TodoItemUi.cs
@@ -0,0 +1,54 @@
+using CommunityToolkit.Mvvm.ComponentModel;
+using Realms;
+using Visitz.Resources.Localization;
+using VisitzModel.Models;
+
+namespace Visitz.Views.Todo;
+
+public partial class TodoItemUi : ObservableObject
+{
+    readonly ObservableRealmQueryMap realmQuery = new();
+
+    List<IRealmObject> todoItems = [];
+
+    [ObservableProperty]
+    int count;
+
+    [ObservableProperty]
+    string itemName;
+
+    public TodoItemUi(IQueryable<IRealmObject> query, Realm icmDataRealm)
+    {
+        realmQuery.ItemsChanged += RealmQuery_ItemsChanged;
+        realmQuery.Subscribe(icmDataRealm, query);
+    }
+    private void RealmQuery_ItemsChanged(object sender, (Type Type, IRealmCollection<IRealmObject> Items, ChangeSet Changes) e)
+    {
+        UpdateTodoItemsList(e.Items, e.Changes);
+    }
+
+    private void UpdateTodoItemsList(IRealmCollection<IRealmObject> items, ChangeSet changes)
+    {
+        if (changes == null)
+        {
+            foreach (var item in items)
+                todoItems.Add(item);
+        }
+        else
+        {
+            foreach (int deleted in changes.DeletedIndices.Reverse())
+                todoItems.RemoveAt(deleted);
+
+            foreach (int inserted in changes.InsertedIndices)
+                todoItems.Insert(inserted, items[inserted]);
+        }
+        Count = todoItems.Count;
+        ItemName = LocalizedStrings.ChildYouthVisits;
+    }
+
+    public void Dispose()
+    {
+        realmQuery.ItemsChanged -= RealmQuery_ItemsChanged;
+        realmQuery.Dispose();
+    }
+}
