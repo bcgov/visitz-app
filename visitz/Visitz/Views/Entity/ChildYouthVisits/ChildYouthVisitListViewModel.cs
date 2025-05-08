368b2046fc97957b6233da22aa1a7ac8eff76465
Babu, George SDPR:EX <George.Babu+bcgovent@gov.bc.ca>
2025-05-08 08:12:15 -0700

    Made DueDate a property and updated the banner logic

--- visitz/Visitz/Views/Entity/ChildYouthVisits/ChildYouthVisitListViewModel.cs
+++ visitz/Visitz/Views/Entity/ChildYouthVisits/ChildYouthVisitListViewModel.cs
@@ -93,15 +93,12 @@ internal partial class ChildYouthVisitListViewModel :
         HasVisitData = personVisits.Count > 0;
         ShowEmptyIcon = !HasVisitData;
         if (personVisits.FirstOrDefault() is PersonVisit lastVisit)
-        {
-            DateTimeOffset nextVisitDate = lastVisit.DateOfVisit.AddDays((double)VisitDaysThreshold.Info);
-            string dueDate = nextVisitDate.ToString("MMMM d, yyyy");
-            SetBannerInfo(lastVisit, dueDate);
-        }
+            SetBannerInfo(lastVisit);
     }
 
-    private void SetBannerInfo(PersonVisit personVisit, string dateInBanner)
+    private void SetBannerInfo(PersonVisit personVisit)
     {
+        string dateInBanner = personVisit.DueDate.ToString("MMMM d, yyyy");
         var threshold = personVisit.CurrentDueDateThreshold;
         switch (threshold)
         {
