d9659c1ab3ad07a5a14a93bda958c06827dc8a37
Babu, George SDPR:EX <George.Babu+bcgovent@gov.bc.ca>
2025-05-26 14:56:59 -0700

    Removed unwanted code

--- visitz/Visitz/Views/Todo/TodoVisitsView.xaml.cs
+++ visitz/Visitz/Views/Todo/TodoVisitsView.xaml.cs
@@ -1,7 +1,4 @@
-using CommunityToolkit.Mvvm.Messaging;
-using Visitz.Messaging;
 using Visitz.Views.BaseClasses;
-using VisitzModel.Models.Navigation;
 
 namespace Visitz.Views.Todo;
 
@@ -13,43 +10,5 @@ public partial class TodoVisitsView : ViewModelContentView
     {
         InitializeComponent();
         BindingContext = ViewModel;
-    }
-
-    protected override async Task InitAsync()
-    {
-        await base.InitAsync();
-        await SendTodoMasterSelectedMessageWhenReady();
-    }
-
-    private async Task SendTodoMasterSelectedMessageWhenReady()
-    {
-        while (!ViewModel.IsInitialized)
-        {
-            await Task.Delay(100);
-        }
-
-        StrongReferenceMessenger.Default.Register<TodoMasterSelectedMessage>(this, (recipient, message) =>
-        {
-            var navItem = message.Value;
-
-            if (navItem != null)
-                (recipient as TodoVisitsView).OpenTodoSection(navItem);
-        });
-    }
-
-    bool disposed;
-    protected override void Dispose(bool disposing)
-    {
-        if (!disposed && disposing)
-        {
-            StrongReferenceMessenger.Default.UnregisterAll(this);
-            disposed = true;
-        }
-        base.Dispose(disposing);
-    }
-
-    private void OpenTodoSection(NavItem navItem)
-    {
-        ViewModel.LoadTodoItemsForNavItem(navItem);
     }
 }
