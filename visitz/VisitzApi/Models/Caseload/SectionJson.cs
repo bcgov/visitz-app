3f75495c42b76180c102eb321b20f3e05448408a
Todd Scharien <todd.scharien@gov.bc.ca>
2025-06-02 15:36:32 -0700

    Remove unused using

--- visitz/VisitzApi/Models/Caseload/SectionJson.cs
+++ visitz/VisitzApi/Models/Caseload/SectionJson.cs
@@ -2,7 +2,6 @@ using System.Text.Json;
 using System.Text.Json.Nodes;
 using System.Text.Json.Serialization;
 using VisitzApi.Models.Base;
-using static System.Collections.Specialized.BitVector32;
 
 namespace VisitzApi.Models.Caseload;
 
