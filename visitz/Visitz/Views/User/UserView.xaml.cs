4de54627c9e49dec1a0839350054a931d05c2e45
Todd Scharien <todd.scharien@gov.bc.ca>
2025-06-02 15:36:24 -0700

    Convert indentation to spaces

--- visitz/Visitz/Views/User/UserView.xaml.cs
+++ visitz/Visitz/Views/User/UserView.xaml.cs
@@ -4,9 +4,9 @@ namespace Visitz.Views.User;
 
 public partial class UserView : ViewModelContentView
 {
-	public UserView() : base(ServiceProvider.GetService<UserViewModel>())
-	{
-		InitializeComponent();
+    public UserView() : base(ServiceProvider.GetService<UserViewModel>())
+    {
+        InitializeComponent();
         BindingContext = ViewModel;
-	}
+    }
 }
