ecd31388e92438624e25af10ee0a1a3e28c5a4be
Todd Scharien <todd.scharien@gov.bc.ca>
2025-07-15 13:31:10 -0700

    Remove required attributes

--- visitz/VisitzApi/Models/Base/AssignableRecordJson.cs
+++ visitz/VisitzApi/Models/Base/AssignableRecordJson.cs
@@ -1,12 +1,8 @@
-using System.Text.Json.Serialization;
-
 namespace VisitzApi.Models.Base;
 
 public class AssignableRecordJson : BaseRecordJson
 {
-    [JsonRequired]
     public string AssignedTo { get; set; }
 
-    [JsonRequired]
     public string AssignedToId { get; set; }
 }
