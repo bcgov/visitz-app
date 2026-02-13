f96be5af722c1adad6d8c9b249431b79ab654070
Mohan, Balachandrika <bamohan@deloitte.com>
2026-02-13 13:14:42 +0530

    STRY0042769-windows camera and micropgone issue fix

--- visitz/Visitz/Device/DevicePermissions.cs
+++ visitz/Visitz/Device/DevicePermissions.cs
@@ -1,14 +1,48 @@
 namespace Visitz.Device;
+#if WINDOWS
+using Windows.Security.Authorization.AppCapabilityAccess;
+using Windows.Media.Capture;
+#endif
 
 public static class DevicePermissions
 {
     public async static Task<PermissionStatus> PromptEnsureCameraAsync()
     {
+        //Checking camera access for non Window devices
         PermissionStatus status = await Permissions.CheckStatusAsync<Permissions.Camera>();
+
+        //Checking camera and microphone access for Window devices
+        #if WINDOWS
+        try
+        {
+            var mediaCapture = new MediaCapture();
+
+            //Configuring both audio and video to check microphone and camera access
+            var settings = new MediaCaptureInitializationSettings
+            {
+                StreamingCaptureMode = StreamingCaptureMode.AudioAndVideo,
+            };
+
+            await mediaCapture.InitializeAsync(settings);
+            status = PermissionStatus.Granted;
+        }
+        catch (UnauthorizedAccessException)
+        {
+            status = PermissionStatus.Denied;
+        }
+        catch
+        {
+            // Other error (camera missing, in use, etc.)
+            status = PermissionStatus.Unknown;
+        }
+        #else
+
         if (status == PermissionStatus.Unknown)
         {
             status = await Permissions.RequestAsync<Permissions.Camera>();
         }
+        #endif
+
         return status;
     }
 }
