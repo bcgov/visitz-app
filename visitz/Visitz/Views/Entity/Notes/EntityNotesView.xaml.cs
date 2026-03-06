ccd848d01394726cd64986a4309f16797fe1ac27
Ramprakash S <Ramprakash.S@gov.bc.ca>
2026-03-06 15:18:48 +0530

    STRY0044005 - Implementation of Selecting and copying case/incident notes

--- visitz/Visitz/Views/Entity/Notes/EntityNotesView.xaml.cs
+++ visitz/Visitz/Views/Entity/Notes/EntityNotesView.xaml.cs
@@ -1,3 +1,4 @@
+using Microsoft.Maui.Handlers;
 using System.Text;
 using Visitz.Views.BaseClasses;
 using VisitzModel.Interfaces;
@@ -30,6 +31,22 @@ public partial class EntityNotesView :
     {
         InitializeComponent();
         BindingContext = ViewModel;
+        EditorHandler.Mapper.AppendToMapping("NoBorder", (handler, view) =>
+        {
+#if ANDROID
+            handler.PlatformView.Background = null;
+#endif
+
+#if IOS || MACCATALYST
+            handler.PlatformView.Layer.BorderWidth = 0;
+            handler.PlatformView.Layer.CornerRadius = 0;
+#endif
+
+#if WINDOWS
+            handler.PlatformView.BorderThickness = new Microsoft.UI.Xaml.Thickness(0);
+#endif
+        });
+
     }
 
     private async void NotesCollectionView_Loaded(object sender, EventArgs e)
@@ -45,26 +62,5 @@ public partial class EntityNotesView :
             NotesCollectionView.ScrollTo(item, noteItemGroup, position: ScrollToPosition.End, animate: false);
     }
 
-    private async void OnCopyClicked(object sender, EventArgs e)
-    {
-        if (BindingContext is EntityNotesViewModel viewModel)
-        {
-            if (viewModel.Notes == null || !viewModel.Notes.Any())
-                return;
-            var builder = new StringBuilder();
-            foreach (var group in viewModel.Notes)
-            {
-                // Add Group Name
-                builder.AppendLine(group.Name);
-                builder.AppendLine(new string('-', group.Name.Length));
-                foreach (var item in group)
-                {
-                    builder.AppendLine(item.Content);
-                }
-                builder.AppendLine(); // spacing between groups
-            }
-            string finalText = builder.ToString();
-            await Clipboard.Default.SetTextAsync(finalText);
-        }
-    }
+  
 }
