2539492c58de021e68ffee8ec80b5698907c1dec
Ramprakashbcgov <Ramprakash.S@gov.bc.ca>
2026-03-26 13:27:05 +0530

    Set default values for string properties in AdditionalInformationJson
    Initialized string properties in AdditionalInformationJson to avoid null references.

--- visitz/VisitzApi/Models/CallDetails/AdditionalInformationJson.cs
+++ visitz/VisitzApi/Models/CallDetails/AdditionalInformationJson.cs
@@ -4,18 +4,18 @@ using System.Text;
 using Microsoft.VisualBasic;
 
 namespace VisitzApi.Models.CallDetails;
-
+#nullable enable
 public class AdditionalInformationJson
 {
-    public string Id { get; set; }
-    public string AdditionalInformation { get; set; }
-    public string Created { get; set; }
-    public string CreatedBy { get; set; }
-    public string CreatedByName { get; set; }
-    public string Updated { get; set; }
-    public string UpdatedBy { get; set; }
-    public string UpdatedByName { get; set; }
-    public string IncidentId { get; set; }
-    public string SRId { get; set; }
-    public string MemoId { get; set; }
+    public string Id { get; set; } = string.Empty;
+    public string AdditionalInformation { get; set; }= string.Empty;
+    public string Created { get; set; }= string.Empty;
+    public string CreatedBy { get; set; }= string.Empty;
+    public string CreatedByName { get; set; }= string.Empty;
+    public string Updated { get; set; }= string.Empty;
+    public string UpdatedBy { get; set; }= string.Empty;
+    public string UpdatedByName { get; set; }= string.Empty;
+    public string IncidentId { get; set; }= string.Empty;
+    public string SRId { get; set; }= string.Empty;
+    public string MemoId { get; set; }= string.Empty;
 }
