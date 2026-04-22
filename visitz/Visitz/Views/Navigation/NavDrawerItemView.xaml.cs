4fa05cf089b9b4734a2d5ef3713ac86b87f7e8ea
Todd Scharien <todd.scharien@gov.bc.ca>
2026-04-22 14:33:46 -0700

    Rework SelectionLayoutBehavior

--- visitz/Visitz/Views/Navigation/NavDrawerItemView.xaml.cs
+++ visitz/Visitz/Views/Navigation/NavDrawerItemView.xaml.cs
@@ -1,31 +1,14 @@
 using System.Windows.Input;
-using Visitz.VisualStates;
 
 namespace Visitz.Views.Navigation;
 
-public partial class NavDrawerItemView : ContentView, ISelectedState
+public partial class NavDrawerItemView : ContentView
 {
-    public static readonly BindableProperty IsSelectedProperty = BindableProperty.Create(
-        nameof(IsSelected),
-        typeof(bool),
-        typeof(NavDrawerItemView)
-    );
-
     public static readonly BindableProperty TappedCommandProperty = BindableProperty.Create(
         nameof(TappedCommand),
         typeof(ICommand),
         typeof(NavDrawerItemView)
     );
-
-    public bool IsSelected
-    {
-        get => (bool)GetValue(IsSelectedProperty);
-        set
-        {
-            SetValue(IsSelectedProperty, value);
-            VisualStateManager.GoToState(this, (this as ISelectedState).GetSelectedState());
-        }
-    }
 
     public ICommand TappedCommand
     {
