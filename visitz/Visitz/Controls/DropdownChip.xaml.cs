63924b7590704b58bf13c6ec3fc89c0b7f764d1a
Todd Scharien <todd.scharien@gov.bc.ca>
2026-05-11 10:26:43 -0700

    Use Placeholder prop for null selections

--- visitz/Visitz/Controls/DropdownChip.xaml.cs
+++ visitz/Visitz/Controls/DropdownChip.xaml.cs
@@ -10,6 +10,9 @@ public partial class DropdownChip : ContentView
     [BindableProperty]
     public partial string Text { get; set; } = string.Empty;
 
+    [BindableProperty(PropertyChangedMethodName = nameof(OnPlaceholderChanged))]
+    public partial string Placeholder { get; set; } = string.Empty;
+
     [BindableProperty]
     public partial IEnumerable<IOption> Items { get; set; } = [];
 
@@ -19,9 +22,19 @@ public partial class DropdownChip : ContentView
     [BindableProperty]
     public partial bool StickySelection { get; set; }
 
+    static void OnPlaceholderChanged(BindableObject bindable, object _, object __)
+    {
+        if (bindable is DropdownChip view)
+            view.ApplySelectionText();
+    }
+
     static void OnSelectedItemChanged(BindableObject bindable, object _, object __)
     {
-        ((DropdownChip)bindable).Popup.Dismiss();
+        if (bindable is DropdownChip view)
+        {
+            view.ApplySelectionText();
+            view.Popup.Dismiss();
+        }
     }
 
     public DropdownChip()
@@ -29,9 +42,14 @@ public partial class DropdownChip : ContentView
         InitializeComponent();
     }
 
-    private void SfChip_Clicked(object? sender, EventArgs e)
+    void SfChip_Clicked(object? sender, EventArgs e)
     {
         if (sender != null)
             Popup.ShowRelativeToView((View)sender, PopupRelativePosition.AlignBottom);
+    }
+
+    void ApplySelectionText()
+    {
+        Text = SelectedOption?.Text ?? Placeholder;
     }
 }
