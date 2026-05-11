5b3bc9f62da1e30792755319a96bb18be6626f11
Todd Scharien <todd.scharien@gov.bc.ca>
2026-05-11 11:46:54 -0700

    Implement IEquatable for FilterOption

--- visitz/Visitz/Controls/FilterOption.cs
+++ visitz/Visitz/Controls/FilterOption.cs
@@ -2,11 +2,32 @@ namespace Visitz.Controls;
 
 #nullable enable
 
-public class FilterOption<TItem>(string label, Func<TItem, bool> wherePredicate) : IOption
+public class FilterOption<TItem>(string label, Func<TItem, bool> wherePredicate)
+    : IOption,
+        IEquatable<FilterOption<TItem>>
 {
     public Func<TItem, bool> WherePredicate { get; set; } = wherePredicate;
 
     public bool Selected { get; set; }
 
     public string Text { get; set; } = label;
+
+    public bool Equals(FilterOption<TItem>? other)
+    {
+        return ReferenceEquals(this, other)
+            || (
+                Text.Equals(other?.Text, StringComparison.InvariantCultureIgnoreCase)
+                && WherePredicate == other?.WherePredicate
+            );
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(WherePredicate, Text);
+    }
+
+    public override bool Equals(object? obj)
+    {
+        return obj is FilterOption<TItem> filter ? Equals(filter) : Equals(this, obj);
+    }
 }
