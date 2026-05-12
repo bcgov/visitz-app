bdfded177ebc478be6019db52bdfc1043e844bd6
Todd Scharien <todd.scharien@gov.bc.ca>
2026-05-12 15:58:30 -0700

    Flesh out defaults for TopAppBar

--- visitz/Visitz/Controls/TopAppBar.cs
+++ visitz/Visitz/Controls/TopAppBar.cs
@@ -2,19 +2,22 @@ using Visitz.Resources.Styles;
 
 namespace Visitz.Controls;
 
-internal class TopAppBar : Grid
+internal partial class TopAppBar : Grid
 {
-    private static readonly double DefaultThickness = 10;
     private static readonly Color DefaultBackgroundColor = VisitzColors.Default_Background;
     private static readonly int DefaultZIndex = 2;
 
     public TopAppBar()
         : base()
     {
-        Padding = new Thickness(DefaultThickness);
         BackgroundColor = DefaultBackgroundColor;
-        HeightRequest = VisitzDimensions.TopAppBarHeight;
+        ColumnSpacing = VisitzDimensions.DefaultSpacing;
+        IsClippedToBounds = true;
+        MinimumHeightRequest = VisitzDimensions.TopAppBarHeight;
+        Padding = VisitzDimensions.DefaultSpacing;
+        RowSpacing = VisitzDimensions.DefaultSpacing;
         Shadow = VisitzShadows.RestingLevel2;
+        VerticalOptions = LayoutOptions.Start;
         ZIndex = DefaultZIndex;
     }
 }
