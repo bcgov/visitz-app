026f26a839058b9814159a424194683c75fe01c1
Todd Scharien <todd.scharien@gov.bc.ca>
2026-05-13 13:10:14 -0700

    Dynamically add/remove filter options for drafts

--- visitz/Visitz/Views/Drafts/DraftsContainerViewModel.cs
+++ visitz/Visitz/Views/Drafts/DraftsContainerViewModel.cs
@@ -2,8 +2,16 @@ using System.Collections.ObjectModel;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Messaging;
 using Visitz.Controls;
+using Visitz.Resources.Localization;
+using Visitz.Storage;
 using Visitz.Views.BaseClasses;
+using VisitzModel.Extensions;
+using VisitzModel.Models;
+using VisitzModel.Models.Attachments;
 using VisitzModel.Models.Drafts;
+using VisitzModel.Models.InPersonVisits;
+using VisitzModel.Models.Notes;
+using VisitzModel.Models.SafetyAssess;
 
 namespace Visitz.Views.Drafts;
 
@@ -11,8 +19,29 @@ namespace Visitz.Views.Drafts;
 
 public partial class DraftsContainerViewModel : VisitzViewModel
 {
+    static readonly FilterOption<IDraftItem> s_allDrafts = new(LocalizedStrings.AllTypes, _ => true);
+
+    readonly FilterOption<IDraftItem> _attachmentsFilter = new(
+        LocalizedStrings.Attachments,
+        draft => draft is AttachmentDraft
+    );
+
+    readonly FilterOption<IDraftItem> _notesFilter = new(LocalizedStrings.Notes, draft => draft is NoteDraft);
+
+    readonly FilterOption<IDraftItem> _safetyAssessmentFilter = new(
+        LocalizedStrings.SafetyAssessments,
+        draft => draft is AssessmentDraft
+    );
+
+    readonly FilterOption<IDraftItem> _visitsFilter = new(
+        LocalizedStrings.ChildYouthVisits,
+        draft => draft is PersonVisitDraft
+    );
+
+    readonly ObservableRealmCount _realmCount = new();
+
     [ObservableProperty]
-    public partial ObservableCollection<FilterOption<IDraftItem>> FilterOptions { get; set; } = [];
+    public partial ObservableCollection<FilterOption<IDraftItem>> FilterOptions { get; set; } = [s_allDrafts];
 
     [ObservableProperty]
     public partial FilterOption<IDraftItem>? SelectedFilter { get; set; }
@@ -20,19 +49,48 @@ public partial class DraftsContainerViewModel : VisitzViewModel
     protected override async Task InitAsync()
     {
         await base.InitAsync();
+
+        _realmCount.CountChanged += RealmCount_CountChanged;
+
+        _realmCount.Subscribe<AttachmentDraft>(await VisitzRealms.GetAttachmentDraftsRealmAsync());
+        _realmCount.Subscribe<NoteDraft>(await VisitzRealms.GetNoteDraftsRealmAsync());
+        _realmCount.Subscribe<AssessmentDraft>(await VisitzRealms.GetSafetyAssessmentDraftRealmAsync());
+        _realmCount.Subscribe<PersonVisitDraft>(await VisitzRealms.GetPersonVisitDraftsRealmAsync());
     }
 
-    bool disposed;
+    bool _disposed;
 
     protected override void Dispose(bool disposing)
     {
-        if (!disposed && disposing)
+        if (!_disposed && disposing)
         {
             StrongReferenceMessenger.Default.UnregisterAll(this);
+            _realmCount.CountChanged -= RealmCount_CountChanged;
+            _realmCount.Dispose();
 
-            disposed = true;
+            _disposed = true;
         }
 
         base.Dispose(disposing);
+    }
+
+    void RealmCount_CountChanged(object? sender, (Type Kind, int Count) e)
+    {
+        if (e.Kind == typeof(AttachmentDraft))
+            UpdateItem(_attachmentsFilter, e.Count);
+        else if (e.Kind == typeof(NoteDraft))
+            UpdateItem(_notesFilter, e.Count);
+        else if (e.Kind == typeof(AssessmentDraft))
+            UpdateItem(_safetyAssessmentFilter, e.Count);
+        else if (e.Kind == typeof(PersonVisitDraft))
+            UpdateItem(_visitsFilter, e.Count);
+    }
+
+    void UpdateItem(FilterOption<IDraftItem> filter, int count)
+    {
+        if (count <= 0)
+            FilterOptions.Remove(filter);
+        else if (!FilterOptions.Contains(filter))
+            FilterOptions.InsertSorted(filter, startIndex: 1);
     }
 }
