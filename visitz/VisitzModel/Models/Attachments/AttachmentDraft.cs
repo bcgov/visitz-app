c5a6339a6cb5972024204732bb8a2e57859566e0
Todd Scharien <todd.scharien@gov.bc.ca>
2026-05-14 08:18:55 -0700

    Compare using LastUpdatedBinding

--- visitz/VisitzModel/Models/Attachments/AttachmentDraft.cs
+++ visitz/VisitzModel/Models/Attachments/AttachmentDraft.cs
@@ -34,6 +34,19 @@ public partial class AttachmentDraft : IRealmObject, IDraftItem
 
     public DateTimeOffset LastUpdated { get; set; } = DateTimeOffset.Now;
 
+    public DateTimeOffset LastUpdatedBinding
+    {
+        get => IsValid ? LastUpdated : default;
+        set
+        {
+            if (!IsValid)
+                return;
+
+            LastUpdated = value;
+            RaisePropertyChanged(nameof(LastUpdated));
+        }
+    }
+
     public string Preview => GeneralStrings.Attachment;
 
     public string DraftLocation { get; set; } = string.Empty;
