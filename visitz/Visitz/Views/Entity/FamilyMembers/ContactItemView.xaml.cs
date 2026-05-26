82dd578709882833ca4bf238496831ec8ff7f5b3
Todd Scharien <todd.scharien@gov.bc.ca>
2026-05-26 09:47:26 -0700

    Adjust item layout and expand logic

--- visitz/Visitz/Views/Entity/FamilyMembers/ContactItemView.xaml.cs
+++ visitz/Visitz/Views/Entity/FamilyMembers/ContactItemView.xaml.cs
@@ -10,4 +10,23 @@ public partial class ContactItemView : BaseContentView
     {
         InitializeComponent();
     }
+
+    async void TapGestureRecognizer_Tapped(object sender, TappedEventArgs e)
+    {
+        if (
+            BindingContext is ContactItemViewModel vm
+            && sender is ContactItemView item
+            && item.Parent is CollectionView cv
+        )
+        {
+            vm.ItemTapped();
+            _ = ScrollTo(cv, vm);
+        }
+    }
+
+    static async Task ScrollTo(CollectionView cv, ContactItemViewModel vm)
+    {
+        await Task.Delay(10); // not a fan but it's the easiest way to let the layout settle
+        cv.ScrollTo(vm, position: ScrollToPosition.Start);
+    }
 }
