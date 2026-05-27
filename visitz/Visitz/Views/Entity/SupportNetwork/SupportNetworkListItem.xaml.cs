0719e5d1d1dfef7d49bf1fea702a2124414e0e82
Todd Scharien <todd.scharien@gov.bc.ca>
2026-05-27 14:02:11 -0700

    Pull UI into SupportNetworkListItem.xaml

---
+++ visitz/Visitz/Views/Entity/SupportNetwork/SupportNetworkListItem.xaml.cs
@@ -0,0 +1,9 @@
+namespace Visitz.Views.Entity.SupportNetwork;
+
+public partial class SupportNetworkListItem : ContentView
+{
+    public SupportNetworkListItem()
+    {
+        InitializeComponent();
+    }
+}
