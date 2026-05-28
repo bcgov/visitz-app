e76a0b83196e447c7a1a755be497ca660ce89191
Todd Scharien <todd.scharien@gov.bc.ca>
2026-05-28 11:57:52 -0700

    Use safe bindings for SupportNetworkItem

--- visitz/VisitzModel/Models/People/SupportNetwork.Bindings.cs
+++ visitz/VisitzModel/Models/People/SupportNetwork.Bindings.cs
@@ -14,6 +14,76 @@ public partial class SupportNetworkItem
         }
     }
 
+    public string NameBinding
+    {
+        get => IsValid ? Name : string.Empty;
+        set
+        {
+            this.Commit(() => Name = value);
+            RaisePropertyChanged(nameof(Name));
+        }
+    }
+
+    public string RelationshipBinding
+    {
+        get => IsValid ? Relationship : string.Empty;
+        set
+        {
+            this.Commit(() => Relationship = value);
+            RaisePropertyChanged(nameof(Relationship));
+        }
+    }
+
+    public string AgencyNameBinding
+    {
+        get => IsValid ? AgencyName : string.Empty;
+        set
+        {
+            this.Commit(() => AgencyName = value);
+            RaisePropertyChanged(nameof(AgencyName));
+        }
+    }
+
+    public string AddressBinding
+    {
+        get => IsValid ? Address : string.Empty;
+        set
+        {
+            this.Commit(() => Address = value);
+            RaisePropertyChanged(nameof(Address));
+        }
+    }
+
+    public string PhoneNumberBinding
+    {
+        get => IsValid ? PhoneNumber : string.Empty;
+        set
+        {
+            this.Commit(() => PhoneNumber = value);
+            RaisePropertyChanged(nameof(PhoneNumber));
+        }
+    }
+
+    public string CellPhoneNumberBinding
+    {
+        get => IsValid ? CellPhoneNumber : string.Empty;
+        set
+        {
+            this.Commit(() => CellPhoneNumber = value);
+            RaisePropertyChanged(nameof(CellPhoneNumber));
+        }
+    }
+
+    public string CommentsBinding
+    {
+        get => IsValid ? Comments : string.Empty;
+        set
+        {
+            this.Commit(() => Comments = value);
+            RaisePropertyChanged(nameof(Comments));
+        }
+    }
+
     public string ActiveBinding
     {
         get => IsValid ? Active : string.Empty;
