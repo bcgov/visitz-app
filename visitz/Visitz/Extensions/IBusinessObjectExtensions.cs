a3711b6f9e61dfc3c1c1100618ea8fc66a7ed2da
Todd Scharien <todd.scharien@gov.bc.ca>
2026-05-28 07:59:24 -0700

    Convert to extension members

--- visitz/Visitz/Extensions/IBusinessObjectExtensions.cs
+++ visitz/Visitz/Extensions/IBusinessObjectExtensions.cs
@@ -9,60 +9,63 @@ namespace Visitz.Extensions;
 
 internal static class IBusinessObjectExtensions
 {
-    /// <summary>
-    /// Prompt the user to ask if they want to download dependent information
-    /// for this business object. Does not initiate a download.
-    /// </summary>
-    /// <param name="businessObject"></param>
-    /// <returns></returns>
-    public static async Task<bool> PromptCanDownloadDependentData(this IBusinessObject businessObject)
+    extension(IBusinessObject businessObject)
     {
-        if (!NetworkHelper.InternetAvailable)
+        /// <summary>
+        /// Prompt the user to ask if they want to download dependent information
+        /// for this business object. Does not initiate a download.
+        /// </summary>
+        /// <param name="businessObject"></param>
+        /// <returns></returns>
+        public async Task<bool> PromptCanDownloadDependentData()
         {
-            await Navigator.CurrentOpenPage.DisplayAlertAsync(
-                LocalizedStrings.NoInternet,
-                LocalizedStrings.NeedInternetToViewRecord,
-                LocalizedStrings.Ok
-            );
-            return false;
+            if (!NetworkHelper.InternetAvailable)
+            {
+                await Navigator.CurrentOpenPage.DisplayAlertAsync(
+                    LocalizedStrings.NoInternet,
+                    LocalizedStrings.NeedInternetToViewRecord,
+                    LocalizedStrings.Ok
+                );
+                return false;
+            }
+            else
+            {
+                string msg = string.Format(
+                    LocalizedStrings.MarkForDownload,
+                    businessObject.EntityType,
+                    businessObject.DisplayName.Trim()
+                );
+
+                return await Navigator.CurrentOpenPage.DisplayAlertAsync(
+                    LocalizedStrings.DownloadRecordInformation,
+                    msg,
+                    LocalizedStrings.Download,
+                    LocalizedStrings.Cancel
+                );
+            }
         }
-        else
+
+        /// <summary>
+        /// Start the download service to get all depdendent data for a business object.
+        /// </summary>
+        /// <param name="businessObject"></param>
+        /// <returns>ID of the service that was started</returns>
+        public async Task<VisitzService.Result> DownloadDependentData()
         {
-            string msg = string.Format(
-                LocalizedStrings.MarkForDownload,
-                businessObject.EntityType,
-                businessObject.DisplayName.Trim()
-            );
+            businessObject.LocalState.ShouldDownloadDuringRefreshBinding = true;
 
-            return await Navigator.CurrentOpenPage.DisplayAlertAsync(
-                LocalizedStrings.DownloadRecordInformation,
-                msg,
-                LocalizedStrings.Download,
-                LocalizedStrings.Cancel
-            );
-        }
-    }
+            try
+            {
+                var handler = ServiceProvider.GetService<ServiceHandler>();
+                var msg = GetAllDataForRecordService.MakeStartMessage(businessObject);
 
-    /// <summary>
-    /// Start the download service to get all depdendent data for a business object.
-    /// </summary>
-    /// <param name="businessObject"></param>
-    /// <returns>ID of the service that was started</returns>
-    public static async Task<VisitzService.Result> DownloadDependentData(this IBusinessObject businessObject)
-    {
-        businessObject.LocalState.ShouldDownloadDuringRefreshBinding = true;
-
-        try
-        {
-            var handler = ServiceProvider.GetService<ServiceHandler>();
-            var msg = GetAllDataForRecordService.MakeStartMessage(businessObject);
-
-            return await handler.TryRunServiceAsync(msg);
-        }
-        catch
-        {
-            businessObject.LocalState.ShouldDownloadDuringRefreshBinding = false;
-            throw;
+                return await handler.TryRunServiceAsync(msg);
+            }
+            catch
+            {
+                businessObject.LocalState.ShouldDownloadDuringRefreshBinding = false;
+                throw;
+            }
         }
     }
 }
