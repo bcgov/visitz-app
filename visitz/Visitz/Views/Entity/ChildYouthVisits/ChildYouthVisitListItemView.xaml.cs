b2f7c8cc661d5d62daa7c73c8c314935f6957e48
Todd Scharien <todd.scharien@gov.bc.ca>
2026-07-10 12:00:44 -0700

    Implement item expander and scroll-to-item behaviour

--- visitz/Visitz/Views/Entity/ChildYouthVisits/ChildYouthVisitListItemView.xaml.cs
+++ visitz/Visitz/Views/Entity/ChildYouthVisits/ChildYouthVisitListItemView.xaml.cs
@@ -1,11 +1,36 @@
+using Microsoft.Extensions.Logging;
+using Visitz.Views.BaseClasses;
+
 namespace Visitz.Views.Entity.ChildYouthVisits;
 
 #nullable enable
 
-public partial class ChildYouthVisitListItemView : ContentView
+public partial class ChildYouthVisitListItemView : BaseContentView
 {
     public ChildYouthVisitListItemView()
     {
         InitializeComponent();
+    }
+
+    async void TapGestureRecognizer_Tapped(object sender, TappedEventArgs e)
+    {
+        try
+        {
+            if (BindingContext is ChildYouthVisitListItemViewModel vm && Parent is CollectionView cv)
+            {
+                vm.ItemTapped();
+                await ScrollTo(cv, vm);
+            }
+        }
+        catch (Exception ex)
+        {
+            Logger.LogError(ex.Message, ex);
+        }
+    }
+
+    static async Task ScrollTo(CollectionView cv, ChildYouthVisitListItemViewModel vm)
+    {
+        await Task.Delay(10); // not a fan but it's the easiest way to let the layout settle
+        cv.ScrollTo(vm, position: ScrollToPosition.Start);
     }
 }
--- visitz/Visitz/Views/Entity/ChildYouthVisits/ChildYouthVisitListItemViewModel.cs
+++ visitz/Visitz/Views/Entity/ChildYouthVisits/ChildYouthVisitListItemViewModel.cs
@@ -1,4 +1,6 @@
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Visitz.FontIcons;
 using Visitz.Views.BaseClasses;
 using VisitzModel.Models.InPersonVisits;
 
@@ -10,4 +12,21 @@ public partial class ChildYouthVisitListItemViewModel(PersonVisit visit) : Visit
 {
     [ObservableProperty]
     public partial PersonVisit Visit { get; set; } = visit;
+
+    [ObservableProperty]
+    public partial bool Expanded { get; set; }
+
+    [ObservableProperty]
+    public partial string ExpandedChevronGlyph { get; set; } = MaterialIcons.Keyboard_arrow_down;
+
+    partial void OnExpandedChanged(bool value)
+    {
+        ExpandedChevronGlyph = value ? MaterialIcons.Keyboard_arrow_up : MaterialIcons.Keyboard_arrow_down;
+    }
+
+    [RelayCommand]
+    public void ItemTapped()
+    {
+        Expanded = !Expanded;
+    }
 }
