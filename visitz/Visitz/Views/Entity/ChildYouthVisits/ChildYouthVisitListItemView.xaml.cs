9ee991e15a412ab49e1f12e21b8d1563b4bda6db
Todd Scharien <todd.scharien@gov.bc.ca>
2026-07-10 10:05:00 -0700

    Pull visit list item into its own file

---
+++ visitz/Visitz/Views/Entity/ChildYouthVisits/ChildYouthVisitListItemView.xaml.cs
@@ -0,0 +1,9 @@
+namespace Visitz.Views.Entity.ChildYouthVisits;
+
+public partial class ChildYouthVisitListItemView : ContentView
+{
+    public ChildYouthVisitListItemView()
+    {
+        InitializeComponent();
+    }
+}
