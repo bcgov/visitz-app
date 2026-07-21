ef8d787cbced1e26a814cd29b8546ea1f041f5c3
Scharien, Todd CITZ:EX <todd.scharien@gov.bc.ca>
2026-07-21 10:39:46 -0700

    Remove unused keyboard logic

--- visitz/Visitz/Views/Entity/ChildYouthVisits/ChildYouthVisitView.xaml.cs
+++ visitz/Visitz/Views/Entity/ChildYouthVisits/ChildYouthVisitView.xaml.cs
@@ -7,54 +7,10 @@ namespace Visitz.Views.Entity.ChildYouthVisits;
 
 public partial class ChildYouthVisitView : IcmRecordContentView<ChildYouthVisitViewModel>
 {
-    private bool _disposed;
-    private bool _isKeyboardOpen;
-
-    private SoftKeyboardOpenHandler _keyboardOpenHandler;
-
     public ChildYouthVisitView()
         : base(ServiceProvider.GetService<ChildYouthVisitViewModel>())
     {
         InitializeComponent();
         BindingContext = ViewModel;
-
-        _keyboardOpenHandler = new SoftKeyboardOpenHandler();
-        _keyboardOpenHandler.KeyboardStateChanged += OnKeyboardStateChanged;
-        DeviceDisplay.MainDisplayInfoChanged += OnMainDisplayInfoChanged;
-    }
-
-    private void OnKeyboardStateChanged(object? sender, KeyboardStateChangedEventArgs e)
-    {
-        _isKeyboardOpen = e.IsKeyboardOpen;
-        CheckAndApplyOrientation(_isKeyboardOpen);
-    }
-
-    private void OnMainDisplayInfoChanged(object? sender, DisplayInfoChangedEventArgs e)
-    {
-        CheckAndApplyOrientation(_isKeyboardOpen);
-    }
-
-    private void CheckAndApplyOrientation(bool isKeyboardOpen)
-    {
-        bool hideForm =
-            VisitsEditor.EditorView.IsFocused
-            && DeviceDisplay.MainDisplayInfo.Orientation == DisplayOrientation.Landscape
-            && isKeyboardOpen;
-
-        ViewModel.ShowFullForm = !hideForm;
-    }
-
-    protected override void Dispose(bool disposing)
-    {
-        if (!_disposed && disposing)
-        {
-            ViewModel.SaveStateHandler.Dispose();
-            _keyboardOpenHandler.Dispose();
-            DeviceDisplay.MainDisplayInfoChanged -= OnMainDisplayInfoChanged;
-
-            _disposed = true;
-        }
-
-        base.Dispose(disposing);
     }
 }
