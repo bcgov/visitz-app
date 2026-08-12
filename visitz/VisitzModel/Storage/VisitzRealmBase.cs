87c1b6e3c09bca080a887433c03928c54c060f31
Todd Scharien <todd.scharien@gov.bc.ca>
2026-08-12 11:59:47 -0700

    Add new DB version

--- visitz/VisitzModel/Storage/VisitzRealmBase.cs
+++ visitz/VisitzModel/Storage/VisitzRealmBase.cs
@@ -16,6 +16,7 @@ public abstract class VisitzRealmBase(string realmName, ulong version, byte[] en
     public static readonly ulong Version2_7_0 = 4;
     public static readonly ulong Version2_7_1 = 5;
     public static readonly ulong Version2_8_0 = 6;
+    public static readonly ulong Version3_0_0 = 7;
 
     public string RealmName { get; private set; } = realmName;
 
