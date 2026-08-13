786a8c8c567e5249038ba38a94bcdc27ced88e70
Todd Scharien <todd.scharien@gov.bc.ca>
2026-08-13 09:02:56 -0700

    Refactor safety assessment migrations

--- visitz/VisitzModel/Storage/Migrations/SafetyAssessmentMigrations.cs
+++ visitz/VisitzModel/Storage/Migrations/SafetyAssessmentMigrations.cs
@@ -1,13 +1,38 @@
 using Realms;
-using VisitzModel.Storage.Migrations.SafetyAssessments;
 
 namespace VisitzModel.Storage.Migrations;
 
 internal static class SafetyAssessmentMigrations
 {
-    public static void MigrateRealm(Migration migration, ulong oldSchemaVersion)
+    internal static void MigrateRealm(Migration migration, ulong oldSchemaVersion)
     {
         if (oldSchemaVersion < VisitzRealmBase.Version2_3_3)
-            SafetyAssessment2_3_3.Migrate(migration);
+            Migrate2_3_3(migration);
+    }
+
+    static void Migrate2_3_3(Migration migration)
+    {
+        const string SafetyAssessmentName = "SafetyAssessment";
+        const string AssessmentDraftName = "AssessmentDraft";
+
+        const string IncidentNumberName = "IncidentNumber";
+        const string DraftCreatedName = "DraftCreated";
+
+        const string LastCreatedName = "LastUpdated";
+
+        static void Create(Migration migration, IRealmObject oldItem)
+        {
+            string pk = oldItem.DynamicApi.Get<string>(IncidentNumberName);
+            var newDraft = migration.NewRealm.DynamicApi.CreateObject(AssessmentDraftName, pk);
+
+            newDraft.DynamicApi.Set(DraftCreatedName, DateTimeOffset.MinValue);
+            newDraft.DynamicApi.Set(LastCreatedName, DateTimeOffset.MinValue);
+        }
+
+
+        var oldItems = migration.OldRealm.DynamicApi.All(SafetyAssessmentName);
+
+        for (int i = 0; i < oldItems.Count(); i++)
+            Create(migration, oldItems.ElementAt(i));
     }
 }
