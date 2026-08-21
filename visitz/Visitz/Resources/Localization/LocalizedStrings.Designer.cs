aea31540e3a3584e9c0ca8c62a9fbc154c70cc3e
Todd Scharien <todd.scharien@gov.bc.ca>
2026-08-21 13:18:42 -0700

    Rework safety assessment list item metadata label

--- visitz/Visitz/Resources/Localization/LocalizedStrings.Designer.cs
+++ visitz/Visitz/Resources/Localization/LocalizedStrings.Designer.cs
@@ -679,6 +679,15 @@ namespace Visitz.Resources.Localization {
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to Created {0} by {1}.
+        /// </summary>
+        public static string CreatedWhenByUserLabel {
+            get {
+                return ResourceManager.GetString("CreatedWhenByUserLabel", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to Date.
         /// </summary>
         public static string Date {
