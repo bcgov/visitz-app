f1bc1666f8a67013ed0067c7741fe5807f8fe0d6
Todd Scharien <todd.scharien@gov.bc.ca>
2026-09-16 10:55:28 -0700

    Use Expander view for support network items

--- visitz/Visitz/Views/Entity/SupportNetwork/SupportNetworkListItem.xaml.cs
+++ visitz/Visitz/Views/Entity/SupportNetwork/SupportNetworkListItem.xaml.cs
@@ -1,6 +1,4 @@
-using Microsoft.Extensions.Logging;
 using Visitz.Views.BaseClasses;
-using VisitzModel.Extensions;
 
 namespace Visitz.Views.Entity.SupportNetwork;
 
@@ -9,31 +7,5 @@ public partial class SupportNetworkListItem : BaseContentView
     public SupportNetworkListItem()
     {
         InitializeComponent();
-    }
-
-    private async void TapGestureRecognizer_Tapped(object? sender, TappedEventArgs e)
-    {
-        try
-        {
-            if (
-                BindingContext is SupportNetworkItemUi itemVm
-                && sender is SupportNetworkListItem item
-                && item.Parent is CollectionView cv
-            )
-            {
-                itemVm.ToggleExpanded();
-                await ScrollTo(cv, itemVm);
-            }
-        }
-        catch (Exception ex)
-        {
-            Logger.LogException(ex);
-        }
-    }
-
-    static async Task ScrollTo(CollectionView cv, SupportNetworkItemUi vm)
-    {
-        await Task.Delay(10); // not a fan but it's the easiest way to let the layout settle
-        cv.ScrollTo(vm, position: ScrollToPosition.Start);
     }
 }
