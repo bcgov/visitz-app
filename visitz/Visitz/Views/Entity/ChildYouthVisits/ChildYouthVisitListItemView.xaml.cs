0c33fcef26958d3f86e280a30b806a03ab20c2aa
Todd Scharien <todd.scharien@gov.bc.ca>
2026-09-16 11:40:36 -0700

    Use Expander view for child/youth visits

--- visitz/Visitz/Views/Entity/ChildYouthVisits/ChildYouthVisitListItemView.xaml.cs
+++ visitz/Visitz/Views/Entity/ChildYouthVisits/ChildYouthVisitListItemView.xaml.cs
@@ -1,6 +1,4 @@
-using Microsoft.Extensions.Logging;
 using Visitz.Views.BaseClasses;
-using VisitzModel.Extensions;
 
 namespace Visitz.Views.Entity.ChildYouthVisits;
 
@@ -9,27 +7,5 @@ public partial class ChildYouthVisitListItemView : BaseContentView
     public ChildYouthVisitListItemView()
     {
         InitializeComponent();
-    }
-
-    async void TapGestureRecognizer_Tapped(object? sender, TappedEventArgs e)
-    {
-        try
-        {
-            if (BindingContext is ChildYouthVisitListItemViewModel vm && Parent is CollectionView cv)
-            {
-                vm.ItemTapped();
-                await ScrollTo(cv, vm);
-            }
-        }
-        catch (Exception ex)
-        {
-            Logger.LogException(ex);
-        }
-    }
-
-    static async Task ScrollTo(CollectionView cv, ChildYouthVisitListItemViewModel vm)
-    {
-        await Task.Delay(10); // not a fan but it's the easiest way to let the layout settle
-        cv.ScrollTo(vm, position: ScrollToPosition.Start);
     }
 }
