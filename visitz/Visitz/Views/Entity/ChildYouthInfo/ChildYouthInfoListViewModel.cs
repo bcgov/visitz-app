ed4d096ac4f7aa6c1c16c00b1cb30520ba5e48f6
Todd Scharien <todd.scharien@gov.bc.ca>
2026-09-16 16:28:18 -0700

    Begin child/youth info UI implementation

--- visitz/Visitz/Views/Entity/ChildYouthInfo/ChildYouthInfoListViewModel.cs
+++ visitz/Visitz/Views/Entity/ChildYouthInfo/ChildYouthInfoListViewModel.cs
@@ -1,5 +1,49 @@
+using CommunityToolkit.Mvvm.ComponentModel;
+using Visitz.Storage;
 using Visitz.Views.BaseClasses;
+using VisitzModel.Models;
+using VisitzModel.Models.People;
 
 namespace Visitz.Views.Entity.ChildYouthInfo;
 
-public partial class ChildYouthInfoListViewModel : IcmRecordViewModel { }
+public partial class ChildYouthInfoListViewModel : IcmRecordViewModel
+{
+    bool _disposed;
+
+    [ObservableProperty]
+    public partial bool IsLoading { get; set; }
+
+    [ObservableProperty]
+    public partial ObservableQuery<ContactEducation> Education { get; private set; }
+
+    [ObservableProperty]
+    public partial ObservableQuery<ContactMedicalBehavioral> MedicalBehavioral { get; private set; }
+
+    [ObservableProperty]
+    public partial ObservableQuery<ContactLanguage> Languages { get; private set; }
+
+    protected override async Task InitAsync()
+    {
+        await base.InitAsync();
+
+        var dataRealm = await VisitzRealms.GetIcmDataRealmAsync();
+
+        Education = new(dataRealm, query: dataRealm.All<ContactEducation>());
+
+        MedicalBehavioral = new(dataRealm, query: dataRealm.All<ContactMedicalBehavioral>());
+
+        Languages = new(dataRealm, query: dataRealm.All<ContactLanguage>());
+    }
+
+    protected override void Dispose(bool disposing)
+    {
+        if (!_disposed && disposing)
+        {
+            Education.Dispose();
+            MedicalBehavioral.Dispose();
+            Languages.Dispose();
+            _disposed = true;
+        }
+        base.Dispose(disposing);
+    }
+}
